93df40afa896ef71121a979a7f5187157db4bcdd
Yvonne Tang <ytang2@stanford.edu>
2021-04-06 10:40:27 -0700

    Slightly adjust underline offset and doc

--- src/tailwind/plugins/components/underline/custom-underline.js
+++ src/tailwind/plugins/components/underline/custom-underline.js
@@ -6,12 +6,14 @@ module.exports = function () {
     const components = {
       // Add spacing between text and underline
       '.underline-offset': {
-        textUnderlineOffset: '0.2em',
+        textUnderlineOffset: '0.2rem',
       },
       // Custom text underline colors
+      // For use directly in an <a> tag
       '.underline-saa-digital-red': {
         textDecorationColor: theme('colors.saa-digital-red'),
       },
+      // For use in a parent container that contains the links
       '.link-underline-saa-digital-red a': {
         textDecorationColor: theme('colors.saa-digital-red'),
       },
