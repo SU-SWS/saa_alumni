10f6b5fc678dcf670362c87de49a9ab26ef4f360
Yvonne Tang <ytang2@stanford.edu>
2021-04-13 01:37:41 -0700

    styles

--- src/components/composite/hero.js
+++ src/components/composite/hero.js
@@ -18,13 +18,13 @@ const Hero = (props) => {
           />
         )}
         <div className='su-absolute su-block su-w-full su-h-full su-top-0 su-bg-gradient-to-b su-from-transparent su-to-saa-black' />
-        <Container className='su-relative su-pt-11 su-pb-7'>
-          <p className='su-text-center su-text-white su-font-semibold su-text-m4 su-mb-02em'>{props.blok.sansSuper}</p>
+        <Container className='su-relative su-rs-pt-10 su-rs-pb-4'>
+          <p className='su-text-center su-text-white su-font-semibold su-text-m4 su-mb-01em'>{props.blok.sansSuper}</p>
           <p className='su-text-center su-text-white su-font-serif su-text-m2 su-mb-02em'>{props.blok.serifSuper}</p>
           <Heading level={1} font='serif' weight='bold' className='su-text-white su-text-center su-text-m8 su-tracking-normal su-mb-02em'>{props.blok.headline}</Heading>
           <p className='su-text-center su-text-white su-max-w-600 su-mx-auto su-text-m1 su-mb-0'>{props.blok.sansSub}</p>
+          <p className='su-text-center su-text-white su-font-semibold su-rs-pt-4 su-font-serif su-font-regular su-text-m1'>Scroll to explore</p>
         </Container>
-        <p className='su-text-center su-text-white su-font-semibold'>Scroll to explore</p>
       </Container>
     </SbEditable>
   );
