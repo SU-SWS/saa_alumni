22e4cfee5255ec8aa61a31b0fb3c1ba3e34f10a2
Shea McKinney <sherakama@gmail.com>
2021-04-20 12:18:00 -0700

    Editor fixup!

--- src/pages/editor.js
+++ src/pages/editor.js
@@ -12,8 +12,8 @@ let storyblokInstance;
  * @returns
  */
 const getParam = function(val) {
-  var result = ''
-  var tmp = []
+  var result = '';
+  var tmp = [];
 
   window.location.search
     .substr(1)
@@ -31,62 +31,57 @@ const getParam = function(val) {
 /**
  * This is Sparta
  */
-const initBridge = function(key, sbResolveRelations, setStory, setError) {
+const initBridge = function(key, sbResolveRelations, setStory, setError, setState) {
 
   storyblokInstance = new window.StoryblokBridge({
     accessToken: key,
     resolveRelations: sbResolveRelations
   });
 
-  console.log(storyblokInstance)
-
   storyblokInstance.on('input', (payload) => {
-    // console.log(payload);
-    setStory(payload.story.content)
+    console.log(payload);
+    if (payload !== undefined) {
+      setStory(payload.story.content)
+    }
   });
 
   storyblokInstance.on('change', (payload) => {
-    // console.log(payload);
-    setStory(payload.story.content)
+    console.log(payload);
+    if (payload) {
+      setStory(payload.story.content)
+    }
   });
 
   storyblokInstance.on('enterEditmode', (info) => {
-    console.log("Entering the edit mode");
+    console.log("Entering the edit mode -------------");
     console.log(info);
-    // setStory(payload.story);
+    console.log("------------------ -------------");
   })
 
   storyblokInstance.on(['published', 'change'], (event) => {
     if (!event.slugChanged) {
-      window.location.reload(true)
+      console.log('on slug change.');
+      window.location.reload()
     }
   })
 
+  storyblokInstance.on('viewLiveVersion', (event) => {
+    console.log("Load live version.");
+    console.log(event);
+  })
+
   // Call ping editor to see if in editor
-  storyblokInstance.pingEditor(() => {
+  storyblokInstance.pingEditor((editor) => {
     if (storyblokInstance.isInEditor()) {
-      // Load draft version of story
-      // storyblokInstance.enterEditmode();
-      // loadStory(setStory, sbResolveRelations)
-      console.log("All done.")
-    } else {
+      setState(true);
+      console.log(editor);
+    }
+    else {
       // Load published version of story
       setError(true);
     }
   })
-}
 
-/**
- *
- */
- const loadStory = (setStory, sbResolveRelations) => {
-  window.storyblok.get({
-    slug: window.storyblok.getParam('path'),
-    version: 'draft',
-    resolve_relations: sbResolveRelations || []
-  }, (data) => {
-    setStory(data.story.content)
-  })
 }
 
 /**
@@ -114,7 +109,7 @@ const StoryblokEntry = (props) => {
           }
         }
       }
-    `,
+    `
   );
 
   /**
@@ -136,8 +131,7 @@ const StoryblokEntry = (props) => {
       script.type = 'text/javascript';
       script.src = 'https://app.storyblok.com/f/storyblok-v2-latest.js';
       script.onload = () => {
-        initBridge(key, sbResolveRelations, setStory, setError);
-        setState(true);
+        initBridge(key, sbResolveRelations, setStory, setError, setState);
       };
       document.getElementsByTagName('head')[0].appendChild(script);
     }
