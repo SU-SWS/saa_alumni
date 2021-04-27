c642804c5e082a83229b7d9debf40aaa10c77ab3
Yvonne Tang <ytang2@stanford.edu>
2021-04-26 18:39:54 -0700

    Grid and section

--- src/components/layout/grid.js
+++ src/components/layout/grid.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import SbEditable from 'storyblok-react';
 import CreateBloks from '../../utilities/createBloks';
-import { DrGrid } from 'decanter-react';
-import { dcnb } from 'cnbuilder';
+import { Grid as DrGrid } from 'decanter-react';
 
 const Grid = (props) => {
   return (
     <SbEditable content={props.blok}>
-      <DrGrid xs={1} md={2} lg={props.blok.numCol} gap={true} className='grid'>
-        <CreateBloks />
+      <DrGrid xs={1} md={2} lg={props.blok.numCol} gap={true} className='grid su-items-start su-justify-items-center'>
+        {/* PurgeCSS: su-grid-cols-1 md:su-grid-cols-2 lg:su-grid-cols-3 lg:su-grid-cols-2 */}
+        <CreateBloks blokSection={props.blok.content} />
       </DrGrid>
     </SbEditable>
   );
--- src/components/layout/section.js
+++ src/components/layout/section.js
@@ -12,24 +12,27 @@ const Section = (props) => {
 
   return (
     <SbEditable content={props.blok}>
-      <section className={dcnb('section su-cc su-text-center su-basefont-23', bgColor)}>
-        {props.blok.superhead &&
-          <SbLink link={props.blok.superLink} classes='su-inline-block su-rs-mb-6'>
-            {props.blok.superhead}
-          </SbLink>
-        }
-        {props.blok.title &&
-          <Heading level={1}
-                   font='serif'
-                   weight='bold'
-                   className={`su-mb-02em su-type-5`}
-          >
-            {props.blok.title}
-          </Heading>
-        }
-        <div className='su-big-paragraph su-max-w-prose su-mx-auto'>
-          <RichTextRenderer wysiwyg={props.blok.intro} className='children:su-leading-display' />
-        </div>
+      <section className={dcnb('section su-cc su-rs-py-6 su-basefont-23', bgColor)}>
+        <header className='su-text-center su-rs-mb-5'>
+          {props.blok.superhead &&
+            <SbLink link={props.blok.superLink} classes='su-inline-block su-rs-mb-6'>
+              {props.blok.superhead}
+            </SbLink>
+          }
+          {props.blok.title &&
+            <Heading level={1}
+                     font='serif'
+                     weight='bold'
+                     className={`su-mb-02em su-type-5`}
+            >
+              {props.blok.title}
+            </Heading>
+          }
+          <div className='su-big-paragraph su-max-w-prose su-mx-auto'>
+            <RichTextRenderer wysiwyg={props.blok.intro} className='children:su-leading-display' />
+          </div>
+        </header>
+        <CreateBloks blokSection={props.blok.content} />
       </section>
     </SbEditable>
   );
