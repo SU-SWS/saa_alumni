b9348a3ebea9445379b14b939717ca65f40aee9f
Yvonne Tang <ytang2@stanford.edu>
2021-04-27 14:41:23 -0700

    Trying Shea's decanter-react #ADAPT-2296

--- src/components/layout/grid.js
+++ src/components/layout/grid.js
@@ -20,7 +20,6 @@ const Grid = (props) => {
   return (
     <SbEditable content={props.blok}>
       <DrGrid xs={1} md={2} lg={props.blok.numCol} className={dcnb('grid su-items-start su-justify-items-center', gridWidth, gapClasses)}>
-        {/* PurgeCSS: su-grid-cols-1 md:su-grid-cols-2 lg:su-grid-cols-3 lg:su-grid-cols-2 */}
         <CreateBloks blokSection={props.blok.content} />
       </DrGrid>
     </SbEditable>
