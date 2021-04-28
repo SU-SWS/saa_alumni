2c8b003df93dee867b3d42109e966ce7a97f06f2
Yvonne Tang <ytang2@stanford.edu>
2021-04-27 17:56:14 -0700

    Grid options

--- src/components/layout/grid.js
+++ src/components/layout/grid.js
@@ -1,26 +1,45 @@
 import React from 'react';
 import SbEditable from 'storyblok-react';
 import CreateBloks from '../../utilities/createBloks';
-import { Grid as DrGrid } from 'decanter-react';
+import { Grid as DrGrid, GridCell } from 'decanter-react';
 import { dcnb } from 'cnbuilder';
 
 const Grid = (props) => {
+  // Sets whether wrapper is edge-to-edge
+  let wrapperClasses = '';
+
+  if (props.blok.width !== 'edge-to-edge') {
+    wrapperClasses = 'su-cc';
+  }
+
+  // Options for grid width if not edge-to-edge
+  let gridWidth = '';
+
+  if (props.blok.width === '8') {
+    gridWidth = 'su-col-span-12 lg:su-col-span-10 lg:su-col-start-2 xl:su-col-span-8 xl:su-col-start-3';
+  }
+  else if (props.blok.width === '10') {
+    gridWidth = 'su-col-span-12 xl:su-col-span-10 xl:su-col-start-2';
+  }
+  else {
+    gridWidth = 'su-col-span-12';
+  }
+
+  // Options to have regular grid gap or 1px horizontal gaps
   let gapClasses = 'su-grid-gap';
 
   if (props.blok.removeGap) {
-    gapClasses = 'su-gap-x-[1px] su-gap-y-2xl md:su-gap-y-[100px]'
-  }
-
-  let gridWidth = '';
-
-  if (props.blok.width === '12') {
-    gridWidth = 'su-cc'
+    gapClasses = 'su-gap-x-[1px]'
   }
 
   return (
     <SbEditable content={props.blok}>
-      <DrGrid xs={1} md={2} lg={props.blok.numCol} className={dcnb('grid su-items-start su-justify-items-center', gridWidth, gapClasses)}>
-        <CreateBloks blokSection={props.blok.content} />
+      <DrGrid xs={12} className={wrapperClasses}>
+        <div className={gridWidth}>
+          <DrGrid xs={1} md={2} lg={props.blok.numCol} className={dcnb('su-items-start su-justify-items-center su-gap-y-2xl md:su-gap-y-[80px] xl:su-gap-y-[100px]', gapClasses)}>
+            <CreateBloks blokSection={props.blok.content} />
+          </DrGrid>
+        </div>
       </DrGrid>
     </SbEditable>
   );
