6d7b56192d8703c085053eef34dd8464928fb72a
Yvonne Tang <ytang2@stanford.edu>
2021-04-28 00:04:17 -0700

    Add auto column option for Grid

--- src/components/layout/grid.js
+++ src/components/layout/grid.js
@@ -13,12 +13,22 @@ const Grid = (props) => {
     gapClasses = 'su-gap-x-[1px]'
   }
 
+  let grid =
+    <DrGrid xs={1} md={2} lg={props.blok.numCol} className={dcnb('su-items-start su-justify-items-center su-gap-y-2xl md:su-gap-y-[80px] xl:su-gap-y-[100px]', gapClasses)}>
+      <CreateBloks blokSection={props.blok.content} />
+    </DrGrid>
+
+  if (props.blok.numCol === 'auto') {
+    grid =
+      <DrGrid className={dcnb('su-grid-cols-[repeat(auto-fit,minmax(34rem,1fr))] su-items-start su-justify-items-center su-gap-y-2xl md:su-gap-y-[80px] xl:su-gap-y-[100px]', gapClasses)}>
+        <CreateBloks blokSection={props.blok.content} />
+      </DrGrid>;
+  }
+
   return (
     <SbEditable content={props.blok}>
       <WidthBox width={props.blok.width} className='saa-grid'>
-        <DrGrid xs={1} md={2} lg={props.blok.numCol} className={dcnb('su-items-start su-justify-items-center su-gap-y-2xl md:su-gap-y-[80px] xl:su-gap-y-[100px]', gapClasses)}>
-          <CreateBloks blokSection={props.blok.content} />
-        </DrGrid>
+        {grid}
       </WidthBox>
     </SbEditable>
   );
