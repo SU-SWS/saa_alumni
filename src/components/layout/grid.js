28ed2c20bb7373c11bba077614e09f8ee0d1da1d
Sherakama <sherakama@gmail.com>
2021-04-29 10:45:31 -0700

    ADAPT-2116: Suggested Edits. (#22)
    * ADAPT-2116: Suggested Edits.
    
    * ADAPT-2116: All the destructures.
    
    * Fixup! buttonstyle.
    
    * scrollTo link in hero fixup
    
    * Update src/components/partials/seo.js
    
    Co-authored-by: Yvonne Tang <ytang2@stanford.edu>
    
    * Better props drilling.
    
    Co-authored-by: Yvonne Tang <ytang2@stanford.edu>

--- src/components/layout/grid.js
+++ src/components/layout/grid.js
@@ -5,29 +5,29 @@ import { Grid as DrGrid } from 'decanter-react';
 import { dcnb } from 'cnbuilder';
 import WidthBox from './widthBox';
 
-const Grid = (props) => {
+const Grid = ({blok: { removeGap, numCol, content, width }, blok}) => {
   // Options to have regular grid gap or 1px horizontal gaps
   let gapClasses = 'su-grid-gap';
 
-  if (props.blok.removeGap) {
+  if (removeGap) {
     gapClasses = 'su-gap-x-[1px]'
   }
 
   let grid =
-    <DrGrid xs={1} md={2} lg={props.blok.numCol} className={dcnb('su-items-start su-justify-items-center su-gap-y-2xl md:su-gap-y-[80px] xl:su-gap-y-[100px]', gapClasses)}>
-      <CreateBloks blokSection={props.blok.content} />
+    <DrGrid xs={1} md={2} lg={numCol} className={dcnb('su-items-start su-justify-items-center su-gap-y-2xl md:su-gap-y-[80px] xl:su-gap-y-[100px]', gapClasses)}>
+      <CreateBloks blokSection={content} />
     </DrGrid>
 
-  if (props.blok.numCol === 'auto') {
+  if (numCol === 'auto') {
     grid =
       <DrGrid className={dcnb('su-grid-cols-[repeat(auto-fit,minmax(34rem,1fr))] su-items-start su-justify-items-center su-gap-y-2xl md:su-gap-y-[80px] xl:su-gap-y-[100px]', gapClasses)}>
-        <CreateBloks blokSection={props.blok.content} />
+        <CreateBloks blokSection={content} />
       </DrGrid>;
   }
 
   return (
-    <SbEditable content={props.blok}>
-      <WidthBox width={props.blok.width} className='saa-grid'>
+    <SbEditable content={blok}>
+      <WidthBox width={width} className='saa-grid'>
         {grid}
       </WidthBox>
     </SbEditable>
