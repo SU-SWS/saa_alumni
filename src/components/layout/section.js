f293ac234a236f21a8635917c49f5ccbfb1c1ead
Yvonne Tang <ytang2@stanford.edu>
2021-04-29 13:10:51 -0700

    Destructure section component props

--- src/components/layout/section.js
+++ src/components/layout/section.js
@@ -8,49 +8,64 @@ import { bgColors, largePaddingTop, largePaddingBottom } from '../../utilities/d
 import SbLink from '../../utilities/sbLink';
 import { dcnb } from 'cnbuilder';
 
-const Section = (props) => {
-  const numCta = getNumBloks(props.blok.cta);
+const Section = ({
+  blok: {
+    superhead,
+    superLink,
+    title,
+    intro,
+    cta,
+    content,
+    isSrOnlyTitle,
+    bgColor,
+    spacingTop,
+    spacingBottom,
+    id
+  },
+  blok
+}) => {
+  const numCta = getNumBloks(cta);
 
-  const bgColor = bgColors[props.blok.bgColor] ?? bgColors['white'];
+  const sectionBgColor = bgColors[bgColor] ?? bgColors['white'];
 
   let superLinkColor = 'su-text-black hocus:su-text-saa-electric-blue su-border-saa-electric-blue';
 
-  if (props.blok.bgColor === 'black') {
+  if (bgColor === 'black') {
     superLinkColor = 'su-text-white hocus:su-text-saa-electric-blue-light su-border-saa-electric-blue-light';
   }
 
-  const paddingTop = largePaddingTop[props.blok.spacingTop] ?? largePaddingTop['lg'];
-  const paddingBottom = largePaddingBottom[props.blok.spacingBottom] ?? largePaddingBottom['lg'];
+  const paddingTop = largePaddingTop[spacingTop] ?? largePaddingTop['lg'];
+  const paddingBottom = largePaddingBottom[spacingBottom] ?? largePaddingBottom['lg'];
 
   return (
-    <SbEditable content={props.blok}>
-      <section className={dcnb('section su-basefont-23', bgColor, paddingTop, paddingBottom)} id={props.blok.id}>
+    <SbEditable content={blok}>
+      <section className={dcnb('section su-basefont-23', sectionBgColor, paddingTop, paddingBottom)} id={id}>
         <header className='su-cc su-text-center su-rs-mb-5'>
-          {props.blok.superhead &&
-            <SbLink link={props.blok.superLink}
+          {superhead &&
+            <SbLink link={superLink}
                     classes={dcnb('su-inline-block su-rs-mb-6 su-pb-6 su-no-underline su-gradient-border su-border-to-r-palo-verde-dark-to-saa-electric-blue su-border-b-[4px] su-border-solid su-transition-colors hocus:su-no-gradient-border', superLinkColor)}
             >
-              {props.blok.superhead}
+              {superhead}
             </SbLink>
           }
-          {props.blok.title &&
+          {title &&
             <Heading level={1}
                      font='serif'
                      weight='bold'
                      className={`su-mb-02em su-type-5`}
-                     srOnly={props.blok.isSrOnlyTitle}
+                     srOnly={isSrOnlyTitle}
             >
-              {props.blok.title}
+              {title}
             </Heading>
           }
           <div className='su-big-paragraph su-max-w-prose su-mx-auto'>
-            <RichTextRenderer wysiwyg={props.blok.intro} className='children:su-leading-display' />
+            <RichTextRenderer wysiwyg={intro} className='children:su-leading-display' />
           </div>
         </header>
-        <CreateBloks blokSection={props.blok.content} />
+        <CreateBloks blokSection={content} />
         {numCta > 0 &&
           <Container width='site' className='su-rs-mt-6'>
-            <CreateBloks blokSection={props.blok.cta}/>
+            <CreateBloks blokSection={cta}/>
           </Container>
         }
       </section>
