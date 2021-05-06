ac91ea2f8628a8f4503732789207d8e8011c08eb
Yvonne Tang <ytang2@stanford.edu>
2021-05-06 00:12:43 -0700

    Add "stretched-link" TW utility; apply to link in heading of perk card which stretches over the whole card

--- src/components/content-types/perk/perkCardView.js
+++ src/components/content-types/perk/perkCardView.js
@@ -24,27 +24,27 @@ const PerkCardView = (props) => {
 
   return (
     <SbEditable content={props.blok}>
-      <article className='perk-card su-relative su-basefont-23 su-max-w-400 su-border su-border-solid su-border-black-90'>
+      <article className='perk-card su-relative su-break-words su-basefont-23 su-max-w-500 su-border su-border-solid su-border-black-90'>
         <div className='perk-card-image-wrapper su-relative'>
           {filename?.startsWith('http') &&
             <figure className='su-overflow-hidden su-w-full su-h-full'>
               <CardImage
                 filename={filename}
                 imageFocus={imageFocus}
+                className='su-w-full su-h-full su-object-cover'
+                loading='lazy'
               />
             </figure>
           }
           <div className={`su-absolute su-block su-w-full su-h-full su-top-0 su-bg-gradient-to-b su-from-transparent su-to-saa-black`} aria-hidden='true' />
         </div>
-        <div className='perk-card-content su-relative su-mt-[-2em] su-rs-px-2 su-rs-pb-3'>
-          <SbLink
-            link={perkPageLink}
-            classes='su-text-white hocus:su-text-white su-no-underline hocus:su-underline su-underline-custom !su-underline-digital-red-xlight'
-          >
-            <Heading level={headingLevel ?? 3} font='serif' size={1} tracking='normal'>{title}</Heading>
-          </SbLink>
-          <p className='su-text-black-20 su-card-paragraph su-mb-0'>{intro}</p>
-        </div>
+        <SbLink
+          link={perkPageLink}
+          classes='su-stretched-link su-text-white hocus:su-text-white su-no-underline hocus:su-underline su-underline-custom !su-underline-digital-red-xlight'
+        >
+          <Heading level={headingLevel ?? 3} font='serif' size={1} tracking='normal' className='su-relative su-mt-[-2em] su-rs-px-2'>{title}</Heading>
+        </SbLink>
+        <p className='su-relative su-text-black-20 su-card-paragraph su-mb-0 su-rs-px-2 su-rs-pb-3'>{intro}</p>
       </article>
     </SbEditable>
   );
