aaf476887893b1b4d5d9607dd1f51633a00f4aa3
Yvonne Tang <ytang2@stanford.edu>
2021-05-05 23:26:16 -0700

    More perk card styles

--- src/components/content-types/perk/perkCardView.js
+++ src/components/content-types/perk/perkCardView.js
@@ -24,7 +24,7 @@ const PerkCardView = (props) => {
 
   return (
     <SbEditable content={props.blok}>
-      <article className='perk-card su-relative su-max-w-500 su-border su-border-solid su-border-black-90'>
+      <article className='perk-card su-relative su-basefont-23 su-max-w-400 su-border su-border-solid su-border-black-90'>
         <div className='perk-card-image-wrapper su-relative'>
           {filename?.startsWith('http') &&
             <figure className='su-overflow-hidden su-w-full su-h-full'>
@@ -36,14 +36,14 @@ const PerkCardView = (props) => {
           }
           <div className={`su-absolute su-block su-w-full su-h-full su-top-0 su-bg-gradient-to-b su-from-transparent su-to-saa-black`} aria-hidden='true' />
         </div>
-        <div className='perk-card-content su-relative su--mt-[1em]'>
+        <div className='perk-card-content su-relative su-mt-[-2em] su-rs-px-2 su-rs-pb-3'>
           <SbLink
             link={perkPageLink}
-            classes='su-text-white hocus:su-text-white su-no-underline hocus:su-underline su-custom-underline'
+            classes='su-text-white hocus:su-text-white su-no-underline hocus:su-underline su-underline-custom !su-underline-digital-red-xlight'
           >
-            <Heading level={headingLevel ?? 3} font='serif' size={2}>{title}</Heading>
+            <Heading level={headingLevel ?? 3} font='serif' size={1} tracking='normal'>{title}</Heading>
           </SbLink>
-          <p className='su-text-black-20 su-card-paragraph'>{intro}</p>
+          <p className='su-text-black-20 su-card-paragraph su-mb-0'>{intro}</p>
         </div>
       </article>
     </SbEditable>
