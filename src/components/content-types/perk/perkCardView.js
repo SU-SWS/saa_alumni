6aed6e9e055564404d26881a18f5ed115b9b62bf
Yvonne Tang <ytang2@stanford.edu>
2021-05-08 11:21:53 -0700

    Test fake category link on top of whole card which is a link

--- src/components/content-types/perk/perkCardView.js
+++ src/components/content-types/perk/perkCardView.js
@@ -5,6 +5,7 @@ import CardImage from '../../media/cardImage';
 import { Heading, SrOnlyText } from 'decanter-react';
 
 const PerkCardView = (props) => {
+
   // Destructure props
   const {
     blok: {
@@ -22,8 +23,10 @@ const PerkCardView = (props) => {
     storyLink
   } = props;
 
+  // Default link is the internal link of the perk content page
   let perkPageLink = { linktype: 'story', url: storyLink + '/' };
 
+  // Link to external URL instead if it is provided
   if (externalUrl) {
     perkPageLink = { linktype: 'url', url: externalUrl };
   }
@@ -40,7 +43,7 @@ const PerkCardView = (props) => {
 
   return (
     <SbEditable content={blok}>
-      <article className='perk-card su-relative su-break-words su-basefont-23 su-max-w-500 su-border su-border-solid su-border-black-90'>
+      <article className='perk-card su-relative su-rs-pb-3 su-break-words su-basefont-23 su-max-w-500 su-border su-border-solid su-border-black-90'>
         <div className='perk-card-image-wrapper su-relative su-aspect-w-4 su-aspect-h-3'>
           {filename?.startsWith('http') &&
             <figure className='su-overflow-hidden su-w-full su-h-full'>
@@ -54,14 +57,15 @@ const PerkCardView = (props) => {
           }
           <div className={`su-absolute su-block su-w-full su-h-full su-top-0 su-bg-gradient-to-b su-from-transparent su-to-saa-black`} aria-hidden='true' />
         </div>
+        {newTab}
         <SbLink
           link={perkPageLink}
           classes='su-stretched-link su-text-white hocus:su-text-white su-no-underline hocus:su-underline su-underline-custom !su-underline-digital-red-xlight'
         >
           <Heading level={headingLevel ?? 3} font='serif' size={1} tracking='normal' className='su-relative su-mt-[-3em] su-rs-px-2'>{title}</Heading>
         </SbLink>
-        {newTab}
-        <p className='su-relative su-text-black-20 su-card-paragraph su-mb-0 su-rs-px-2 su-rs-pb-3'>{intro}</p>
+        <p className='su-relative su-text-black-20 su-card-paragraph su-mb-0 su-rs-px-2'>{intro}</p>
+        <a href='https://google.com' className='su-relative su-inline-block su-z-10 su-rs-mt-0 su-rs-ml-2 su-no-underline hocus:su-underline su-text--m1'>Test link</a>
       </article>
     </SbEditable>
   );
