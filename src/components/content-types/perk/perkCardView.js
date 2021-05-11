91f346e19bc1db9d8812be02255bf64b8b9ec7a4
Yvonne Tang <ytang2@stanford.edu>
2021-05-10 18:48:04 -0700

    fixup and documentation

--- src/components/content-types/perk/perkCardView.js
+++ src/components/content-types/perk/perkCardView.js
@@ -41,20 +41,21 @@ const PerkCardView = (props) => {
     perkType = "Member benefit";
   }
 
-  const perkTypeLink = perkTypeLinks[type] ?? perkTypeLinks["perk"];
+  // Check dataSource.js to see if there is a link provided for the perk type taxonomy
+  const perkTypeLink = perkTypeLinks[type] ?? "";
 
-  // Perk type taxonomy tag is just text if no link object is provided in dataSource.js
+  // Perk type taxonomy tag is just plain text if no link object is provided in dataSource.js
   let perkTypeTag =
-    <p className='su-relative su-inline-block su-w-fit su-mt-auto su-mb-0 su-text-digital-red-xlight su-rs-mt-0 su-rs-ml-2 su-text-17 md:su-text-19 xl:su-text-20 su-font-regular'>
+    <p className="su-relative su-inline-block su-w-fit su-mt-auto su-mb-0 su-text-digital-red-xlight su-rs-mt-0 su-rs-ml-2 su-text-17 md:su-text-19 xl:su-text-20 su-font-regular">
       {perkType}
     </p>
 
-  // If link object is provided for the perk type, use the SbLink component instead
+  // If link object is provided for the perk type, use the SbLink component instead plus extra hover/focus classes for style
   if (perkTypeLink !== "") {
     perkTypeTag =
       <SbLink
         link={perkTypeLink}
-        classes='su-relative su-inline-block su-w-fit su-mt-auto su-mb-0 su-text-digital-red-xlight hocus:su-text-black-20 su-z-10 su-rs-mt-0 su-rs-ml-2 su-no-underline hocus:su-underline su-text-17 md:su-text-19 xl:su-text-20 su-font-regular su-transition-colors su-underline-custom'
+        classes="su-relative su-inline-block su-w-fit su-mt-auto su-mb-0 su-text-digital-red-xlight hocus:su-text-black-20 su-z-10 su-rs-mt-0 su-rs-ml-2 su-no-underline hocus:su-underline su-text-17 md:su-text-19 xl:su-text-20 su-font-regular su-transition-colors su-underline-custom"
       >
         {perkType}
       </SbLink>
@@ -62,34 +63,34 @@ const PerkCardView = (props) => {
 
   return (
     <SbEditable content={blok}>
-      <article className='su-group perk-card su-relative su-flex su-flex-col su-bg-saa-black su-rs-pb-3 su-break-words su-basefont-23 su-max-w-500 su-border su-border-solid su-border-black'>
-        <div className='perk-card-image-wrapper su-relative su-mb-[-3em] su-aspect-w-4 su-aspect-h-3'>
+      <article className="su-group perk-card su-relative su-flex su-flex-col su-bg-saa-black su-rs-pb-3 su-break-words su-basefont-23 su-max-w-500 su-border su-border-solid su-border-black">
+        <div className="perk-card-image-wrapper su-relative su-mb-[-3em] su-aspect-w-4 su-aspect-h-3">
           {filename?.startsWith("http") &&
-          <figure className='su-overflow-hidden su-w-full su-h-full'>
+          <figure className="su-overflow-hidden su-w-full su-h-full">
             <CardImage
               filename={filename}
               imageFocus={imageFocus}
-              className='su-w-full su-h-full su-object-cover su-transition-transform su-transform-gpu group-hover:su-scale-[1.03]'
-              loading='lazy'
+              className="su-w-full su-h-full su-object-cover su-transition-transform su-transform-gpu group-hover:su-scale-[1.03]"
+              loading="lazy"
             />
           </figure>
           }
-          <div className={`su-absolute su-block su-w-full su-h-full su-top-0 su-left-0 su-bg-gradient-to-b su-from-transparent su-to-saa-black`} aria-hidden='true'/>
+          <div className="su-absolute su-block su-w-full su-h-full su-top-0 su-left-0 su-bg-gradient-to-b su-from-transparent su-to-saa-black" aria-hidden='true'/>
         </div>
         <SbLink
           link={perkPageLink}
-          classes='su-group su-stretched-link su-mb-08em su-rs-px-2 su-text-white hocus:su-text-white su-no-underline hocus:su-underline su-underline-custom !su-underline-digital-red-xlight'
+          classes="su-group su-stretched-link su-mb-08em su-rs-px-2 su-text-white hocus:su-text-white su-no-underline hocus:su-underline su-underline-custom !su-underline-digital-red-xlight"
         >
-          <Heading level={headingLevel ?? 3} font='serif' size={1} tracking='normal' className='su-relative su-inline'>{title}</Heading>
+          <Heading level={headingLevel ?? 3} font="serif" size={1} tracking="normal" className="su-relative su-inline">{title}</Heading>
           <ArrowRightIcon
-            className='su-relative su-transition su-transform-gpu group-hocus:su-translate-x-02em su-inline-block su-ml-03em su--mt-03em su-text-digital-red-xlight su-w-1em group-hocus:su-text-white'
-            aria-hidden='true'
+            className="su-relative su-transition su-transform-gpu group-hocus:su-translate-x-02em su-inline-block su-ml-03em su--mt-03em su-text-digital-red-xlight su-w-1em group-hocus:su-text-white"
+            aria-hidden="true"
           />
         </SbLink>
         {isNew &&
-        <TabLabel text='New' srText={perkType}/>
+        <TabLabel text="New" srText={perkType}/>
         }
-        <p className='su-relative su-text-black-20 su-card-paragraph su-rs-px-2'>{intro}</p>
+        <p className="su-relative su-text-black-20 su-card-paragraph su-rs-px-2">{intro}</p>
         {perkTypeTag}
       </article>
     </SbEditable>
