4aee3b9a8bf9d6ea6c4bc6246077c029489b0e50
Yvonne Tang <ytang2@stanford.edu>
2021-05-20 18:48:11 -0700

    eslint

--- src/components/identity/masthead.js
+++ src/components/identity/masthead.js
@@ -12,10 +12,7 @@ const Masthead = ({ blok: { mainNav, utilityNav }, blok }) => (
     >
       <FlexBox direction="row">
         <FlexCell className="su-bg-gradient-to-b su-from-digital-red su-to-cardinal-red su-cc xl:su-pr-61 2xl:su-pr-90 su-rs-pt-5 su-rs-pb-3 su-ml-0">
-          <Link
-            to="/"
-            className="su-block su-w-fit"
-          >
+          <Link to="/" className="su-block su-w-fit">
             <img
               src="/images/saa-logo-white.svg"
               className="su-w-[18rem] xl:su-w-[25rem] 2xl:su-w-[28rem]"
@@ -23,7 +20,10 @@ const Masthead = ({ blok: { mainNav, utilityNav }, blok }) => (
             />
           </Link>
         </FlexCell>
-        <FlexCell grow={true} className="su-bg-gradient-to-b su-from-masthead-black-top su-to-masthead-black-bottom">
+        <FlexCell
+          grow
+          className="su-bg-gradient-to-b su-from-masthead-black-top su-to-masthead-black-bottom"
+        >
           <CreateBloks blokSection={utilityNav} />
           <CreateBloks blokSection={mainNav} />
         </FlexCell>
