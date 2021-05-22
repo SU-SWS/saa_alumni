20905511a3d65235685a094355e4c200db9efc2c
Yvonne Tang <ytang2@stanford.edu>
2021-05-21 19:37:52 -0700

    Placeholder for search; adjust logo size for XL breakpoint

--- src/components/identity/masthead.js
+++ src/components/identity/masthead.js
@@ -11,11 +11,11 @@ const Masthead = ({ blok: { mainNav, utilityNav }, blok }) => (
       width="full"
     >
       <FlexBox direction="row">
-        <FlexCell className="su-bg-gradient-to-b su-from-digital-red su-to-cardinal-red su-cc xl:su-pr-61 2xl:su-pr-90 su-rs-pt-5 su-rs-pb-3 su-ml-0">
+        <FlexCell className="su-bg-gradient-to-b su-from-digital-red su-to-cardinal-red su-cc xl:su-pr-38 2xl:su-pr-90 su-rs-pt-5 su-rs-pb-3 su-ml-0">
           <Link to="/" className="su-block su-w-fit">
             <img
               src="/images/saa-logo-white.svg"
-              className="su-w-[18rem] xl:su-w-[25rem] 2xl:su-w-[28rem]"
+              className="su-w-[18rem] xl:su-w-[21rem] 2xl:su-w-[28rem]"
               alt="Stanford Alumni Association"
             />
           </Link>
@@ -25,9 +25,14 @@ const Masthead = ({ blok: { mainNav, utilityNav }, blok }) => (
           className="su-cc su-mr-0 su-bg-gradient-to-b su-from-masthead-black-top su-to-masthead-black-bottom su-backface-hidden"
         >
           <nav aria-label="Utility Menu" className="su-rs-mt-0 su-text-right">
-            <ul className="su-list-unstyled su-link-white su-link-no-underline su-link-regular su-text-18 children:su-inline-block children:su-mr-[2em]">
+            <ul className="su-list-unstyled su-inline-block su-link-white su-link-no-underline su-link-regular su-text-18 children:su-inline-block children:su-mr-[2em]">
               <CreateBloks blokSection={utilityNav} />
             </ul>
+            <form className='su-inline-block'>
+              <label className="su-sr-only">Search Stanford Alumni</label>
+              <input type="text" className="su-inline-block su-site-search__input" placeholder="Search" />
+              <button value="Search" type="submit" name="submit" className="su-sr-only" aria-label="Search">Submit Search</button>
+            </form>
           </nav>
           <CreateBloks blokSection={mainNav} />
         </FlexCell>
