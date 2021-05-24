6857bf07d5551d6ca9f829ceb866a050168f09dc
Yvonne Tang <ytang2@stanford.edu>
2021-05-24 11:53:32 -0700

    focus and transition

--- src/components/navigation/mainMenuGroup.js
+++ src/components/navigation/mainMenuGroup.js
@@ -4,16 +4,16 @@ import CreateBloks from "../../utilities/createBloks";
 
 const MainMenuGroup = ({ blok: { parentText, menuItems } }) => (
   <Popover as="li" className="focus-within:su-bg-cardinal-red">
-    <Popover.Button className="su-font-bold su-bg-transparent su-pb-[4.7rem] su-border-solid su-border-b-[5px] su-border-transparent hover:su-border-digital-red-xlight hover:su-text-digital-red-xlight hocus:su-no-underline focus:su-bg-cardinal-red focus:su-text-white hocus:su-bg-transparent">
+    <Popover.Button className="su-font-bold su-bg-transparent su-pb-[4.7rem] su-border-solid su-border-b-[5px] su-border-transparent hover:su-border-digital-red-xlight hover:su-text-digital-red-xlight hocus:su-no-underline focus:su-bg-cardinal-red focus:su-text-white hover:su-bg-transparent focus:su-outline-none">
       {parentText}
     </Popover.Button>
     <Transition
       enter="su-transition su-ease-out"
-      enterFrom="su-transform-gpu su-scale-95 su-opacity-0"
+      enterFrom="su-transform-gpu su-scale-75 su-opacity-0"
       enterTo="su-transform-gpu su-scale-100 su-opacity-100"
-      leave="su-transition su-ease-out"
+      leave="su-transition su-ease-out su-duration-75"
       leaveFrom="su-transform-gpu su-scale-100 su-opacity-100"
-      leaveTo="su-transform-gpu su-scale-95 su-opacity-0"
+      leaveTo="su-transform-gpu su-scale-75 su-opacity-0"
     >
       <Popover.Panel
         as='ul'
