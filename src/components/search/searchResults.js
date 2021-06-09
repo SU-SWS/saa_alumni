195167df959162ddc4fd0ab5b7add1361daae1a4
Nathan Plowman <nathan.plowman@ffwagency.com>
2021-06-09 11:24:13 -0600

    ADAPT-2670: Work in progress for Search Results component.

---
+++ src/components/search/searchResults.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+const SearchResults = ({results}) => {
+  if (!results.hits) {
+    return (
+      <div></div>
+    )
+  }
+
+  return (
+    <div>
+      <div><strong>{results.nbHits}</strong> results:</div>
+      {results.hits.map((result) => {
+        return (
+          <div className="su-py-30 su-px-38 su-border-b-[1px] su-border-black-20">  
+            <div className="su-flex">
+              <div className="su-flex-1"> 
+                <div className="su-text-16 su-mb-10">{result.domain}</div>
+                <h3 className='su-text-24'><a className="su-font-serif su-text-digital-red-light" href={result.url}>{result.title}</a></h3>
+                <div dangerouslySetInnerHTML={{__html: result._snippetResult.body.value}}></div>
+              </div>
+              {result.image &&
+              <div className="su-w-[225px] su-h-[150px] su-ml-30">
+                <img className="su-block su-object-cover su-object-center su-h-full" src={result.image} alt={`Thumbnail image for ${result.title}`} />
+              </div>
+              }
+            </div>
+          </div>
+        )
+      })}
+    </div>
+  )   
+}
+export default SearchResults
