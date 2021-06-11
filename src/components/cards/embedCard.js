5dd812002c8f9f569c5d6fd94819593d5518a522
rebeccahongsf <rebeccahongsf@gmail.com>
2021-06-10 17:50:15 -0700

    ADAPT-2437 Updated uniqueId package and implement loader

--- src/components/cards/embedCard.js
+++ src/components/cards/embedCard.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable no-restricted-syntax */
 /* eslint-disable react/no-danger */
 /* eslint-disable no-undef */
@@ -10,36 +11,60 @@
  * is also not good practice to inject and manipulate the page outside of
  * REACT as that can lead to irregularities and troubles.
  */
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import SbEditable from "storyblok-react";
 import postscribe from "postscribe";
-import { v4 as uuidv4 } from "uuid";
+import nextId from "react-id-generator";
+import Loader from "react-loader-spinner";
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 
 const EmbedCard = ({ blok: { embed: html }, blok }) => {
   const myEmbed = useRef(null);
-  const uniqueId = uuidv4();
+  const uniqueId = nextId("su-alumni-");
+  const [scriptLoaded, setScriptLoaded] = useState(false);
 
   useEffect(() => {
     if (!html) return;
 
     if (html.includes("script")) {
-      postscribe(`#${uniqueId}`, html);
+      postscribe(`#${uniqueId}`, html, {
+        done: () => {
+          setScriptLoaded(true);
+        },
+      });
     } else {
       // Create a 'tiny' document and parse the html string.
       // https://developer.mozilla.org/en-US/docs/Web/API/DocumentFragment
       const miniDom = document.createRange().createContextualFragment(html);
-
       // Clear the container.
       myEmbed.current.innerHTML = "";
-
       // Append the new content.
       myEmbed.current.appendChild(miniDom);
     }
-  }, [html, uniqueId]);
+  }, []);
 
   return (
     <SbEditable content={blok}>
-      {html.includes("script") ? <div id={uniqueId} /> : <div ref={myEmbed} />}
+      {html.includes("script") ? (
+        <>
+          <div id={uniqueId} />
+          {!scriptLoaded && (
+            <>
+              <Loader
+                type="TailSpin"
+                color="#00BFFF"
+                height={50}
+                width={50}
+                timeout={30000}
+                aria-busy="true"
+              />
+              <p>Loading...</p>
+            </>
+          )}
+        </>
+      ) : (
+        <div ref={myEmbed} />
+      )}
     </SbEditable>
   );
 };
