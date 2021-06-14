82c53de3e65d892e8fcfb3fcfc9702c2eab762cf
Yvonne Tang <ytang2@stanford.edu>
2021-06-14 00:23:21 -0700

    Fixup basic page content spacing and margin

--- src/components/partials/basicContentNoSidebar.js
+++ src/components/partials/basicContentNoSidebar.js
@@ -1,23 +1,35 @@
 import React from "react";
 import { dcnb } from "cnbuilder";
+import { render } from "storyblok-rich-text-react-renderer";
 import CreateBloks from "../../utilities/createBloks";
 import RichTextRenderer from "../../utilities/richTextRenderer";
 import WidthBox from "../layout/widthBox";
+import getNumBloks from "../../utilities/getNumBloks";
 
-const BasicContentNoSidebar = ({ blok: { content, intro }, className }) => (
-  <WidthBox
-    width="8"
-    className={dcnb(
-      "basic-page-main-content su-basefont-23 su-mx-auto",
-      className
-    )}
-  >
-    <RichTextRenderer
-      wysiwyg={intro}
-      className="su-text-m1 xl:su-text-m2 su-rs-mb-3"
-    />
-    <CreateBloks blokSection={content} />
-  </WidthBox>
-);
+const BasicContentNoSidebar = ({ blok: { content, intro }, className }) => {
+  const renderedIntro = render(intro);
+  const hasIntro = getNumBloks(renderedIntro) > 0;
+  const hasContent = getNumBloks(content) > 0;
+
+  if (!hasIntro && !hasContent) {
+    return null;
+  }
+
+  return (
+    <WidthBox
+      width="8"
+      className={dcnb(
+        "basic-page-main-content su-basefont-23 su-mx-auto",
+        className
+      )}
+    >
+      <RichTextRenderer
+        wysiwyg={intro}
+        className="su-text-m1 xl:su-text-m2 su-rs-mb-3"
+      />
+      <CreateBloks blokSection={content} />
+    </WidthBox>
+  );
+};
 
 export default BasicContentNoSidebar;
