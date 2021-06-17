e1ab399ca20f75b39d1a16cf119f3a483b44e93c
romakravchuk <roman4ik@gmail.com>
2021-06-17 17:34:28 +0300

    ADAPT-2777 add dismiss button functionality

--- src/components/composite/alert.js
+++ src/components/composite/alert.js
@@ -1,21 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import SbEditable from "storyblok-react";
-import { Alert as DecanterAlert } from "decanter-react";
+import { Alert as DecanterAlert, DismissButton } from "decanter-react";
 import RichTextRenderer from "../../utilities/richTextRenderer";
 import CreateBloks from "../../utilities/createBloks";
 import "../../styles/alert.css";
 
-const Alert = ({ blok }) => {
-  const { type, alertCta, label, heading, body } = blok;
+const Alert = ({
+  blok: { type, alertCta, label, heading, body, hasDismiss },
+  blok,
+}) => {
+  const [isAlertDismissed, setIsAlertDismissed] = useState(true);
   const isLinkDark = type === "warning";
   const footerContent = (
     <CreateBloks blokSection={alertCta} isLinkDark={isLinkDark} />
+  );
+
+  useEffect(() => {
+    const isDismissed = sessionStorage.getItem(blok._uid);
+    if (isDismissed) setIsAlertDismissed(false);
+  }, []);
+
+  const dismissHandler = () => {
+    sessionStorage.setItem(blok._uid, "dismissed");
+    setIsAlertDismissed(false);
+  };
+
+  const DismissBtn = (
+    <DismissButton
+      iconProps={{ className: "su-ml-02em" }}
+      text="Dismiss"
+      srText="alert"
+      onClick={dismissHandler}
+      color={isLinkDark ? "black" : "white"}
+      className="su-text-17 su-uppercase su-font-bold su-inline-block su-tracking-widest su-mr-0 su-ml-auto"
+    />
   );
 
   const customStyles = {
     footerWrapper: "su-rs-mt-1",
     body: "su-alert-body-link-light",
   };
+
+  if (!isAlertDismissed) return null;
 
   return (
     <SbEditable content={blok}>
@@ -25,12 +51,12 @@ const Alert = ({ blok }) => {
         heading={heading}
         footer={footerContent}
         classes={customStyles}
-        hasDismiss
+        dismissBtn={DismissBtn}
+        hasDismiss={hasDismiss}
       >
         <RichTextRenderer wysiwyg={body} />
       </DecanterAlert>
     </SbEditable>
   );
 };
-
 export default Alert;
