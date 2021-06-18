b237685baf86415d7bcc60623ba30f745b5a3528
Shea McKinney <sherakama@gmail.com>
2021-06-18 11:30:02 -0700

    fixup! Get all the alerts.

--- src/components/composite/globalAlert.js
+++ src/components/composite/globalAlert.js
@@ -4,17 +4,23 @@ import Alert from "./alert";
 
 const query = graphql`
   query {
-    storyblokEntry(field_component: { eq: "alert" }) {
-      id
-      name
-      created_at
-      uuid
-      slug
-      full_slug
-      content
-      is_startpage
-      parent_id
-      group_id
+    allStoryblokEntry(
+      filter: { field_component: { eq: "alert" } }
+      sort: { fields: published_at, order: DESC }
+    ) {
+      edges {
+        node {
+          content
+          uuid
+          id
+          slug
+          name
+          field_component
+          field_type_string
+          field_heading_string
+          field_label_string
+        }
+      }
     }
   }
 `;
@@ -22,9 +28,19 @@ const query = graphql`
 const GlobalAlert = () => (
   <StaticQuery
     query={query}
-    render={({ storyblokEntry }) => {
-      if (!storyblokEntry) return null;
-      return <Alert blok={JSON.parse(storyblokEntry.content)} />;
+    render={({ allStoryblokEntry }) => {
+      if (!allStoryblokEntry?.edges.length) return null;
+
+      return (
+        <>
+          {allStoryblokEntry.edges.map(({ node: { content, uuid } }) => {
+            const blok = JSON.parse(content);
+            // eslint-disable-next-line dot-notation
+            blok["_uid"] = uuid;
+            return <Alert blok={blok} />;
+          })}
+        </>
+      );
     }}
   />
 );
