b0c3806eed075669d7e825217d8caf00cccc6c0e
Yvonne Tang <ytang2@stanford.edu>
2021-06-19 11:10:13 -0700

    Change my mind - having property on gradient overlay alone seems to do the job

--- src/components/content-types/perk/perkCardView.js
+++ src/components/content-types/perk/perkCardView.js
@@ -81,7 +81,7 @@ const PerkCardView = ({
             <CardImage
               filename={filename}
               imageFocus={cardImageFocus || imageFocus}
-              className="su-w-full su-h-full su-transition-transform su-transform-gpu group-hover:su-scale-[1.03] su-backface-hidden"
+              className="su-w-full su-h-full su-transition-transform su-transform-gpu group-hover:su-scale-[1.03]"
               loading={orientation === "horizontal" ? "eager" : "lazy"}
               size={orientation === "horizontal" ? "horizontal" : "vertical"}
             />
