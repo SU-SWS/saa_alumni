138c96b6fda69ab74d110dc18a356e68bb7b6433
Yvonne Tang <ytang2@stanford.edu>
2021-07-01 11:30:41 -0700

    Fill in small gap using same bg color as stroke in circularImage between stroke and image

--- src/components/media/circularImage.js
+++ src/components/media/circularImage.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { dcnb } from "cnbuilder";
-import { borderColors } from "../../utilities/dataSource";
+import { borderColors, bgColors } from "../../utilities/dataSource";
 import CardImage from "./cardImage";
 
 const CircularImage = ({
@@ -15,12 +15,14 @@ const CircularImage = ({
   // Option to display image as round thumbnail with colored border
   const imageBorderColor =
     borderColors[borderColor] ?? borderColors["digital-red"];
+  const imageBgColor = bgColors[borderColor ?? "digital-red"];
 
   return (
     <div
       className={dcnb(
         "su-w-[14rem] su-h-[14rem] su-rounded-full su-border-[7px] su-border-solid su-overflow-hidden",
         imageBorderColor,
+        imageBgColor,
         className
       )}
       aria-hidden={alt ? "false" : "true"}
