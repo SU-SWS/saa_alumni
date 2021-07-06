283943f8c955556bb9dd36881f18c0956cd445fe
Yvonne Tang <ytang2@stanford.edu>
2021-07-06 16:03:37 -0700

    Add hero image width option

--- src/components/content-types/story/storyPageView.js
+++ src/components/content-types/story/storyPageView.js
@@ -7,6 +7,7 @@ import FullWidthImage from "../../media/fullWidthImage";
 import getNumBloks from "../../../utilities/getNumBloks";
 import RichTextRenderer from "../../../utilities/richTextRenderer";
 import WidthBox from "../../layout/widthBox";
+import CardImage from "../../media/cardImage";
 
 const StoryPageView = (props) => {
   // Destructure props
@@ -14,6 +15,7 @@ const StoryPageView = (props) => {
     blok: {
       image: { filename, alt } = {},
       imageFocus,
+      notFullwidthImage,
       caption,
       title,
       intro,
@@ -27,6 +29,51 @@ const StoryPageView = (props) => {
   } = props;
 
   const numBelow = getNumBloks(belowContent);
+
+  let heroImage;
+
+  // Default hero image is edge to edge
+  if (filename) {
+    heroImage = (
+      <figure className="su-rs-mb-6">
+        <div className="su-relative su-overflow-hidden su-w-full md:su-h-[50vw] 2xl:su-h-900">
+          <FullWidthImage
+            filename={filename}
+            imageFocus={imageFocus}
+            alt={alt ?? ""}
+            className="su-w-full su-h-full su-object-cover"
+            loading="eager"
+          />
+        </div>
+        {caption && (
+          <figcaption className="su-cc">
+            <RichTextRenderer
+              wysiwyg={caption}
+              className="su-caption su-max-w-prose su-mt-06em su-mr-0 su-ml-auto su-text-right children:su-leading-snug"
+            />
+          </figcaption>
+        )}
+      </figure>
+    );
+  }
+
+  if (notFullwidthImage && filename) {
+    heroImage = (
+      <WidthBox width="10" className="su-rs-mb-6">
+        <figure>
+          <CardImage filename={filename} alt={alt ?? ""} loading="eager" />
+          {caption && (
+            <figcaption>
+              <RichTextRenderer
+                wysiwyg={caption}
+                className="su-caption su-max-w-prose su-mt-06em su-mr-0 su-ml-auto su-text-right children:su-leading-snug"
+              />
+            </figcaption>
+          )}
+        </figure>
+      </WidthBox>
+    );
+  }
 
   return (
     <SbEditable content={blok}>
@@ -54,27 +101,7 @@ const StoryPageView = (props) => {
                   </p>
                 )}
               </Container>
-              {filename?.startsWith("http") && (
-                <figure className="su-rs-mb-5">
-                  <div className="su-relative su-overflow-hidden su-w-full md:su-h-500 lg:su-h-700 xl:su-h-900">
-                    <FullWidthImage
-                      filename={filename}
-                      imageFocus={imageFocus}
-                      alt={alt ?? ""}
-                      className="su-w-full su-h-full su-object-cover"
-                      loading="eager"
-                    />
-                  </div>
-                  {caption && (
-                    <figcaption className="su-cc">
-                      <RichTextRenderer
-                        wysiwyg={caption}
-                        className="su-caption su-max-w-prose su-mt-06em su-mr-0 su-ml-auto su-text-right children:su-leading-snug"
-                      />
-                    </figcaption>
-                  )}
-                </figure>
-              )}
+              {heroImage}
               {author && (
                 <WidthBox width="8">
                   <p className="su-mb-0 su-font-serif">
