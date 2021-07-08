9b9f0f075e26bf1e4350d1abda040d5850d2f869
Yvonne Tang <ytang2@stanford.edu>
2021-07-08 12:57:06 -0700

    mobile menu styles

--- src/components/navigation/mainNav.js
+++ src/components/navigation/mainNav.js
@@ -18,6 +18,7 @@ const MainNav = ({ blok: { mainMenuGroups }, blok, className }) => {
       <nav
         className={dcnb("main-nav-desktop su-hidden lg:su-block", className)}
         aria-label="Main Menu"
+
       >
         <ul className="su-hidden lg:su-flex su-flex-col lg:su-ml-auto lg:su-flex-row lg:su-items-end su-list-unstyled children:su-mb-0">
           <CreateBloks blokSection={mainMenuGroups} />
@@ -27,16 +28,21 @@ const MainNav = ({ blok: { mainMenuGroups }, blok, className }) => {
         <Button
           variant="unset"
           size="minimal"
-          className="su-flex su-flex-col su-w-40 su-items-center su-rs-ml-0 su-text-14 su-font-semibold lg:su-hidden"
+          className="su-group su-flex su-flex-col su-w-40 su-items-center su-rs-ml-0 su-text-14 su-font-semibold lg:su-hidden"
           onClick={toggleMenu}
           aria-expanded={menuOpened}
           aria-label={menuOpened ? "Close Menu" : "Open Menu"}
         >
-          <MenuIcon aria-hidden="true" className="su-w-[2.4rem]" />
+          <MenuIcon
+            aria-hidden="true"
+            className="su-transition-colors su-w-[2.4rem] group-hocus:su-text-digital-red-xlight"
+          />
           {menuOpened ? "Close" : "Menu"}
         </Button>
         <ul
-          className={`${menuOpened ? "" : "su-hidden "}su-absolute su-right-0 su-top-[107px] md:su-top-[119px] su-cc su-w-full su-bg-cardinal-red-xdark su-border-t su-border-solid su-border-digital-red-xlight su-flex su-flex-col su-list-unstyled children:su-mb-0`}
+          className={`${
+            menuOpened ? "" : "su-hidden "
+          }su-absolute su-right-0 su-top-[107px] md:su-top-[119px] su-cc su-w-full su-bg-cardinal-red-xdark su-shadow-lg su-border-t su-border-solid su-border-digital-red-xlight su-flex su-flex-col su-list-unstyled children:su-mb-0`}
           aria-hidden={!menuOpened}
         >
           <CreateBloks blokSection={mainMenuGroups} />
