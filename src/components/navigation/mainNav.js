842bd8bfb2a89ee90f7dcd6481e44e10b1e35658
Yvonne Tang <ytang2@stanford.edu>
2021-07-07 17:00:16 -0700

    Removing HeadlessUI for hamburger for now

--- src/components/navigation/mainNav.js
+++ src/components/navigation/mainNav.js
@@ -3,24 +3,32 @@ import SbEditable from "storyblok-react";
 import { dcnb } from "cnbuilder";
 import { MenuIcon } from "@heroicons/react/outline";
 import CreateBloks from "../../utilities/createBloks";
-import { Popover, Transition } from "@headlessui/react";
+import { Button } from "decanter-react";
 
 const MainNav = ({ blok: { mainMenuGroups }, blok, className }) => (
   <SbEditable content={blok}>
-    <nav className={dcnb("main-nav-desktop su-hidden lg:su-block", className)} aria-label="Main Menu">
+    <nav
+      className={dcnb("main-nav-desktop su-hidden lg:su-block", className)}
+      aria-label="Main Menu"
+    >
       <ul className="su-hidden lg:su-flex su-flex-col lg:su-ml-auto lg:su-flex-row lg:su-items-end su-list-unstyled children:su-mb-0">
         <CreateBloks blokSection={mainMenuGroups} />
       </ul>
     </nav>
-    <Popover as="nav" className={dcnb("main-nav-mobile lg:!su-hidden", className)} aria-label="Main Menu">
-      <Popover.Button className="su-flex su-flex-col su-items-center su-rs-ml-0 su-text-14 su-font-semibold lg:su-hidden">
-          <MenuIcon aria-hidden="true" className="su-w-[2.4rem]" />
-          Menu
-      </Popover.Button>
-      <Popover.Panel as="ul" className="su-absolute su-right-0 su-cc su-w-full su-bg-cardinal-red-xdark su-border-t su-border-solid su-border-digital-red-xlight su-flex su-flex-col su-list-unstyled children:su-mb-0">
+    <nav
+      className={dcnb("main-nav-mobile lg:!su-hidden", className)}
+      aria-label="Main Menu"
+    >
+      <Button variant="unset" size="minimal" className="su-flex su-flex-col su-items-center su-rs-ml-0 su-text-14 su-font-semibold lg:su-hidden">
+        <MenuIcon aria-hidden="true" className="su-w-[2.4rem]" />
+        Menu
+      </Button>
+      <ul
+        className="su-absolute su-right-0 su-cc su-w-full su-bg-cardinal-red-xdark su-border-t su-border-solid su-border-digital-red-xlight su-flex su-flex-col su-list-unstyled children:su-mb-0"
+      >
         <CreateBloks blokSection={mainMenuGroups} />
-      </Popover.Panel>
-    </Popover>
+      </ul>
+    </nav>
   </SbEditable>
 );
 
