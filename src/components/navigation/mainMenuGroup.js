686c30cfa5daefefc3ac0bccef7c0376ca601e69
Yvonne Tang <ytang2@stanford.edu>
2021-07-09 11:15:15 -0700

    Mobile main nav styles

--- src/components/navigation/mainMenuGroup.js
+++ src/components/navigation/mainMenuGroup.js
@@ -6,13 +6,15 @@ import CreateBloks from "../../utilities/createBloks";
 const MainMenuGroup = ({
   blok: { parentText, parentTextSecond, menuItems, panelFacing },
 }) => {
+  // Styles for 1st level parent item buttons
   const buttonMobile =
-    "su-w-full hocus:su-bg-cardinal-red-xxdark hocus:su-shadow-none su-py-20";
+    "su-flex su-items-center su-w-full hocus:su-bg-cardinal-red-xxdark hocus:su-shadow-none su-py-20";
   const buttonDesktop =
-    "su-px-15 xl:su-pt-20 lg:su-pb-18 xl:su-pb-[3rem] lg:hocus:su-bg-transparent lg:su-whitespace-pre lg:hocus:su-text-digital-red-light lg:hocus:su-no-underline lg:su-border-b-[5px] lg:su-border-solid lg:su-border-transparent lg:hocus:su-border-digital-red-xlight";
+    "lg:su-items-end lg:su-px-15 xl:su-pt-20 lg:su-pb-18 xl:su-pb-[3rem] lg:hocus:su-bg-transparent lg:su-whitespace-pre lg:hocus:su-text-digital-red-light lg:hocus:su-no-underline lg:su-border-b-[5px] lg:su-border-solid lg:su-border-transparent lg:hocus:su-border-digital-red-xlight";
 
+  // Styles for the down chevron
   const chevronMobile =
-    "su-absolute su-right-0 su-w-30 su-bg-digital-red su-rounded-full group-hocus:!su-bg-digital-red-xlight su-mr-15";
+    "su-absolute su-right-0 su-w-30 su-bg-digital-red su-rounded-full group-hocus:!su-bg-digital-red-light su-mr-15";
   const chevronDesktop =
     "lg:su-relative lg:su-mr-0 lg:su-w-[1.2em] lg:su-bg-transparent lg:group-hocus:su-text-digital-red-xlight lg:group-hocus:!su-bg-transparent";
 
@@ -26,7 +28,7 @@ const MainMenuGroup = ({
           <Popover.Button
             className={`${
               open
-                ? "!su-text-white !su-bg-cardinal-red-xdark !su-border-cardinal-red-xdark"
+                ? "!su-text-white !su-bg-cardinal-red-xxdark lg:!su-bg-cardinal-red-xdark !su-border-cardinal-red-xdark"
                 : ""
             } su-group ${buttonMobile} ${buttonDesktop} su-font-bold su-text-left su-leading-snug su-bg-transparent focus:su-outline-none`}
           >
@@ -38,8 +40,9 @@ const MainMenuGroup = ({
               </>
             )}
             <ChevronDownIcon
-              className={`${open ? "su-transform-gpu su-rotate-180" : ""}
-                       ${chevronMobile} ${chevronDesktop} su-inline-block su-text-white su-transition`}
+              className={`su-inline-block su-text-white su-transition ${
+                open ? "su-transform-gpu su-rotate-180" : ""
+              } ${chevronMobile} ${chevronDesktop}`}
               aria-hidden="true"
             />
           </Popover.Button>
@@ -70,9 +73,13 @@ const MainMenuGroup = ({
           >
             <Popover.Panel
               as="ul"
-              className={`${
+              className={`su-list-unstyled ${
                 panelFacing === "left" ? "lg:su-right-0" : ""
-              } su-list-unstyled su-shadow-md su-w-[29rem] su-px-20 su-py-10 su-relative lg:su-absolute su-bg-cardinal-red-xdark children:su-mb-0`}
+              } ${
+                open
+                  ? "su-bg-cardinal-red-xxdark su-w-full lg:su-bg-cardinal-red-xdark"
+                  : ""
+              } lg:su-shadow-md lg:su-w-[29rem] su-px-20 su-py-10 su-relative lg:su-absolute su-bg-cardinal-red-xdark children:su-mb-0`}
             >
               <CreateBloks blokSection={menuItems} />
             </Popover.Panel>
