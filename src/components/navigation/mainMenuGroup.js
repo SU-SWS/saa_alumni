d067a8c97140fa895f8a8902b8afd549ee7b5557
Yvonne Tang <ytang2@stanford.edu>
2021-07-09 16:56:29 -0700

    More main nav styles

--- src/components/navigation/mainMenuGroup.js
+++ src/components/navigation/mainMenuGroup.js
@@ -8,15 +8,15 @@ const MainMenuGroup = ({
 }) => {
   // Styles for 1st level parent item buttons
   const buttonMobile =
-    "su-flex su-items-center su-w-full hocus:su-bg-cardinal-red-xxdark hocus:su-shadow-none su-py-20";
+    "su-flex su-items-center su-w-full hocus:su-bg-cardinal-red-xxdark hocus:su-shadow-none su-py-20 su-pl-20 su-pr-80 su-text-20";
   const buttonDesktop =
-    "lg:su-items-end lg:su-px-15 xl:su-pt-20 lg:su-pb-18 xl:su-pb-[3rem] lg:hocus:su-bg-transparent lg:su-whitespace-pre lg:hocus:su-text-digital-red-light lg:hocus:su-no-underline lg:su-border-b-[5px] lg:su-border-solid lg:su-border-transparent lg:hocus:su-border-digital-red-xlight";
+    "lg:su-items-end lg:su-px-15 xl:su-pt-20 lg:su-pb-18 xl:su-pb-[3rem] lg:hocus:su-bg-transparent lg:su-whitespace-pre lg:hocus:su-text-digital-red-light lg:hocus:su-no-underline lg:su-border-b-[5px] lg:su-border-solid lg:su-border-transparent lg:hocus:su-border-digital-red-xlight lg:su-text-19 2xl:su-text-21";
 
   // Styles for the down chevron
   const chevronMobile =
-    "su-absolute su-right-0 su-w-[3.4rem] su-pt-3 su-pb-1 su-px-2 su-bg-digital-red su-rounded-full group-hocus:!su-bg-digital-red-light su-mr-15";
+    "su-absolute su-right-0 su-w-[3.4rem] su-pt-3 su-pb-1 su-px-2 su-bg-digital-red su-rounded-full group-hocus:!su-bg-digital-red-light su-mr-20";
   const chevronDesktop =
-    "lg:su-relative lg:su-mr-0 lg:su-w-[1.2em] lg:su-bg-transparent lg:group-hocus:su-text-digital-red-xlight lg:group-hocus:!su-bg-transparent";
+    "lg:su-relative lg:su-mr-0 lg:su-w-[1.2em] lg:su-pt-0 lg:su-pb-0 lg:su-px-0 lg:su-bg-transparent lg:group-hocus:su-text-digital-red-xlight lg:group-hocus:!su-bg-transparent";
 
   return (
     <Popover
@@ -28,9 +28,9 @@ const MainMenuGroup = ({
           <Popover.Button
             className={`${
               open
-                ? "!su-text-white !su-bg-cardinal-red-xxdark lg:!su-bg-cardinal-red-xdark !su-border-cardinal-red-xdark"
+                ? "lg:hocus:su-text-white !su-bg-cardinal-red-xxdark lg:!su-bg-cardinal-red-xdark !su-border-cardinal-red-xdark hover:!su-bg-digital-red lg:hover:!su-bg-cardinal-red-xdark"
                 : ""
-            } su-group ${buttonMobile} ${buttonDesktop} su-font-bold su-text-left su-leading-snug su-bg-transparent focus:su-outline-none`}
+            } su-group ${buttonMobile} ${buttonDesktop} su-font-bold su-text-left su-leading-snug su-bg-transparent focus:su-outline-none su-underline-offset`}
           >
             {parentText}
             {parentTextSecond && (
@@ -79,7 +79,7 @@ const MainMenuGroup = ({
                 open
                   ? "su-bg-cardinal-red-xxdark su-w-full lg:su-bg-cardinal-red-xdark"
                   : ""
-              } lg:su-shadow-md lg:su-w-[29rem] su-px-20 su-py-10 su-relative lg:su-absolute su-bg-cardinal-red-xdark children:su-mb-0`}
+              } lg:su-shadow-md lg:su-w-[29rem] su-px-20 su-pt-2 su-pb-10 lg:su-py-10 su-relative lg:su-absolute su-bg-cardinal-red-xdark children:su-mb-0`}
             >
               <CreateBloks blokSection={menuItems} />
             </Popover.Panel>
