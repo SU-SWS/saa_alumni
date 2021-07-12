0248b23f310f4e0446ea40cce863a2156e1e7667
Nathan Plowman <nathan.plowman@ffwagency.com>
2021-07-12 10:31:07 -0600

    ADAPT-2665: Refactored SearchField component for reusability. Added autcomplete functionality to search modal.

---
+++ src/components/search/searchAutocomplete.js
@@ -0,0 +1,57 @@
+import React from "react";
+import sanitize from "sanitize-html";
+
+const searchAutocomplete = ({
+  autocompleteSuggestions,
+  showAutocomplete,
+  onSelect,
+  selectedSuggestion,
+  autocompleteContainerClasses,
+  autocompleteLinkClasses,
+}) => (
+  <div
+    className={`${autocompleteContainerClasses}
+    ${showAutocomplete ? "" : "su-hidden"}`}
+  >
+    {Array.isArray(autocompleteSuggestions) && (
+      <div className="su-list-unstyled" role="listbox">
+        {autocompleteSuggestions.map((suggestion, index) => (
+          // eslint-disable-next-line jsx-a11y/click-events-have-key-events
+          <div
+            key={`autocomplete-item-${suggestion.objectID}`}
+            role="option"
+            tabIndex={showAutocomplete ? 0 : -1}
+            className={`su-mb-0
+                        ${autocompleteLinkClasses}
+                        ${
+                          index === selectedSuggestion
+                            ? "su-bg-black-20 su-text-digital-red"
+                            : ""
+                        }
+                      `}
+            onClick={(e) => onSelect(e, suggestion.query)}
+            aria-selected={selectedSuggestion === index ? "true" : "false"}
+            id="search-autocomplete-listbox"
+          >
+            {
+              // eslint-disable-next-line no-underscore-dangle
+              suggestion._highlightResult && (
+                <span
+                  // eslint-disable-next-line react/no-danger
+                  dangerouslySetInnerHTML={{
+                    __html: sanitize(
+                      // eslint-disable-next-line no-underscore-dangle
+                      suggestion._highlightResult.query.value
+                    ),
+                  }}
+                />
+              )
+            }
+          </div>
+        ))}
+      </div>
+    )}
+  </div>
+);
+
+export default searchAutocomplete;
