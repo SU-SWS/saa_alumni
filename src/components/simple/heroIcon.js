894e5748fefb0978f367b7b9b45eee5053ffc7e7
Yvonne Tang <ytang2@stanford.edu>
2021-07-13 15:46:25 -0700

    refactor HeroIcon component

--- src/components/simple/heroIcon.js
+++ src/components/simple/heroIcon.js
@@ -2,33 +2,38 @@ import React from "react";
 import { SrOnlyText } from "decanter-react";
 import { dcnb } from "cnbuilder";
 import { VideoCameraIcon, MicrophoneIcon } from "@heroicons/react/outline";
-import { ArrowRightIcon, ArrowUpIcon } from "@heroicons/react/solid";
+import { ArrowRightIcon } from "@heroicons/react/solid";
 
 const HeroIcon = ({ iconType, srText, hideSrText, className, ...props }) => {
-  let Icon;
-  let iconStyle;
+  const heroIconMap = {
+    video: {
+      heroicon: VideoCameraIcon,
+      iconStyle: "su-w-08em su-mt-[-0.2em]",
+    },
+    podcast: {
+      heroicon: MicrophoneIcon,
+      iconStyle: "su-w-08em su-mt-[-0.25em]",
+    },
+    external: {
+      heroicon: ArrowRightIcon,
+      iconStyle:
+        "su-w-08em su-ml-02em su--rotate-45 group-hocus:su--rotate-45 group-hocus:su-translate-x-02em group-hocus:su--translate-y-02em",
+    },
+    internal: {
+      heroicon: ArrowRightIcon,
+      iconStyle:
+        "su-w-08em su-ml-03em su--mt-01em group-hocus:su-translate-x-02em",
+    },
+  };
 
-  if (iconType === "video") {
-    Icon = VideoCameraIcon;
-    iconStyle = "su-w-08em su-mt-[-0.2em]";
-  } else if (iconType === "podcast") {
-    Icon = MicrophoneIcon;
-    iconStyle = "su-w-08em su-mt-[-0.25em]";
-  } else if (iconType === "external") {
-    Icon = ArrowUpIcon;
-    iconStyle =
-      "su-w-08em su-ml-02em su-rotate-45 group-hocus:su-rotate-45 group-hocus:su-translate-x-02em group-hocus:su--translate-y-02em";
-  } else if (iconType === "internal") {
-    Icon = ArrowRightIcon;
-    iconStyle =
-      "su-w-08em su-ml-03em su--mt-01em group-hocus:su-translate-x-02em";
-  }
+  const Icon = heroIconMap[iconType].heroicon;
+  const heroIconStyle = heroIconMap[iconType].iconStyle;
 
   return (
     <>
       <Icon
         aria-hidden="true"
-        className={dcnb(iconStyle, className)}
+        className={dcnb(heroIconStyle, className)}
         {...props}
       />
       {!hideSrText && <SrOnlyText srText={srText || ` ${iconType} `} />}
