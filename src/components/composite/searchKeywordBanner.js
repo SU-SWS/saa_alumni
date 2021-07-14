cbf6d9e4d1d7a8c871adfec013bd71ea2aad3463
andrewbaraniuk <andrew.baraniuk@ffwagency.com>
2021-07-14 16:28:06 +0300

    ADAPT-2679: show recently created Banner if multiple banners match, show content depending on component

--- src/components/composite/searchKeywordBanner.js
+++ src/components/composite/searchKeywordBanner.js
@@ -1,5 +1,10 @@
 import React from "react";
 import { StaticQuery, graphql } from "gatsby";
+import { Container, FlexCell, FlexBox } from "decanter-react";
+import PerkCardHorizontal from "../cards/perkCardHorizontal";
+import BasicCard from "../cards/basicCard";
+import Wysiwyg from "../simple/wysiwyg";
+import Poster from "../composite/poster";
 
 const query = graphql`
   query {
@@ -10,8 +15,8 @@ const query = graphql`
     ) {
       edges {
         node {
-          name
           content
+          created_at
           field_keywords_string
         }
       }
@@ -24,15 +29,39 @@ const SearchKeywordBanner = ({ queryText }) => (
     query={query}
     render={({ allStoryblokEntry }) => {
       if (!allStoryblokEntry?.edges.length) return null;
-      return (
-        <>
-          {allStoryblokEntry.edges.map(({ node: { name, content, field_keywords_string } }) => {
-            let split = field_keywords_string.split(',')
-            let newSplit = split.map(str => str.trim());
-            return newSplit.indexOf(queryText) >= 0 ? name : '';
-          })}
-        </>
-      );
+      let created = {};
+      allStoryblokEntry.edges.map(({ node: { content, created_at, field_keywords_string } }) => {
+        const blok = JSON.parse(content);
+        let split = field_keywords_string.split(',')
+        let newSplit = split.map(str => str.trim());
+        if (newSplit.indexOf(queryText) >= 0) {
+          created[Date.parse(created_at)] = blok;
+        }
+      })
+      let banner = '';
+      if (created) {
+        // Return most recently created Banner.
+        let max = Math.max(...Object.keys(created));
+        banner = created[max];
+      }
+      let content = '';
+      if (banner) {
+        switch (banner.content[0].component) {
+          case 'perkCardHorizontal':
+            content = (<PerkCardHorizontal blok={banner}/>);
+            break;
+          case 'basicCard':
+            content = (<BasicCard blok={banner}/>);
+            break;
+          case 'wysiwyg':
+            content = (<Wysiwyg blok={banner}/>);
+            break;
+          case 'poster':
+            content = (<Poster blok={banner}/>);
+            break;
+        }
+      }
+      return content;
     }}
   />
 );
