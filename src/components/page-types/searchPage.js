1514f6188dd5a448373efff1c66734d7f01f6728
Nathan Plowman <nathanplowman@Nathans-MacBook-Pro.local>
2021-07-14 11:03:17 -0600

    ADAPT-2671: Added scroll-to action after clicking pager link.

--- src/components/page-types/searchPage.js
+++ src/components/page-types/searchPage.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import SbEditable from "storyblok-react";
 import algoliasearch from "algoliasearch";
 import { Container, FlexCell, FlexBox, Heading } from "decanter-react";
+import scrollTo from "gatsby-plugin-smoothscroll";
 import {
   useQueryParam,
   NumberParam,
@@ -61,6 +62,7 @@ const SearchPage = (props) => {
   const updatePage = (pageNumber) => {
     setPage(pageNumber);
     setPageParam(pageNumber);
+    scrollTo("#search-results");
   };
 
   // Update facet values when facet is selected.
@@ -141,7 +143,7 @@ const SearchPage = (props) => {
                 )}
               </FlexCell>
             )}
-            <FlexCell xs="full" lg={8}>
+            <FlexCell xs="full" lg={8} id="search-results">
               {results.nbHits > 0 && (
                 <>
                   <SearchResults results={results} />
