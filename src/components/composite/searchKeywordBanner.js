e0538ed0bb6ebf3b8258d2313187656b56e7b943
andrewbaraniuk <andrew.baraniuk@ffwagency.com>
2021-07-15 11:43:56 +0300

    ADAPT-2679: improvements

--- src/components/composite/searchKeywordBanner.js
+++ src/components/composite/searchKeywordBanner.js
@@ -1,11 +1,8 @@
 import React from "react";
 import { StaticQuery, graphql } from "gatsby";
-import { Container, FlexCell, FlexBox } from "decanter-react";
-import PerkCardHorizontal from "../cards/perkCardHorizontal";
-import BasicCard from "../cards/basicCard";
-import Wysiwyg from "../simple/wysiwyg";
-import Poster from "../composite/poster";
+import CreateBloks from "../../utilities/createBloks";
 
+// Get Search Keyword Banners.
 const query = graphql`
   query {
     allStoryblokEntry(
@@ -23,45 +20,32 @@ const query = graphql`
     }
   }
 `;
+// Get most recently created Banner.
+const getBanner = (entry, query) => {
+  const created = {};
+  entry.edges.map(({ node: { content, created_at, field_keywords_string } }) => {
+    const blok = JSON.parse(content);
+    const split = field_keywords_string.split(',');
+    const newSplit = split.map(str => str.trim());
+    if (newSplit.indexOf(query) >= 0) {
+      created[Date.parse(created_at)] = blok;
+    }
+  });
+
+  return created ? created[Math.max(...Object.keys(created))].content : '';
+}
 
 const SearchKeywordBanner = ({ queryText }) => (
   <StaticQuery
     query={query}
     render={({ allStoryblokEntry }) => {
       if (!allStoryblokEntry?.edges.length) return null;
-      let created = {};
-      allStoryblokEntry.edges.map(({ node: { content, created_at, field_keywords_string } }) => {
-        const blok = JSON.parse(content);
-        let split = field_keywords_string.split(',')
-        let newSplit = split.map(str => str.trim());
-        if (newSplit.indexOf(queryText) >= 0) {
-          created[Date.parse(created_at)] = blok;
-        }
-      })
-      let banner = '';
-      if (created) {
-        // Return most recently created Banner.
-        let max = Math.max(...Object.keys(created));
-        banner = created[max];
-      }
-      let content = '';
+      const banner = getBanner(allStoryblokEntry, queryText);
       if (banner) {
-        switch (banner.content[0].component) {
-          case 'perkCardHorizontal':
-            content = (<PerkCardHorizontal blok={banner}/>);
-            break;
-          case 'basicCard':
-            content = (<BasicCard blok={banner}/>);
-            break;
-          case 'wysiwyg':
-            content = (<Wysiwyg blok={banner}/>);
-            break;
-          case 'poster':
-            content = (<Poster blok={banner}/>);
-            break;
-        }
+        return (<CreateBloks blokSection={banner} />)
       }
-      return content;
+
+      return '';
     }}
   />
 );
