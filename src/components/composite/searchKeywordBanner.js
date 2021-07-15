19768835b186952915fb28ea7989f337fcf766c2
andrewbaraniuk <andrew.baraniuk@ffwagency.com>
2021-07-15 12:17:46 +0300

    ADAPT-2679: fixed eslint errors

--- src/components/composite/searchKeywordBanner.js
+++ src/components/composite/searchKeywordBanner.js
@@ -6,9 +6,7 @@ import CreateBloks from "../../utilities/createBloks";
 const query = graphql`
   query {
     allStoryblokEntry(
-      filter: {
-        field_component: { eq: "searchKeywordBanner" }
-      }
+      filter: { field_component: { eq: "searchKeywordBanner" } }
     ) {
       edges {
         node {
@@ -23,17 +21,19 @@ const query = graphql`
 // Get most recently created Banner.
 const getBanner = (entry, query) => {
   const created = {};
-  entry.edges.map(({ node: { content, created_at, field_keywords_string } }) => {
-    const blok = JSON.parse(content);
-    const split = field_keywords_string.split(',');
-    const newSplit = split.map(str => str.trim());
-    if (newSplit.indexOf(query) >= 0) {
-      created[Date.parse(created_at)] = blok;
+  entry.edges.map(
+    ({ node: { content, created_at, field_keywords_string } }) => {
+      const blok = JSON.parse(content);
+      const split = field_keywords_string.split(",");
+      const newSplit = split.map((str) => str.trim());
+      if (newSplit.indexOf(query) >= 0) {
+        created[Date.parse(created_at)] = blok;
+      }
     }
-  });
+  );
 
-  return created ? created[Math.max(...Object.keys(created))].content : '';
-}
+  return created ? created[Math.max(...Object.keys(created))].content : "";
+};
 
 const SearchKeywordBanner = ({ queryText }) => (
   <StaticQuery
@@ -42,10 +42,10 @@ const SearchKeywordBanner = ({ queryText }) => (
       if (!allStoryblokEntry?.edges.length) return null;
       const banner = getBanner(allStoryblokEntry, queryText);
       if (banner) {
-        return (<CreateBloks blokSection={banner} />)
+        return <CreateBloks blokSection={banner} />;
       }
 
-      return '';
+      return "";
     }}
   />
 );
