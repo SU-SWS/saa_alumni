030ef5b5948a426e6c595d9b4b89320004bff051
Yvonne Tang <ytang2@stanford.edu>
2021-07-23 10:36:58 -0700

    tweak gatsby link import to use new convention

--- src/utilities/sbLink.js
+++ src/utilities/sbLink.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useLocation } from "@reach/router";
 import { parse } from "query-string";
-import Link from "gatsby-link";
+import { Link } from "gatsby";
 import { dcnb } from "cnbuilder";
 import { isNetlify } from "../contexts/GlobalContext";
 import { config } from "./config";
