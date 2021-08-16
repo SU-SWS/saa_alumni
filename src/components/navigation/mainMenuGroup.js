9a2cdca919eec68480421e5433d8700381992f6c
Yvonne Tang <ytang2@stanford.edu>
2021-08-16 10:27:21 -0700

    menu child items active state

--- src/components/navigation/mainMenuGroup.js
+++ src/components/navigation/mainMenuGroup.js
@@ -37,7 +37,7 @@ const MainMenuGroup = ({
     // Loop through children menu items and add active styles to parent button if any childrem items are active
     for (let i = 0; i < menuItems.length; i += 1) {
       const data = menuItems;
-      if (browserUrl.indexOf(data[i].link?.cached_url) > -1) {
+      if (browserUrl.includes(data[i].link?.cached_url)) {
         activeButtonStyles =
           "su-bg-cardinal-red-xxdark lg:su-text-digital-red-xlight lg:su-bg-transparent lg:!su-border-digital-red-xlight";
         activeChevronStyles =
