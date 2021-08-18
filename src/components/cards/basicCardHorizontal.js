e041c7661aecc6aa23269ca6e09a4ee2f22e356d
Yvonne Tang <ytang2@stanford.edu>
2021-08-18 00:30:54 -0700

    Horizontal basic card

---
+++ src/components/cards/basicCardHorizontal.js
@@ -0,0 +1,93 @@
+import React from "react";
+import SbEditable from "storyblok-react";
+import { dcnb } from "cnbuilder";
+import { FlexBox } from "decanter-react";
+import CardImage from "../media/cardImage";
+import CircularImage from "../media/circularImage";
+import BasicCardContent from "./basicCardContent";
+
+const BasicCardHorizontal = ({
+  blok: {
+    cta,
+    borderColor,
+    isRound,
+    isMinimal,
+    isBigHeadline,
+    image: { filename, focus } = {},
+    imageFocus,
+    headline,
+    headingLevel,
+    text,
+  },
+  blok,
+}) => {
+  // Default wrapper classes for white, non-minimal cards
+  let wrapperClasses =
+    "su-bg-white su-text-black su-border su-border-solid su-border-black-30-opacity-40 su-bg-clip-padding su-shadow-sm su-rs-pt-2 su-rs-px-2 su-rs-pb-3";
+
+  // Card content padding for non-minimal cards
+  let bodyPadding = "su-rs-pl-2";
+
+  // Basic card image has aspect ratio 3x2 for non-round option
+  let cardImage = (
+    <div className="su-w-400 su-flex-shrink-0" aria-hidden="true">
+      <div className="su-aspect-w-3 su-aspect-h-2">
+        <CardImage
+          filename={filename}
+          size="vertical"
+          imageFocus={imageFocus}
+          smartFocus={focus}
+          loading="lazy"
+        />
+      </div>
+    </div>
+  );
+
+  if (isRound && filename) {
+    cardImage = (
+      <CircularImage
+        borderColor={borderColor}
+        filename={filename}
+        imageFocus={imageFocus}
+        smartFocus={focus}
+        className={isMinimal ? "su-flex-shrink-0" : "su-flex-shrink-0"}
+        loading="lazy"
+      />
+    );
+  }
+
+  if (isMinimal) {
+    wrapperClasses = "";
+    bodyPadding = "";
+
+    // Add left padding to content if the minimal card has an image
+    if (filename) {
+      bodyPadding = "su-rs-pl-2";
+    }
+  }
+
+  return (
+    <SbEditable content={blok}>
+      <FlexBox
+        element="article"
+        direction="row"
+        className={dcnb(
+          "basic-card su-w-full su-basefont-23 su-break-words",
+          wrapperClasses
+        )}
+      >
+        {filename?.startsWith("http") && cardImage}
+        <BasicCardContent
+          headline={headline}
+          headingLevel={headingLevel}
+          isBigHeadline={isBigHeadline}
+          text={text}
+          cta={cta}
+          className={bodyPadding}
+        />
+      </FlexBox>
+    </SbEditable>
+  );
+};
+
+export default BasicCardHorizontal;
