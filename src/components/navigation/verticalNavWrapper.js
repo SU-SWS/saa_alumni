5236bc38727173566e230298f6c17e1e55cd1482
Elena <elena.cracea@gmail.com>
2021-08-19 14:31:04 -0600

    ADAPT-3074: Lint

--- src/components/navigation/verticalNavWrapper.js
+++ src/components/navigation/verticalNavWrapper.js
@@ -14,51 +14,56 @@ const VerticalNavWrapper = ({
     // Check if menu item's url matches the current page url
     const urlMatch = (link) => {
       const url = window.location.href;
-      return url.indexOf(link) > -1 && (!url.split(link)[1] || url.split(link)[1] === "/")
-    }
+      return (
+        url.indexOf(link) > -1 &&
+        (!url.split(link)[1] || url.split(link)[1] === "/")
+      );
+    };
 
     // Recursive function that will add active and activeTrail props to the active link, it's parents and the
     // immediate children if available.
     const setLinkProps = (obj) => {
       if (obj) {
         if (urlMatch(obj.link.cached_url)) {
+          // eslint-disable-next-line no-param-reassign
           obj.active = true;
-        } else {
-          if (obj.childItems.length > 0) {
-            obj.childItems.map((child) => {
-              obj.activeTrail = true;
-              if (urlMatch(child.link.cached_url)) {
-                child.active = true;
-              }
-              else {
-                setLinkProps(child);
-              }
-            })
-          }
+        } else if (obj.childItems.length > 0) {
+          // eslint-disable-next-line no-param-reassign, array-callback-return
+          obj.childItems.map((child) => {
+            // eslint-disable-next-line no-param-reassign
+            obj.activeTrail = true;
+            if (urlMatch(child.link.cached_url)) {
+              // eslint-disable-next-line no-param-reassign
+              child.active = true;
+            } else {
+              setLinkProps(child);
+            }
+          });
         }
       }
     };
 
     if (items.length > 0) {
+      // eslint-disable-next-line array-callback-return
       items.map((item, key) => {
         // Recursive function that will check which of the first level items have the active item and need to be opened.
         const getActiveSubmenu = function (obj) {
           if (urlMatch(obj.link.cached_url)) {
             setLinkProps(items[key]);
           } else if (obj.childItems.length > 0) {
+            // eslint-disable-next-line array-callback-return
             obj.childItems.map((child) => {
               if (urlMatch(child.link.cached_url)) {
                 setLinkProps(items[key]);
-              }
-              else {
+              } else {
                 getActiveSubmenu(child);
               }
-            })
+            });
           }
-        }
+        };
 
         getActiveSubmenu(item);
-      })
+      });
     }
   }
   return (
