638333d76042d2961c29a0f0adb0a23cf0a7d328
Yvonne Tang <ytang2@stanford.edu>
2021-08-31 19:19:39 -0700

    Add support for newer FA icons

--- src/components/simple/faIcon.js
+++ src/components/simple/faIcon.js
@@ -6,19 +6,37 @@ import { fas } from "@fortawesome/free-solid-svg-icons";
 import { far } from "@fortawesome/free-regular-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-const FaIcon = ({ iconChoice, iconType, isOutline, srText, ...props }) => {
+const FaIcon = ({
+  iconChoice,
+  iconType,
+  isOutline,
+  proFaIcon,
+  iconStyle,
+  srText,
+  ...props
+}) => {
   // Add all 3 types of FA icons to the library so you can use any of them
   library.add(fab, far, fas);
 
   // Remove "fa-" from Storyblok FA plugin to use Fontawesome React format
-  const faIcon = iconChoice.replace("fa-", "");
+  const faIcon = proFaIcon || iconChoice.replace("fa-", "");
+  let faStyle = iconType || "far";
+
+  if (iconStyle === "regular" || (isOutline && !iconStyle)) {
+    faStyle = "far";
+  } else if (iconStyle === "brands") {
+    faStyle = "fab";
+  } else if (iconStyle === "solid") {
+    faStyle = "fas";
+  } else if (iconStyle === "light") {
+    faStyle = "fal";
+  } else if (iconStyle === "duotone") {
+    faStyle = "fad";
+  }
 
   return (
     <>
-      <FontAwesomeIcon
-        icon={[isOutline ? "far" : iconType, faIcon]}
-        {...props}
-      />
+      <FontAwesomeIcon icon={[faStyle, faIcon]} {...props} />
       {srText && <SrOnlyText srText={srText} />}
     </>
   );
