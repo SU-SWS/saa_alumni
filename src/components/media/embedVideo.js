2d3bc56b20e7f2023b0b48adb65417bb1db09943
andrewbaraniuk <andrew.baraniuk@ffwagency.com>
2021-09-09 17:49:29 +0300

    ADAPT-3223: added workaround for using custom video iframe title set in CMS

--- src/components/media/embedVideo.js
+++ src/components/media/embedVideo.js
@@ -20,6 +20,7 @@ const EmbedVideo = ({
     spacingTop,
     spacingBottom,
     videoWidth,
+    videoTitle,
   },
   blok,
 }) => {
@@ -30,7 +31,26 @@ const EmbedVideo = ({
   const spacingBottomStyle = smallPaddingBottom[spacingBottom] ?? "";
 
   const convertToSecond = (min, sec) => min * 60 + sec;
-  const aspectRatioStyle = mediaAspectRatio[aspectRatio ?? "16x9"];
+
+  const start = convertToSecond(startMin, startSec);
+  const width = "100%";
+  const height = "100%";
+  const frameBorder = "0";
+  const allowFullScreen = "1";
+  const YouTubeRegexp = /(?:youtube(?:-nocookie)?\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})/;
+  const VimeoRegexp = /(vimeo(pro)?\.com)\/(?:[^\d]+)?(\d+)\??(.*)?$/;
+  let allow = "";
+  let src = "";
+  let video = "";
+
+  if (video = videoUrl.match(YouTubeRegexp)) {
+    const params = `autoplay=0&mute=0&controls=1&playsinline=1&showinfo=0&rel=0&iv_load_policy=3&modestbranding=1&enablejsapi=1`;
+    src = `https://www.youtube.com/embed/${video[1]}?start=${start}&${params}`;
+    allow = `accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture`;
+  } else if (video = videoUrl.match(VimeoRegexp)) {
+    src = `https://player.vimeo.com/video/${video[3]}`;
+    allow = `autoplay; encrypted-media`;
+  }
 
   return (
     <SbEditable content={blok}>
@@ -40,18 +60,19 @@ const EmbedVideo = ({
         captionAlign={captionAlign}
         className={dcnb(spacingTopStyle, spacingBottomStyle)}
       >
-        <ReactPlayer
-          className={dcnb("su-media__wrapper", aspectRatioStyle)}
-          width=""
-          height=""
-          url={videoUrl}
-          controls="true"
-          config={{
-            youtube: {
-              playerVars: { start: convertToSecond(startMin, startSec) },
-            },
-          }}
-        />
+        <div className="su-media__wrapper su-aspect-w-16 su-aspect-h-9">
+          <div style={{ width: `${width}`, height: `${height}` }}>
+            <iframe
+              frameBorder={frameBorder}
+              allowFullScreen={allowFullScreen}
+              allow={allow}
+              title={videoTitle}
+              width={width}
+              height={height}
+              src={src}
+            />
+          </div>
+        </div>
       </CaptionMedia>
     </SbEditable>
   );
