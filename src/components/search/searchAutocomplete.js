348f9b31b7aa136d549fc504fcd3e51ee6379626
andrewbaraniuk <andrew.baraniuk@ffwagency.com>
2021-09-16 11:32:39 +0300

    ADAPT-3713: used unique identifier instead of a random string

--- src/components/search/searchAutocomplete.js
+++ src/components/search/searchAutocomplete.js
@@ -1,15 +1,6 @@
 import React from "react";
 import sanitize from "sanitize-html";
 
-const searchAutocompleteListboxID = function () {
-  // Math.random should be unique because of its seeding algorithm.
-  // Convert it to base 36 (numbers + letters), and grab the first 9 characters
-  // after the decimal.
-  const hash = Math.random().toString(36).substr(2, 9);
-
-  return `search-autocomplete-listbox_${hash}`;
-};
-
 const searchAutocomplete = ({
   autocompleteSuggestions,
   showAutocomplete,
@@ -49,7 +40,7 @@ const searchAutocomplete = ({
             }}
             onFocus={(e) => setSelectedSuggestion(index)}
             aria-selected={selectedSuggestion === index ? "true" : "false"}
-            id={searchAutocompleteListboxID()}
+            id={`search-autocomplete-listbox-${suggestion.objectID}`}
           >
             {
               // eslint-disable-next-line no-underscore-dangle
