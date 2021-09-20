a7b3220092f8da51f493fa8c4ace7a2f7bae4200
Yvonne Tang <ytang2@stanford.edu>
2021-09-20 13:01:17 -0700

    Adjust utility nav so it doesn't wrap to 2nd line for 360-374px devices

--- src/tailwind/plugins/theme/screens.js
+++ src/tailwind/plugins/theme/screens.js
@@ -1,8 +1,15 @@
 /**
- * Extra breakpoint (2XL + 2 x the screen edge margins)
+ * Extra breakpoints
+ * xs: Allow for targeting extra narrow screens under 375px (though in reality we really only support 360px and up)
+ * i.e., no responsive variant prefix needed for 0 to 374px; use xs variant for 375px and up
+ * 3xl: 2xl + 2 x the screen edge margins
  */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = function () {
   return {
+    xs: "375px",
+    ...defaultTheme.screens,
     "3xl": "1700px",
   };
 };
