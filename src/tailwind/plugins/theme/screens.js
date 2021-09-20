d4553d4bf442015fa9394ecd331c9790a885b629
Yvonne Tang <ytang2@stanford.edu>
2021-09-20 13:59:19 -0700

    Breakpoint fixup - use decanter default instead of TW default

--- src/tailwind/plugins/theme/screens.js
+++ src/tailwind/plugins/theme/screens.js
@@ -1,15 +1,18 @@
 /**
- * Extra breakpoints
+ * Decanter default breakpoints + 2 extra breakpoints
  * xs: Allow for targeting extra narrow screens under 375px (though in reality we really only support 360px and up)
  * i.e., no responsive variant prefix needed for 0 to 374px; use xs variant for 375px and up
  * 3xl: 2xl + 2 x the screen edge margins
  */
-const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = function () {
   return {
     xs: "375px",
-    ...defaultTheme.screens,
+    sm: "576px",
+    md: "768px",
+    lg: "992px",
+    xl: "1200px",
+    "2xl": "1500px",
     "3xl": "1700px",
   };
 };
