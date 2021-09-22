6f8633bffa1562b59ce0b33777092353bed1b511
Yvonne Tang <ytang2@stanford.edu>
2021-09-21 17:26:41 -0700

    clean up

--- src/components/media/simpleImage.js
+++ src/components/media/simpleImage.js
@@ -49,31 +49,34 @@ const SimpleImage = ({
   if (filename != null) {
     const originalWidth = getImageSize(filename).width;
     const originalHeight = getImageSize(filename).height;
+    const imgAspectRatio = originalWidth / originalHeight;
 
     if (imageWidth === "center-container" && originalWidth > 1500) {
       processedImg = transformImage(filename, "/1500x0");
       imgWidth = "1500";
-      imgHeight = Math.round((1500 * originalHeight) / originalWidth);
+      imgHeight = Math.round(1500 * imgAspectRatio);
     } else if (imageWidth === "10" && originalWidth > 1300) {
       processedImg = transformImage(filename, "/1300x0");
       imgWidth = "1300";
-      imgHeight = Math.round((1300 * originalHeight) / originalWidth);
+      imgHeight = Math.round(1300 * imgAspectRatio);
     } else if (imageWidth === "8" && originalWidth > 1000) {
       processedImg = transformImage(filename, "/1000x0");
       imgWidth = "1000";
-      imgHeight = Math.round((1000 * originalHeight) / originalWidth);
+      imgHeight = Math.round(1000 * imgAspectRatio);
     } else if (imageWidth === "6" && originalWidth > 800) {
       processedImg = transformImage(filename, "/800x0");
       imgWidth = "800";
-      imgHeight = Math.round((800 * originalHeight) / originalWidth);
+      imgHeight = Math.round(800 * imgAspectRatio);
     } else if (imageWidth === "4" && originalWidth > 600) {
       processedImg = transformImage(filename, "/600x0");
       imgWidth = "600";
-      imgHeight = Math.round((600 * originalHeight) / originalWidth);
+      imgHeight = Math.round(600 * imgAspectRatio);
     }
     // If no downsizing is needed, just run it through transformImage to reduce jpg quality to 60%
     else {
       processedImg = transformImage(filename, "");
+      imgWidth = originalWidth;
+      imgHeight = originalHeight;
     }
   }
 
