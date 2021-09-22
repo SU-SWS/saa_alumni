b44a95d1c14b0b9a564e897f97250044c1cba494
Matt Anglin <mattanglin@gmail.com>
2021-09-22 16:35:54 -0700

    Re-enable scroll-to-top for all pagination query params

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -25,7 +25,6 @@ export const shouldUpdateScroll = (ctx) => {
 
   // Prevent scrolling trip filter pages
   const filterQueryParams = [
-    'page=',
     'trip-region=',
     'trip-year=',
     'trip-month=',
