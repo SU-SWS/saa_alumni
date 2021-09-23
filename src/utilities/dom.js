e61873d69030076520e7493da19fe62f55c30016
Matt Anglin <mattanglin@gmail.com>
2021-09-23 11:02:47 -0700

    smooth scroll on focus

--- src/utilities/dom.js
+++ src/utilities/dom.js
@@ -1,7 +1,12 @@
-export const focusElement = (selector = '') => {
+import scrollTo from 'gatsby-plugin-smoothscroll';
+
+export const focusElement = (selector = '', { smooth = true } = {}) => {
   if (global.document !== 'undefined') {
     const el = document.querySelector(selector);
 
-    if (el && el.focus) el.focus();
+    if (el && el.focus) {
+      el.focus({ preventScroll: smooth });
+      if (smooth) scrollTo(selector);
+    }
   }
 };
