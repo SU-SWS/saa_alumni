62b446687e634364fedce474423fb379d97e7883
Yvonne Tang <ytang2@stanford.edu>
2021-09-24 13:39:41 -0700

    Add components to global header

--- src/components/identity/global-header/GlobalHeader.jsx
+++ src/components/identity/global-header/GlobalHeader.jsx
@@ -1,9 +1,11 @@
 /* eslint-disable react/forbid-prop-types */
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Container } from 'decanter-react';
+import { Container, FlexBox, FlexCell } from 'decanter-react';
 import * as styles from './GlobalHeaderStyles';
 import CreateBloks from '../../../utilities/createBloks';
+import Logo from '../logo';
+import OpenSearchModalButton from '../../search/openSearchModalButton';
 
 export const GlobalHeaderProps = {
   utilityNav: PropTypes.array,
@@ -20,12 +22,20 @@ const GlobalHeader = ({ utilityNav, mainNav, hasHero }) => (
         </ul>
       </nav>
     </Container>
-    <Container width="full" className={styles.root}>
-      <nav aria-label="Utility Menu" className={styles.utilNav}>
-        <ul className={styles.utilNavMenu}>
-          <CreateBloks blokSection={utilityNav} />
-        </ul>
-      </nav>
+    <Container className={styles.root}>
+      <FlexBox justifyContent="space-between" alignItems="center">
+        <FlexCell className="su-w-fit su-px-16 su-pt-12 su-pb-10 su-bg-cardinal-red">
+          <Logo className="su-w-[13rem] xl:su-w-[16.5rem]" />
+        </FlexCell>
+        <FlexCell>
+          <nav aria-label="Utility Menu" className={styles.utilNav}>
+            <ul className={styles.utilNavMenu}>
+              <CreateBloks blokSection={utilityNav} />
+            </ul>
+          </nav>
+          <OpenSearchModalButton />
+        </FlexCell>
+      </FlexBox>
     </Container>
   </>
 );
