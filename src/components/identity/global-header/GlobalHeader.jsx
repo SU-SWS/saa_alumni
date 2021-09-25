0b95f3f72fb79c8a8f6620ba540377dce66d59b4
Yvonne Tang <ytang2@stanford.edu>
2021-09-24 18:12:52 -0700

    Add search modal

--- src/components/identity/global-header/GlobalHeader.jsx
+++ src/components/identity/global-header/GlobalHeader.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/forbid-prop-types */
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Container, FlexBox, FlexCell } from 'decanter-react';
 import * as styles from './GlobalHeaderStyles';
@@ -7,6 +7,7 @@ import CreateBloks from '../../../utilities/createBloks';
 import Logo from '../logo';
 import OpenSearchModalButton from '../../search/openSearchModalButton';
 import SbLink from '../../../utilities/sbLink';
+import SearchModal from '../../search/searchModal';
 
 export const GlobalHeaderProps = {
   utilityNav: PropTypes.array,
@@ -14,43 +15,55 @@ export const GlobalHeaderProps = {
   hasHero: PropTypes.bool,
 };
 
-const GlobalHeader = ({ siteName, siteLink, utilityNav, mainNav, hasHero }) => (
-  <>
-    <Container width="full" className={styles.rootMobile}>
-      <nav aria-label="Utility Menu" className={styles.utilNavMobile}>
-        <ul className={styles.utilNavMobileMenu}>
-          <CreateBloks blokSection={utilityNav} />
-        </ul>
-      </nav>
-    </Container>
-    <Container className={styles.root}>
-      <FlexBox justifyContent="space-between" alignItems="start">
-        <FlexCell className={styles.logoWrapper}>
-          <Logo className={styles.logo} />
-        </FlexCell>
-        <FlexCell className="su-flex su-flex-grow su-rs-mt-0">
-          <nav aria-label="Utility Menu" className={styles.utilNav}>
-            <ul className={styles.utilNavMenu}>
-              <CreateBloks
-                blokSection={utilityNav}
-                className={styles.utilNavItem}
-              />
-            </ul>
-          </nav>
-          <OpenSearchModalButton />
-        </FlexCell>
-      </FlexBox>
-      <div className="su-basefont-23 su-rs-mt-0">
-        <SbLink
-          link={siteLink}
-          classes="su-text-white su-font-bold hocus:su-text-white su-no-underline hocus:su-no-underline su-text-m3"
-        >
-          {siteName}
-        </SbLink>
-      </div>
-    </Container>
-  </>
-);
+const GlobalHeader = ({ siteName, siteLink, utilityNav, mainNav, hasHero }) => {
+  const [modalOpen, setModalOpen] = useState(false);
+
+  return (
+    <>
+      <Container width="full" className={styles.rootMobile}>
+        <nav aria-label="Utility Menu" className={styles.utilNavMobile}>
+          <ul className={styles.utilNavMobileMenu}>
+            <CreateBloks blokSection={utilityNav} />
+          </ul>
+        </nav>
+      </Container>
+      <Container className={styles.root}>
+        <FlexBox justifyContent="space-between" alignItems="start">
+          <FlexCell className={styles.logoWrapper}>
+            <Logo className={styles.logo} />
+          </FlexCell>
+          <FlexCell className="su-flex su-flex-grow su-rs-mt-0">
+            <nav aria-label="Utility Menu" className={styles.utilNav}>
+              <ul className={styles.utilNavMenu}>
+                <CreateBloks
+                  blokSection={utilityNav}
+                  className={styles.utilNavItem}
+                />
+              </ul>
+            </nav>
+            <OpenSearchModalButton
+              openOpen={modalOpen}
+              setModalOpen={setModalOpen}
+            />
+          </FlexCell>
+        </FlexBox>
+        <div className="su-basefont-23 su-rs-mt-0 su-rs-mb-1">
+          <SbLink
+            link={siteLink}
+            classes="su-text-white su-font-bold hocus:su-text-white su-no-underline hocus:su-no-underline su-text-m3"
+          >
+            {siteName}
+          </SbLink>
+        </div>
+      </Container>
+      <SearchModal
+        isOpen={modalOpen}
+        setIsOpen={setModalOpen}
+        onClose={() => setModalOpen(false)}
+      />
+    </>
+  );
+};
 GlobalHeader.propTypes = GlobalHeaderProps;
 
 export default GlobalHeader;
