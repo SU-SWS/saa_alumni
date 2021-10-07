bcf119a2fd75d9157a077a8f774311f35cd6579f
Yvonne Tang <ytang2@stanford.edu>
2021-10-07 11:10:50 -0700

    LG font size

--- src/components/page-types/TripPage/TripPageSectionNav.styles.js
+++ src/components/page-types/TripPage/TripPageSectionNav.styles.js
@@ -3,7 +3,7 @@ export const root =
 export const menu = 'su-flex su-list-unstyled su-bg-saa-black';
 export const listItem = 'su-mb-0';
 export const link =
-  'su-block su-text-white su-transition-colors su-font-bold su-leading-snug su-no-underline focus:su-outline-none su-underline-offset hocus:su-text-digital-red-xlight su-px-20 su-py-[4rem] su-border-b-[5px] su-border-solid su-border-transparent hocus:su-border-digital-red-xlight su-text-19 xl:su-text-21';
+  'su-block su-text-white su-transition-colors su-font-bold su-leading-snug su-no-underline focus:su-outline-none su-underline-offset hocus:su-text-digital-red-xlight su-px-20 su-py-[4rem] su-border-b-[5px] su-border-solid su-border-transparent hocus:su-border-digital-red-xlight su-text-21';
 export const button = 'su-ml-[8rem] su-mt-[-0.5rem]';
 
 // Mobile styles
