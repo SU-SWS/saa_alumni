883b8ec36b9377555a503cf7eec1bf770bc48f62
Yvonne Tang <ytang2@stanford.edu>
2021-10-10 23:34:34 -0700

    Mobile section nav styles

--- src/components/page-types/TripPage/TripPageSectionNav.jsx
+++ src/components/page-types/TripPage/TripPageSectionNav.jsx
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import PropTypes from 'prop-types';
+import { dcnb } from 'cnbuilder';
+import { MenuIcon, XIcon } from '@heroicons/react/outline';
+import { Container } from 'decanter-react';
 import * as styles from './TripPageSectionNav.styles';
 import { SBLinkType } from '../../../types/storyblok/SBLinkType';
 import SAALinkButton from '../../cta/SAALinkButton';
+import useEscape from '../../../hooks/useEscape';
+import useOnClickOutside from '../../../hooks/useOnClickOutside';
+import { isExpanded } from '../../../utilities/menuHelpers';
 
 export const TripPageSectionNavProps = {
   facultyHeading: PropTypes.string,
@@ -23,6 +29,29 @@ export const TripPageSectionNav = (props) => {
     reservationURL,
     activeSection,
   } = props;
+
+  const [navOpened, setNavOpened] = useState(false);
+  const ref = useRef(null);
+  const burgerRef = useRef(null);
+
+  const toggleNav = () => {
+    setNavOpened(!navOpened);
+  };
+
+  let NavIcon = MenuIcon;
+  if (navOpened) {
+    NavIcon = XIcon;
+  }
+
+  // Close menu if escape key is pressed and return focus to the menu button
+  useEscape(() => {
+    if (burgerRef.current && isExpanded(burgerRef.current)) {
+      setNavOpened(false);
+      burgerRef.current.focus();
+    }
+  });
+
+  useOnClickOutside(ref, () => setNavOpened(false));
 
   return (
     <>
@@ -86,43 +115,101 @@ export const TripPageSectionNav = (props) => {
             size="small-short"
             className={styles.button}
           >
-            Nofity
+            Notify me
           </SAALinkButton>
         )}
       </nav>
 
-      <nav aria-label={ariaLabel} className={styles.rootMobile}>
-        <ul className={styles.menu}>
-          <li className={styles.listItem}>
-            <a href="#overview-section">Overview</a>
-          </li>
-          {renderFacultySection && (
-            <li className={styles.listItem}>
-              <a href="#faculty-leaders-section">Faculty Leaders</a>
-            </li>
-          )}
-          {renderItinerarySection && (
-            <li className={styles.listItem}>
-              <a href="#itinerary-section">Itinerary</a>
-            </li>
-          )}
-          {renderDetailsSection && (
-            <li className={styles.listItem}>
-              <a href="#trip-details-section">Trip Details</a>
-            </li>
-          )}
-        </ul>
-        {reservationURL?.cached_url && (
-          <SAALinkButton link={reservationURL} size="small-short">
-            Reserve
-          </SAALinkButton>
-        )}
-        {!reservationURL?.cached_url && inquireURL?.cached_url && (
-          <SAALinkButton link={inquireURL} size="small-short">
-            Nofity
-          </SAALinkButton>
-        )}
-      </nav>
+      <Container className={styles.navWrapperMobile}>
+        <nav aria-label={ariaLabel} className={styles.rootMobile} ref={ref}>
+          <button
+            type="button"
+            onClick={toggleNav}
+            aria-label={`${navOpened ? 'Close' : 'Open'} section menu`}
+            aria-expanded={!!navOpened}
+            ref={burgerRef}
+            className={styles.burger({ navOpened })}
+          >
+            <span>{navOpened ? 'Close' : 'Section menu'}</span>
+            <NavIcon aria-hidden className={styles.burgerIcon({ navOpened })} />
+          </button>
+          <div
+            aria-hidden={!navOpened}
+            className={styles.menuMobile({ navOpened })}
+          >
+            <ul className={styles.listMobile}>
+              <li className={styles.listItemMobile}>
+                <a
+                  href="#overview-section"
+                  className={styles.linkMobile({
+                    isActive: activeSection === 1,
+                  })}
+                  aria-current={activeSection === 1}
+                >
+                  Overview
+                </a>
+              </li>
+              {renderFacultySection && (
+                <li className={styles.listItemMobile}>
+                  <a
+                    href="#faculty-leader-section"
+                    className={styles.linkMobile({
+                      isActive: activeSection === 2,
+                    })}
+                    aria-current={activeSection === 2}
+                  >
+                    Faculty Leader
+                  </a>
+                </li>
+              )}
+              {renderItinerarySection && (
+                <li className={styles.listItemMobile}>
+                  <a
+                    href="#itinerary-section"
+                    className={styles.linkMobile({
+                      isActive: activeSection === 3,
+                    })}
+                    aria-current={activeSection === 3}
+                  >
+                    Itinerary
+                  </a>
+                </li>
+              )}
+              {renderDetailsSection && (
+                <li className={styles.listItemMobile}>
+                  <a
+                    href="#trip-details-section"
+                    className={styles.linkMobile({
+                      isActive: activeSection === 4,
+                    })}
+                    aria-current={activeSection === 4}
+                  >
+                    Trip Details
+                  </a>
+                </li>
+              )}
+            </ul>
+            {reservationURL?.cached_url && (
+              <SAALinkButton
+                link={reservationURL}
+                size="small-short"
+                className={styles.button}
+              >
+                Reserve
+              </SAALinkButton>
+            )}
+            {!reservationURL?.cached_url && inquireURL?.cached_url && (
+              <SAALinkButton
+                link={inquireURL}
+                size="small-short"
+                className={styles.button}
+              >
+                Notify me
+              </SAALinkButton>
+            )}
+          </div>
+        </nav>
+      </Container>
     </>
   );
 };
--- src/components/page-types/TripPage/TripPageSectionNav.styles.js
+++ src/components/page-types/TripPage/TripPageSectionNav.styles.js
@@ -1,7 +1,7 @@
 import { dcnb } from 'cnbuilder';
 
 export const root =
-  'su-hidden lg:su-flex su-sticky su-top-0 su-z-20 su-cc su-pt-5 su-bg-saa-black su-justify-center su-items-center';
+  'section-menu su-hidden lg:su-flex su-sticky su-top-0 su-z-30 su-cc su-pt-5 su-bg-saa-black su-justify-center su-items-center';
 export const menu = 'su-flex su-list-unstyled su-bg-saa-black';
 export const listItem = 'su-mb-0';
 export const link = ({ isActive }) =>
@@ -14,6 +14,28 @@ export const link = ({ isActive }) =>
 export const button = 'su-ml-[8rem] su-mt-[-0.5rem]';
 
 // Mobile styles
-export const rootMobile = 'su-flex lg:su-hidden';
-export const menuMobile =
-  'su-flex su-flex-col su-list-unstyled su-bg-saa-black';
+export const navWrapperMobile = 'su-bg-saa-black su-rs-pb-2';
+export const rootMobile = 'section-menu-desktop su-flex lg:su-hidden';
+export const burger = ({ navOpened }) =>
+  dcnb(
+    'su-group su-w-full su-flex su-justify-between su-font-semibold su-items-center su-mt-20 su-transition-colors su-border-solid su-border su-border-black-70 su-py-12 su-px-20 hocus:su-bg-digital-red hocus:su-border-digital-red hocus:su-text-white hocus:su-shadow-lg',
+    {
+      'su-bg-digital-red su-text-white su-border-digital-red': navOpened,
+      'su-text-black-10': !navOpened,
+    }
+  );
+export const burgerIcon = ({ navOpened }) =>
+  dcnb('su-transition-colors su-w-[2.4rem] group-hocus:su-text-white', {
+    'su-text-white': navOpened,
+    'su-text-digital-red-light': !navOpened,
+  });
+export const menuMobile = ({ navOpened }) =>
+  dcnb(
+    'su-flex su-flex-col su-absolute su-z-20 su-shadow-xl su-bg-white su-w-full',
+    {
+      'su-block': navOpened,
+      'su-hidden': !navOpened,
+    });
+export const listMobile = 'su-flex su-flex-col su-list-unstyled';
+export const listItemMobile = '';
+export const linkMobile = ({ isActive }) => '';
