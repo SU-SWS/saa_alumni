274feb25cbf6b768e307402dd5284850334cbfa7
Yvonne Tang <ytang2@stanford.edu>
2021-11-19 09:50:39 -0800

    Wrap SBHeading component with SbEditable

--- src/components/storyblok/heading.jsx
+++ src/components/storyblok/heading.jsx
@@ -1,31 +1,34 @@
 import React from 'react';
 import { dcnb } from 'cnbuilder';
+import SbEditable from 'storyblok-react';
 import { Heading } from '../simple/Heading';
 import {
   smallPaddingTop,
   smallPaddingBottom,
 } from '../../utilities/dataSource';
 
-export const SBHeading = ({
-  blok: {
+export const SBHeading = ({ blok }) => {
+  const {
     title,
     fontSize,
     headingLevel = 2,
-    spacingBottom = 'lg',
     spacingTop = 'none',
-  },
-}) => {
+    spacingBottom = 'lg',
+  } = blok;
+
   const paddingTop = smallPaddingTop[spacingTop];
   const paddingBottom = smallPaddingBottom[spacingBottom];
 
   return (
-    <Heading
-      level={headingLevel}
-      size={fontSize === 'default' ? null : fontSize}
-      font="serif"
-      className={dcnb(paddingTop, paddingBottom)}
-    >
-      {title}
-    </Heading>
+    <SbEditable content={blok}>
+      <Heading
+        level={headingLevel}
+        size={fontSize === 'default' ? null : fontSize}
+        font="serif"
+        className={dcnb(paddingTop, paddingBottom)}
+      >
+        {title}
+      </Heading>
+    </SbEditable>
   );
 };
