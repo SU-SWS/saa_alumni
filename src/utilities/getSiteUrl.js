aac2f6b7a047175a8d5fa6f342831e2df3c63991
Nathan Plowman <nathan.plowman@ffwagency.com>
2022-01-14 12:27:19 -0700

    ADAPT-4168: Added debug statements, to help determine why siteUrl is not getting set correctly for Netlify Preview builds.

--- src/utilities/getSiteUrl.js
+++ src/utilities/getSiteUrl.js
@@ -2,6 +2,11 @@ const getSiteUrl = () => {
   // Support for Gatsby CLI
   let siteUrl = 'http://localhost:8000';
 
+  console.debug('process.env.CONTEXT', process.env.CONTEXT);
+  console.debug('process.env.NETLIFY', process.env.NETLIFY);
+  console.debug('process.env.DEPLOY_PRIME_URL', process.env.DEPLOY_PRIME_URL);
+  console.debug('process.env.NETLIFY_DEV', process.env.NETLIFY_DEV);
+
   // Support for Production site builds.
   if (process.env.CONTEXT === 'production') {
     siteUrl = process.env.URL;
@@ -14,6 +19,7 @@ const getSiteUrl = () => {
   else if (process.env.NETLIFY_DEV === 'true') {
     siteUrl = 'http://localhost:64946';
   }
+  console.debug('siteUrl', siteUrl);
 
   return siteUrl;
 };
