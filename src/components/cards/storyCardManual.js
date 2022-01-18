395dcf2caec3e29c64334899b7bebf446d611bf6
Elena <elena.cracea@gmail.com>
2022-01-18 10:45:38 -0700

    Lint fix

--- src/components/cards/storyCardManual.js
+++ src/components/cards/storyCardManual.js
@@ -100,7 +100,8 @@ const saaStoryCard = ({
                 filename={cardFilename || filename}
                 imageFocus={cardImageFocus || imageFocus}
                 smartFocus={focus}
-                size="vertical"cons
+                size="vertical"
+                cons
                 className="su-w-full su-h-full su-object-cover su-transition-transform su-transform-gpu group-hover:su-scale-[1.03] group-focus-within:su-scale-[1.03]"
                 loading="lazy"
                 width="600"
