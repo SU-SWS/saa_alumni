b29876901fbd6573a534107aa9a43485b688008e
Nathan Plowman <nathan.plowman@ffwagency.com>
2022-01-24 15:41:33 -0700

    Partially commented back in useAuth code.

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -8,25 +8,25 @@ export const useAuth = (redirectUnauthorized) => {
   const [isAuthenticated, setAuthenticated] = useState(false);
 
   useEffect(() => {
-    // const url = `${window.location.protocol}//${window.location.host}/api/session`;
-    // fetch(url, { follow: 0 })
-    //   .then((res) => res.json())
-    //   .then((body) => {
-    //     if (body === 'UNAUTHORIZED') {
-    //       setIsAuthenticating(false);
-    //       setAuthenticated(false);
-    //       setUser(null);
-    //       if (redirectUnauthorized) {
-    //         const returnUrl = window.location.pathname;
-    //         const query = new URLSearchParams({ final_destination: returnUrl });
-    //         window.location = `/api/login?${query.toString()}`;
-    //       }
-    //     } else {
-    //       setIsAuthenticating(false);
-    //       setUser(body);
-    //       setAuthenticated(true);
-    //     }
-    //   });
+    const url = `${window.location.protocol}//${window.location.host}/api/session`;
+    fetch(url, { follow: 0 })
+      .then((res) => res.json())
+      .then((body) => {
+        if (body === 'UNAUTHORIZED') {
+          setIsAuthenticating(false);
+          setAuthenticated(false);
+          setUser(null);
+          // if (redirectUnauthorized) {
+          //   const returnUrl = window.location.pathname;
+          //   const query = new URLSearchParams({ final_destination: returnUrl });
+          //   window.location = `/api/login?${query.toString()}`;
+          // }
+        } else {
+          setIsAuthenticating(false);
+          setUser(body);
+          setAuthenticated(true);
+        }
+      });
   }, [redirectUnauthorized]);
 
   return {
