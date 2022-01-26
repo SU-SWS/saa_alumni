2ed1d76ad72b85dcb5eff2488cf3562bd6aaacb7
Elena <elena.cracea@gmail.com>
2022-01-26 10:12:42 -0700

    ADAPT-3834: Creeatee ereeusable card componeent

--- src/components/components.js
+++ src/components/components.js
@@ -52,7 +52,7 @@ import Section from './layout/section';
 import SimpleImage from './media/simpleImage';
 import Story from './content-types/story/story';
 import StoryCard from './cards/storyCard';
-import StoryCardView from './content-types/story/storyCardView';
+import SaaStoryCard from './cards/saaStoryCard';
 import { SBTripCard } from './storyblok/tripCard';
 import { SBTsContentPicker } from './storyblok/tsContentPicker';
 import { SBTsContentTemplate } from './storyblok/tsContentTemplate';
@@ -118,7 +118,7 @@ const ComponentList = {
   simpleImage: SimpleImage,
   story: Story,
   storyCard: StoryCard,
-  saaStoryCard: StoryCardView,
+  saaStoryCard: SaaStoryCard,
   trip: TripPage,
   tripCard: SBTripCard,
   tripFilterPage: TripFilterPage,
