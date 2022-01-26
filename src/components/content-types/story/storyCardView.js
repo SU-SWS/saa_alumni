06f0ae25105e0b7a60e31b34571bf8818664d461
Elena <elena.cracea@gmail.com>
2022-01-26 10:49:31 -0700

    Lint fix

--- src/components/content-types/story/storyCardView.js
+++ src/components/content-types/story/storyCardView.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import SaaStoryCard from '../../cards/saaStoryCard';
 
-const StoryCardView = ({ blok, ...props }) => {
-  return <SaaStoryCard blok={{ ...blok, ...props }} />;
-};
+const StoryCardView = ({ blok, ...props }) => (
+  <SaaStoryCard blok={{ ...blok, ...props }} />
+);
 export default StoryCardView;
