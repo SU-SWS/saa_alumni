e1342d879c58979b657751e1da6434cebf0977ca
Shea McKinney <sherakama@gmail.com>
2022-01-26 14:17:02 -0800

    DEBUG: Two

---
+++ lambda/logout.js
@@ -0,0 +1,12 @@
+const serverless = require('serverless-http');
+const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const authInstance = require('./config/authInstance');
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.get('/api/logout', authInstance.destroySession());
+
+exports.handler = serverless(app);
