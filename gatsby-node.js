704c27243161828dc942c6df22bfd6e73593584a
Shea McKinney <sherakama@gmail.com>
2022-01-27 09:12:47 -0800

    DEBUG: Continue.

--- gatsby-node.js
+++ gatsby-node.js
@@ -56,17 +56,7 @@ exports.createPages = ({ graphql, actions }) => {
         entries.forEach((entry, index) => {
           let slug = `${entry.node.full_slug}`;
           slug = slug.replace(/^\/|\/$/g, '');
-          let pagePath = entry.node.full_slug === 'home' ? '' : `${slug}/`;
-
-          // Wire up the 404 page by setting the path to just 404 as Gatsby expects it.
-          if (pagePath.match(/^404/)) {
-            pagePath = '404';
-          }
-
-          // Wire up the 403 page by setting the path to just 403 as Gatsby expects it.
-          if (pagePath.match(/^403/)) {
-            pagePath = '403';
-          }
+          const pagePath = entry.node.full_slug === 'home' ? '' : `${slug}/`;
 
           // Determine if the page is canonical, or is using a custom canonical URL.
           const content = JSON.parse(entry.node.content);
