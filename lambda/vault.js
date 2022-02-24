9ae1079c6e9c76f04df3ce3e8c6de1e8c824237f
Nathan Plowman <nathan.plowman@ffwagency.com>
2022-02-24 14:33:07 -0700

    ADAPT-4355: removed includes for vault runtime poc.

--- lambda/vault.js
+++ lambda/vault.js
@@ -1,10 +1,4 @@
-const {
-  fetchVaultSecrets,
-} = require('netlify-plugin-vault-variables/src/runtime');
-
 exports.handler = async function (event, context) {
-  const secrets = await fetchVaultSecrets();
-
   return {
     statusCode: 200,
     body: JSON.stringify({
