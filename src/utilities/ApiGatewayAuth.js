c9a2b3d003eaaa709fb66e68db286ad85a88524b
rebeccahongsf <rebeccahongsf@gmail.com>
2022-02-24 09:28:31 -0800

    move ApiGatewayAuth into utilities and remove directory from api folder

--- src/utilities/ApiGatewayAuth.js
+++ src/utilities/ApiGatewayAuth.js
@@ -27,19 +27,3 @@ export class ApiGatewayAuth {
     return this.token;
   };
 }
-
-// Get A Token.
-// Do not enable for public use. This is for development/debugging purposes only.
-// Token can be viewed at /api/api-gateway-auth/ApiGatewayAuth
-// -----------------------------------------------------------------------------
-export default async function handler(req, res) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-
-  try {
-    const apiGatewayAuth = new ApiGatewayAuth();
-    const token = await apiGatewayAuth.authenticate();
-    res.status(200).json(token);
-  } catch (e) {
-    res.status(500).json(e);
-  }
-}
