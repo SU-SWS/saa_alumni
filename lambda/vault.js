e53c49c6318ba7dac5e9cf9adb811373bab1a961
Nathan Plowman <nathan.plowman@ffwagency.com>
2022-02-25 15:02:04 -0700

    ADAPT-4355: Added additional debugging variables in test lamdba.

--- lambda/vault.js
+++ lambda/vault.js
@@ -6,6 +6,10 @@ exports.handler = async function (event, context) {
     body: JSON.stringify({
       ADAPT_AUTH_SAML_RETURN_PATH: process.env.ADAPT_AUTH_SAML_RETURN_PATH,
       TEST_VAR: process.env.TEST_VAR,
+      CONTEXT: process.env.CONTEXT,
+      HEAD: process.env.HEAD,
+      BRANCH: process.env.BRANCH,
+      PULL_REQUEST: process.env.PULL_REQUEST,
     }),
   };
 };
