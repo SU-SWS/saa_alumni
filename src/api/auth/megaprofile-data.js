abea578099d7733358e1395957d3982089c6b20d
rebeccahongsf <rebeccahongsf@gmail.com>
2022-03-09 13:31:10 -0800

    tweak comments

--- src/api/auth/megaprofile-data.js
+++ src/api/auth/megaprofile-data.js
@@ -1,6 +1,6 @@
-// Get A User Data. Delete this prior to merging branch into TSGG
+// Get MP User Data and append to User
 // Do not enable for public use. This is for development/debugging purposes only.
-// Data can be viewed at /api/megaprofile-test
+// Data can be viewed at /api/auth/megaprofile-data
 // -----------------------------------------------------------------------------
 import connect from 'next-connect';
 import { MegaProfile } from '../../utilities/MegaProfile';
