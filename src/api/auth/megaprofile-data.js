7893e2ca7cec3410e0d048394ef2ca47a77aca66
rebeccahongsf <rebeccahongsf@gmail.com>
2022-03-09 13:29:46 -0800

    fixup mp get data

--- src/api/auth/megaprofile-data.js
+++ src/api/auth/megaprofile-data.js
@@ -9,20 +9,19 @@ import { authInstance } from '../../utilities/authInstance';
 const megaprofileHandler = async (req, res) => {
   const mp = new MegaProfile();
   try {
-    console.log('req.user: ', req.user);
-    const { contact } = await mp.get(
-      // `${req.user.encodedSUID}/profiles/contact`
-      `66530456105/profiles/contact`
+    const { user } = req;
+    const { data: contact } = await mp.get(
+      `/${req.user.encodedSUID}/profiles/contact`
     );
-    console.log('contact: ', contact);
-    res.status(200).json(contact);
-    // res.status(200).json({ message: 'test' });
-    // TODO: Once we have the user data, we can append the megaprofile data.
+    const mpUser = { user, contact };
+    res.status(200).json(mpUser);
+    // TODO: ADAPT-4438 Once we have the user data, we can append the megaprofile data.
     // const { addresses } = await mp.get(`${user.encodedSUID}/profiles/addresses`);
     // const { emails } = await mp.get(`${user.encodedSUID}/profiles/emails`);
     // const { phoneNumbers } = await mp.get(`${user.encodedSUID}/profiles/phonenumbers`);
-    // const newUser = { ...user, contact, addresses, emails, phoneNumbers };
-    // res.status(200).json(newUser);
+    // ...additional endpoints here
+    // const mpUser = { user, contact, addresses, emails, phoneNumbers, ...additional endpoints here };
+    // res.status(200).json(mpUser);
   } catch (e) {
     res.status(500).json(e.response.data);
   }
