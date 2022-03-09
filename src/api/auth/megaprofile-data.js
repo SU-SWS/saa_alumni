bddb3c8c3fb41e897890fcf229dae2f74a1b24a0
rebeccahongsf <rebeccahongsf@gmail.com>
2022-03-09 15:20:12 -0800

    clone and simplify ApiExceptions from MyAcc

--- src/api/auth/megaprofile-data.js
+++ src/api/auth/megaprofile-data.js
@@ -5,6 +5,10 @@
 import connect from 'next-connect';
 import { MegaProfile } from '../../utilities/MegaProfile';
 import { authInstance } from '../../utilities/authInstance';
+import {
+  ExceptionHandler,
+  NotFoundException,
+} from '../../utilities/ApiExceptions';
 
 const megaprofileHandler = async (req, res) => {
   const mp = new MegaProfile();
@@ -13,8 +17,9 @@ const megaprofileHandler = async (req, res) => {
     const { data: contact } = await mp.get(
       `/${req.user.encodedSUID}/profiles/contact`
     );
+    if (!contact) return NotFoundException(res);
     const mpUser = { user, contact };
-    res.status(200).json(mpUser);
+    return res.status(200).json(mpUser);
     // TODO: ADAPT-4438 Once we have the user data, we can append the megaprofile data.
     // const { addresses } = await mp.get(`${user.encodedSUID}/profiles/addresses`);
     // const { emails } = await mp.get(`${user.encodedSUID}/profiles/emails`);
@@ -22,8 +27,8 @@ const megaprofileHandler = async (req, res) => {
     // ...additional endpoints here
     // const mpUser = { user, contact, addresses, emails, phoneNumbers, ...additional endpoints here };
     // res.status(200).json(mpUser);
-  } catch (e) {
-    res.status(500).json(e.response.data);
+  } catch (err) {
+    return ExceptionHandler(res, err);
   }
 };
 
