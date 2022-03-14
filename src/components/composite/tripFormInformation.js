2822bd1eeb5ccb1f00dd07853ef36adaa50af4c4
rebeccahongsf <rebeccahongsf@gmail.com>
2022-03-14 16:33:36 -0700

    fixup tripFormInfo richtext display

--- src/components/composite/tripFormInformation.js
+++ src/components/composite/tripFormInformation.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import SbEditable from 'storyblok-react';
+import RichTextRenderer from '../../utilities/richTextRenderer';
 
 const TripFormInformation = ({ blok: { body }, blok }) => (
   <SbEditable content={blok}>
-    <p>{body}</p>
+    <RichTextRenderer
+      wysiwyg={body}
+      className="su-leading su-rs-mb-5 children:su-leading-snug children:!su-mb-06em children:last:!su-mb-0"
+    />
   </SbEditable>
 );
 
