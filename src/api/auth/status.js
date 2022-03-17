0832fbaa1a2f43be7439dae4057202e4478b7976
rebeccahongsf <rebeccahongsf@gmail.com>
2022-03-17 14:24:34 -0700

    create api status page for gateway auth

---
+++ src/api/auth/status.js
@@ -0,0 +1,43 @@
+// Gateway Auth Token
+// Do not enable for public use. This is for development/debugging purposes only.
+// Data can be viewed at /api/auth/status
+// -----------------------------------------------------------------------------
+import connect from 'next-connect';
+import { authInstance } from '../../utilities/authInstance';
+import { ApiGatewayAuth } from '../../utilities/ApiGatewayAuth';
+
+const gatewayAuthHandler = async (req, res) => {
+  const auth = new ApiGatewayAuth();
+  // let tokenData;
+  const tokenData = await auth.token.access_token;
+  res.status(200).json({ tokenData });
+
+  // Token protect enpoint.
+  // if (req.query.token !== process.env.STATUSCAKE_TOKEN) {
+  //   res.status(403).send('Could not validate page token.');
+  //   return;
+  // }
+
+  // Fetch the token from the API Gateway
+  // try {
+  // tokenData = await auth.token.access_token;
+  // } catch (error) {
+  //   res.status(500).send('Could not fetch bearer token.');
+  // }
+
+  // const userProfile = await profileFetcher('66530456105', tokenData);
+
+  // If we get a profile from the profile API send it to the browser.
+  // if (userProfile && userProfile.encodedSUID) {
+  //   res.status(200).send('Success');
+  // }
+  // Otherwise, put out an error json message.
+  // else {
+  //   console.error(userProfile);
+  //   res.status(500).send('Could not fetch user profile data.');
+  // }
+};
+
+const handler = connect().use(authInstance.authorize()).get(gatewayAuthHandler);
+
+export default handler;
