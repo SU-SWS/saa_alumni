f8a3ea1dc615eeee4c1cc19fc2ab27c006597648
rebeccahongsf <rebeccahongsf@gmail.com>
2022-03-24 14:35:15 -0700

    theme form page

--- src/components/page-types/formPage/formPage.js
+++ src/components/page-types/formPage/formPage.js
@@ -9,7 +9,7 @@ import Ankle from '../../partials/ankle/ankle';
 import { HeroImage } from '../../composite/HeroImage/HeroImage';
 import { Grid } from '../../layout/Grid';
 import { GridCell } from '../../layout/GridCell';
-import AuthenticatedPage from '../../auth/AuthenticatedPage';
+// import AuthenticatedPage from '../../auth/AuthenticatedPage';
 
 const FormPage = (props) => {
   const {
@@ -26,59 +26,59 @@ const FormPage = (props) => {
   const numAnkle = getNumBloks(ankleContent);
 
   return (
-    <AuthenticatedPage>
-      <SbEditable content={blok}>
-        <Layout {...props}>
-          <Container
-            as="main"
-            id="main-content"
-            className="basic-page su-relative su-flex-grow su-w-full"
-            width="full"
-          >
-            <div className="su-fixed su-top-0 su-z-0 su-h-full su-w-full">
-              <HeroImage
-                filename={filename}
-                alt={alt}
-                focus={focus}
-                overlay="formDark"
-                aspectRatio="5x2"
-                className="su-object-cover su-h-full su-w-full"
-              />
-            </div>
-            <Grid gap xs={12} className="su-relative su-cc su-z-10 su-rs-pb-8">
-              <GridCell xs={12} lg={5} xl={5}>
-                <div className="su-sticky su-top-0 su-h-fit su-text-white su-rs-pt-6">
-                  {title && (
-                    <Heading
-                      level={1}
-                      align="left"
-                      font="serif"
-                      srOnly={isSrOnlyTitle}
-                      id="page-title"
-                    >
-                      {title}
-                    </Heading>
-                  )}
-                  <CreateBloks
-                    blokSection={formContent}
-                    className="su-rs-mt-2 su-drop-shadow"
-                  />
-                </div>
-              </GridCell>
-              <GridCell
-                xs={12}
-                lg={5}
-                xl={5}
-                className=" su-rs-pt-6 su-rs-mt-5"
-              >
-                <CreateBloks blokSection={giveGabForm} />
-              </GridCell>
-            </Grid>
-            {numAnkle > 0 && <Ankle isDark {...props} />}
-          </Container>
-        </Layout>
-      </SbEditable>
-    </AuthenticatedPage>
+    // <AuthenticatedPage>
+    <SbEditable content={blok}>
+      <Layout {...props}>
+        <Container
+          as="main"
+          id="main-content"
+          className="basic-page su-relative su-flex-grow su-w-full"
+          width="full"
+        >
+          <div className="su-fixed su-top-0 su-z-0 su-h-full su-w-full">
+            <HeroImage
+              filename={filename}
+              alt={alt}
+              focus={focus}
+              overlay="formDark"
+              aspectRatio="5x2"
+              className="su-object-cover su-h-full su-w-full"
+            />
+          </div>
+          <Grid gap xs={12} className="su-relative su-cc su-z-10 su-rs-pb-8">
+            <GridCell xs={12} lg={5} xl={5}>
+              <div className="su-sticky su-top-0 su-h-fit su-text-white su-rs-pt-6">
+                {title && (
+                  <Heading
+                    level={1}
+                    align="left"
+                    font="serif"
+                    srOnly={isSrOnlyTitle}
+                    id="page-title"
+                  >
+                    {title}
+                  </Heading>
+                )}
+                <CreateBloks
+                  blokSection={formContent}
+                  className="su-rs-mt-2 su-drop-shadow"
+                />
+              </div>
+            </GridCell>
+            <GridCell
+              xs={12}
+              lg={6}
+              xl={5}
+              className=" su-rs-pt-6 su-rs-mt-5 lg:su-col-start-7 xl:su-col-start-7"
+            >
+              <CreateBloks blokSection={giveGabForm} />
+            </GridCell>
+          </Grid>
+          {numAnkle > 0 && <Ankle isDark {...props} />}
+        </Container>
+      </Layout>
+    </SbEditable>
+    // </AuthenticatedPage>
   );
 };
 
