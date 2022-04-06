861cc9c9e39e970892d3b1bab271386961abe58f
rebeccahongsf <rebeccahongsf@gmail.com>
2022-04-05 17:09:09 -0700

    add TODO comments for future adjustments in ADAPT-4677

--- src/contexts/FormContext.js
+++ src/contexts/FormContext.js
@@ -7,7 +7,9 @@ export default function FormProvider({ children }) {
 
   useEffect(() => {
     if (travelersData.length !== 0) {
+      // TODO: ADAPT-4677 - Determine how to cross-reference the travelers data with their associated addresses
       console.log('Prefill form data: ', travelersData);
+      // TODO: ADAPT-4677 - Update prefill obj with travelers AND their associated addresses
       window.prefill = travelersData;
     }
   }, [travelersData]);
