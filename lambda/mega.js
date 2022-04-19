8b9a97f8ad84a5295aca6c803fdb6e9557b57cee
rebeccahongsf <rebeccahongsf@gmail.com>
2022-04-18 21:01:18 -0700

    enable mp token endpoint

--- lambda/mega.js
+++ lambda/mega.js
@@ -97,10 +97,10 @@ const profileFetcher = async (profileID, token) => {
 // Get A Token.
 // Do not enable for public use. This is for debugging purposes only.
 // -----------------------------------------------------------------------------
-// app.get(`/api/mega/token`, async (req, res) => {
-//   const data = await tokenFetcher();
-//   res.send(JSON.stringify(data));
-// });
+app.get(`/api/mega/token`, async (req, res) => {
+  const data = await tokenFetcher();
+  res.send(JSON.stringify(data));
+});
 
 // Profile Data Endpoint
 //-----------------------------------------------------------------------------
