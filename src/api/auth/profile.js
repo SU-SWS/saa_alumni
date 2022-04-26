20356c8de86c6a13ca4e4382559ec162b3880242
Nathan Plowman <nathan.plowman@ffwagency.com>
2022-04-26 14:46:42 -0600

    ADAPT-4873: Refactored profile endpoint to use fullgg.

--- src/api/auth/profile.js
+++ src/api/auth/profile.js
@@ -3,29 +3,78 @@
 // Data can be viewed at /api/auth/megaprofile-data
 // -----------------------------------------------------------------------------
 import connect from 'next-connect';
+import Url from 'url-parse';
+import { ClientCredentials } from 'simple-oauth2';
+import axios from 'axios';
 import { MegaProfile } from '../../utilities/MegaProfile';
 import { authInstance } from '../../utilities/authInstance';
 import { ExceptionHandler } from '../../utilities/ApiExceptions';
 
+const fetchProfileToken = async () => {
+  // The OAuth Bearer token granter.
+  const TOKEN_URL = process.env.LEGACY_MEGAPROFILE_TOKEN_URL;
+
+  const bearerUrl = Url(TOKEN_URL);
+  // These are required to operate and are not defaulted. Contact a developer for
+  // these credentials. These credentials are used to fetch a bearer token from
+  // the TOKEN_URL endpoint.
+  const CLIENT_ID = process.env.LEGACY_MEGAPROFILE_CLIENT_ID;
+  const CLIENT_SECRET = process.env.LEGACY_MEGAPROFILE_CLIENT_SECRET;
+
+  const megaTokenAuth = new ClientCredentials({
+    client: {
+      id: CLIENT_ID,
+      secret: CLIENT_SECRET,
+    },
+    auth: {
+      tokenHost: `${bearerUrl.protocol}//${bearerUrl.host}`,
+      tokenPath: bearerUrl.pathname,
+    },
+  });
+
+  let response;
+  const tokenParams = { scope: [] };
+  const tokenOpts = { json: true };
+
+  try {
+    response = await megaTokenAuth.getToken(tokenParams, tokenOpts);
+  } catch (error) {
+    return false;
+  }
+
+  if (response && response.token && response.token.access_token) {
+    return response.token.access_token;
+  }
+
+  throw new Error('Response did not contain access token');
+};
+
+const fetchLegacyProfile = async (profileId) => {
+  const token = await fetchProfileToken();
+  const contact = await axios
+    .get(`${process.env.MEGAPROFILE_URL}${profileId}/profiles/fullgg`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+    .then((result) => result.data)
+    .catch((err) => {
+      throw new Error('Failed to fetch legacy profile.');
+    });
+
+  return contact;
+};
+
 const megaprofileHandler = async (req, res) => {
   const mp = new MegaProfile();
   try {
-    const { user } = req;
-    const { data: contact } = await mp.get(
-      `/${req.user.encodedSUID}/profiles/contact`
-    );
+    const profileId = req.user.encodedSUID;
+    const contact = await fetchLegacyProfile(profileId);
     const { data: affiliations } = await mp.get(
-      `/${req.user.encodedSUID}/profiles/affiliations`
+      `/${profileId}/profiles/affiliations`
     );
-    const mpUser = { user, contact, affiliations: affiliations.affiliations };
+    const mpUser = { ...contact, affiliations };
     return res.status(200).json(mpUser);
-    // TODO: ADAPT-4438 Once we have the user data, we can append the megaprofile data.
-    // const { addresses } = await mp.get(`${user.encodedSUID}/profiles/addresses`);
-    // const { emails } = await mp.get(`${user.encodedSUID}/profiles/emails`);
-    // const { phoneNumbers } = await mp.get(`${user.encodedSUID}/profiles/phonenumbers`);
-    // ...additional endpoints here
-    // const mpUser = { user, contact, addresses, emails, phoneNumbers, ...additional endpoints here };
-    // res.status(200).json(mpUser);
   } catch (err) {
     return ExceptionHandler(res, err);
   }
--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -3,6 +3,7 @@ import AuthIdleTimeoutOverlay from '../components/auth/AuthIdleTimeoutOverlay';
 
 const initialAuthState = {
   userProfile: null,
+  userSession: null,
   isAuthenticated: false,
   isAuthenticating: true,
 };
@@ -13,6 +14,8 @@ function authReducer(state, action) {
       return { ...state, isAuthenticating: action.payload };
     case 'setAuthenticated':
       return { ...state, isAuthenticated: action.payload };
+    case 'setUserSession':
+      return { ...state, userSession: action.payload };
     case 'setUserProfile':
       return { ...state, userProfile: action.payload };
     default:
@@ -31,18 +34,28 @@ class AuthContextProvider extends React.Component {
   }
 
   componentDidMount() {
-    const url = `${window.location.protocol}//${window.location.host}/api/auth/profile`;
+    const sessionUrl = `${window.location.protocol}//${window.location.host}/api/auth/session`;
+    const profileUrl = `${window.location.protocol}//${window.location.host}/api/auth/profile`;
 
-    fetch(url).then(async (res) => {
+    fetch(sessionUrl).then(async (res) => {
       if (res.status === 200) {
         const body = await res.json();
         this.dispatch({ type: 'setAuthenticated', payload: true });
-        this.dispatch({ type: 'setUserProfile', payload: body });
+        this.dispatch({ type: 'setUserSession', payload: body });
         this.dispatch({ type: 'setAuthenticating', payload: false });
       } else {
         this.dispatch({ type: 'setAuthenticated', payload: false });
-        this.dispatch({ type: 'setUserProfile', payload: null });
+        this.dispatch({ type: 'setUserSession', payload: null });
         this.dispatch({ type: 'setAuthenticating', payload: false });
+      }
+    });
+
+    fetch(profileUrl).then(async (res) => {
+      if (res.status === 200) {
+        const body = await res.json();
+        this.dispatch({ type: 'setUserProfile', payload: body });
+      } else {
+        this.dispatch({ type: 'setUserProfile', payload: null });
       }
     });
   }
@@ -58,11 +71,13 @@ class AuthContextProvider extends React.Component {
 
   render() {
     const { children } = this.props;
-    const { userProfile, isAuthenticated, isAuthenticating } = this.state;
+    const { userProfile, userSession, isAuthenticated, isAuthenticating } =
+      this.state;
     return (
       <AuthContext.Provider
         value={{
           userProfile,
+          userSession,
           isAuthenticated,
           isAuthenticating,
           setAuthState: this.setAuthState,
