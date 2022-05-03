eb14256cfed001a48f5b62944bf091fcf6b6f0ff
rebeccahongsf <rebeccahongsf@gmail.com>
2022-05-03 08:24:36 -0700

    add single column layout

--- src/components/page-types/formPage/formPage.js
+++ src/components/page-types/formPage/formPage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import SbEditable from 'storyblok-react';
+import { dcnb } from 'cnbuilder';
 import { Container } from '../../layout/Container';
 import { Heading } from '../../simple/Heading';
 import Layout from '../../partials/layout';
@@ -10,6 +11,7 @@ import { HeroImage } from '../../composite/HeroImage/HeroImage';
 import { Grid } from '../../layout/Grid';
 import { GridCell } from '../../layout/GridCell';
 import AuthenticatedPage from '../../auth/AuthenticatedPage';
+import { content } from '../TripPage/TripPageHeroSection.styles';
 
 const FormPage = (props) => {
   const {
@@ -20,67 +22,77 @@ const FormPage = (props) => {
       formContent,
       giveGabForm,
       ankleContent,
+      isSingleColumn,
     },
     blok,
   } = props;
   const numAnkle = getNumBloks(ankleContent);
+  let fullWidth = false;
+  let contentStyle = 'su-sticky su-top-0 su-h-fit';
+  let formCardStyle = 'su-rs-mt-5 lg:su-col-start-7 xl:su-col-start-7';
+
+  if (isSingleColumn) {
+    fullWidth = 12;
+    contentStyle = '';
+    formCardStyle = '';
+  }
 
   return (
-    <AuthenticatedPage>
-      <SbEditable content={blok}>
-        <Layout {...props}>
-          <Container
-            as="main"
-            id="main-content"
-            className="basic-page su-relative su-flex-grow su-w-full"
-            width="full"
+    // <AuthenticatedPage>
+    <SbEditable content={blok}>
+      <Layout {...props}>
+        <Container
+          as="main"
+          id="main-content"
+          className="basic-page su-relative su-flex-grow su-w-full"
+          width="full"
+        >
+          <div className="su-fixed su-top-0 su-z-0 su-h-full su-w-full">
+            <HeroImage
+              filename={filename}
+              alt={alt}
+              focus={focus}
+              overlay="formDark"
+              aspectRatio="5x2"
+              className="su-object-cover su-h-full su-w-full"
+            />
+          </div>
+          <Grid
+            gap
+            xs={12}
+            className="su-relative su-cc su-z-10 su-rs-pb-8 su-rs-pt-6"
           >
-            <div className="su-fixed su-top-0 su-z-0 su-h-full su-w-full">
-              <HeroImage
-                filename={filename}
-                alt={alt}
-                focus={focus}
-                overlay="formDark"
-                aspectRatio="5x2"
-                className="su-object-cover su-h-full su-w-full"
-              />
-            </div>
-            <Grid
-              gap
+            <GridCell xs={12} lg={fullWidth || 5} xl={fullWidth || 5}>
+              <div className={dcnb('su-text-white', contentStyle)}>
+                {title && (
+                  <Heading
+                    level={1}
+                    align="left"
+                    font="serif"
+                    srOnly={isSrOnlyTitle}
+                    id="page-title"
+                    className="su-rs-mt-5"
+                  >
+                    {title}
+                  </Heading>
+                )}
+                <CreateBloks blokSection={formContent} />
+              </div>
+            </GridCell>
+            <GridCell
               xs={12}
-              className="su-relative su-cc su-z-10 su-rs-pb-8 su-rs-pt-6"
+              lg={fullWidth || 6}
+              xl={fullWidth || 5}
+              className={formCardStyle}
             >
-              <GridCell xs={12} lg={5} xl={5}>
-                <div className="su-sticky su-top-0 su-h-fit su-text-white">
-                  {title && (
-                    <Heading
-                      level={1}
-                      align="left"
-                      font="serif"
-                      srOnly={isSrOnlyTitle}
-                      id="page-title"
-                      className="su-rs-mt-5"
-                    >
-                      {title}
-                    </Heading>
-                  )}
-                  <CreateBloks blokSection={formContent} />
-                </div>
-              </GridCell>
-              <GridCell
-                xs={12}
-                lg={6}
-                xl={5}
-                className="su-rs-mt-5 lg:su-col-start-7 xl:su-col-start-7"
-              >
-                <CreateBloks blokSection={giveGabForm} />
-              </GridCell>
-            </Grid>
-            {numAnkle > 0 && <Ankle isDark {...props} />}
-          </Container>
-        </Layout>
-      </SbEditable>
-    </AuthenticatedPage>
+              <CreateBloks blokSection={giveGabForm} />
+            </GridCell>
+          </Grid>
+          {numAnkle > 0 && <Ankle isDark {...props} />}
+        </Container>
+      </Layout>
+    </SbEditable>
+    // </AuthenticatedPage>
   );
 };
 
