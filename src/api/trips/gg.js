e065fd38e63f9258a3b9713452acd8958752eb18
Shea McKinney <sherakama@gmail.com>
2022-05-09 11:33:54 -0700

    ADAPT-4862: Reformat the approach to pull all the trips all the time.

--- src/api/trips/gg.js
+++ src/api/trips/gg.js
@@ -30,20 +30,51 @@ const formatData = (data) => {
  * @param {*} tripId
  * @returns
  */
-const getTripStory = async (tripId) => {
-  const tripRes = await storyblok.get(`cdn/stories`, {
+const getAllTrips = async () => {
+  let currentPage = 1;
+  let trips = [];
+  const perpage = 25;
+  const requests = [];
+  const storyblokRes = await storyblok.get(`cdn/stories/`, {
     filter_query: {
       component: {
         in: 'trip',
       },
-      tripId: {
-        in: tripId,
-      },
     },
-    per_page: 1,
+    per_page: perpage,
+    page: currentPage,
   });
 
-  return tripRes?.data?.stories.pop();
+  const { total } = storyblokRes;
+  trips = trips.concat(storyblokRes.data.stories);
+
+  while (currentPage * perpage < total) {
+    currentPage += 1;
+    requests.push(
+      storyblok.get(`cdn/stories/`, {
+        filter_query: {
+          component: {
+            in: 'trip',
+          },
+        },
+        per_page: perpage,
+        page: currentPage,
+      })
+    );
+  }
+
+  await Promise.all(requests)
+    .then((vals) => {
+      vals.forEach((val) => {
+        trips = trips.concat(val.data.stories);
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({ error: err.message });
+      console.log(err);
+    });
+
+  return trips;
 };
 
 /**
@@ -74,45 +105,42 @@ const getTripFormStory = async (uuid) => {
  */
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Content-Type', 'text/csv');
+  // res.setHeader('Content-Type', 'text/csv');
   res.setHeader('x-robots-tag', ['noindex', 'nofollow', 'nosnippet']);
   res.setHeader('x-content-type-options', 'nosniff');
   res.setHeader('x-xss-protection', '1; mode=block');
   res.setHeader('Cache-Control', ['max-age=300', 'private']);
-  res.setHeader('Expires', '0');
+  res.setHeader('Expires', new Date().toUTCString());
   res.setHeader('date', new Date().toUTCString());
   res.setHeader('accept-ranges', 'none');
   res.setHeader('vary', 'Accept-Encoding');
 
-  // const defaultArray = [
-  //   'prompt', '', 'none', 'TRUE', '', 'No URLData or data query parameter provided.', 'TRUE', 'USD', '', '', '', '', '', 'TRUE',
-  // ];
+  // TMP: for testing
+  const options = {
+    single: 'Single Bed',
+    double: 'Double Bed',
+    triple: 'Triple Bed',
+    quad: 'Quad Bed',
+    king: 'King Bed',
+    queen: 'Queen Bed',
+    twin: 'Twin Bed',
+    sofa: 'Sofa',
+    sofaBed: 'Sofa Bed',
+    sofaCabinet: 'Sofa Cabinet',
+  };
 
-  // Get the trip ID out of the URL and sanitize it to number.
-  let tripId = Number(req?.query?.data || req?.query?.urlData);
+  // Fetch the trip stories from storyblok.
+  const trips = await getAllTrips();
 
-  tripId = 123;
+  if (!trips) {
+    res.status(404).send(`Unable to find any trips`);
+    return;
+  }
 
-  res.setHeader(
-    'content-disposition',
-    `attachment; filename="trip-${tripId}.csv"`
-  );
+  // TMP FOR TESTING.
+  trips.push({ content: { tripId: '123' } });
 
-  console.log(req.query);
-
-  // if (!tripId) {
-  //   res.status(200).send(`${defaultArray.concat(',').slice(0, -1)}`);
-  //   return;
-  // }
-
-  // Fetch the trip story from storyblok.
-  // const trip = await getTripStory(tripId);
-  // if (!trip) {
-  //   res.status(404).send(`Trip with id ${tripId} not found`);
-  //   return;
-  // }
-
-  // // Fetch the trip form information from storyblok.
+  // // Fetch the trip form information from storyblok. if we need it.
   // const registrationForm = await getTripFormStory(trip.uuid);
   // if (!registrationForm) {
   //   res.status(404).send(`Registration Form with id ${tripId} not found`);
@@ -138,73 +166,35 @@ export default async function handler(req, res) {
       'answer_fixed',
     ],
   ];
-  data.push([
-    'prompt',
-    tripId,
-    'single',
-    'TRUE',
-    '',
-    'Single Bedroom',
-    'TRUE',
-    'USD',
-    '',
-    '',
-    '',
-    '',
-    '',
-    'TRUE',
-  ]);
 
-  data.push([
-    'prompt',
-    tripId,
-    'double',
-    'TRUE',
-    '',
-    'Double Bedroom',
-    'TRUE',
-    'USD',
-    '',
-    '',
-    '',
-    '',
-    '',
-    'TRUE',
-  ]);
+  trips.forEach((trip) => {
+    // No trip Id. No Entry.
+    if (!Number(trip.content.tripId)) {
+      return;
+    }
 
-  data.push([
-    'prompt',
-    tripId,
-    'queen',
-    'TRUE',
-    '',
-    'Queen Bedroom',
-    'TRUE',
-    'USD',
-    '',
-    '',
-    '',
-    '',
-    '',
-    'TRUE',
-  ]);
-
-  data.push([
-    'prompt',
-    tripId,
-    'king',
-    'TRUE',
-    '',
-    'King Bedroom',
-    'TRUE',
-    'USD',
-    '',
-    '',
-    '',
-    '',
-    '',
-    'TRUE',
-  ]);
+    // Loop through the bed types in our temporary array. In the future we will
+    // pull the options right from the trip information.
+    Object.entries(options).forEach((option) => {
+      const [bedTypeKey, bedTypeValue] = option;
+      data.push([
+        'prompt',
+        trip.content.tripId,
+        bedTypeKey,
+        'TRUE',
+        '',
+        bedTypeValue,
+        'TRUE',
+        'USD',
+        '',
+        '',
+        '',
+        '',
+        '',
+        'TRUE',
+      ]);
+    });
+  });
 
   // Trip.
   res.status(200).send(formatData(data));
