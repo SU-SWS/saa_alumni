a478d379a0e2bb428013c3573635b88925b07070
Shea McKinney <sherakama@gmail.com>
2022-05-10 22:22:10 -0700

    Added Aria-Live and Role to protected content wrapper.

--- src/components/cards/protectedContentItem.js
+++ src/components/cards/protectedContentItem.js
@@ -2,7 +2,11 @@ import React from 'react';
 import CreateBloks from '../../utilities/createBloks';
 
 const protectedContentItem = ({ blok }) => (
-  <div className="su-text-center su-text-white">
+  <div
+    className="su-text-center su-text-white"
+    aria-live="polite"
+    role="status"
+  >
     <CreateBloks blokSection={blok.content} />
   </div>
 );
