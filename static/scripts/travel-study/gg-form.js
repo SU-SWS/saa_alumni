6b97c9d3373419c90d5d55c75f3080aae8d040b0
rebeccahongsf <rebeccahongsf@gmail.com>
2022-06-06 16:39:14 -0700

    create seperate func for non-trip id forms

--- static/scripts/travel-study/gg-form.js
+++ static/scripts/travel-study/gg-form.js
@@ -1,3 +1,4 @@
+/* eslint-disable new-cap */
 class ggForm {
   /**
    *
@@ -6,8 +7,6 @@ class ggForm {
   constructor(options) {
     this.id = options.id;
     this.form = options.form;
-    this.tripApi =
-      options?.tripApi || 'http://localhost:8000/api/travel-study/trips';
   }
 
   /**
@@ -17,9 +16,7 @@ class ggForm {
     this.elem = document.getElementById(this.id);
     this.render('Loading user information...');
     await this.getUserInfo();
-    this.render('Loading trip information...');
-    this.embedTripSelect();
-    this.autocompleteConfig();
+    this.embedInterstitialPage();
   };
 
   /**
@@ -44,6 +41,7 @@ class ggForm {
       this.user = {
         uid: 0,
         su_display_name: 'Guest',
+        display_name: 'Guest-suid',
         mail: 'person@example.com',
       };
     }
@@ -62,47 +60,7 @@ class ggForm {
    * Put the ADC Window variables into place.
    */
   setADCVariables = () => {
-    const uuid = document.getElementById(`${this.id}-select`).value;
-    this.uuid = uuid;
-    window.su_trip_id = this.trips[uuid].tripId;
-    window.su_trip_name = this.trips[uuid].title;
-    window.su_dname = this.user.su_display_name;
-    window.su_email = this.user.mail;
-  };
-
-  /**
-   * Get the information box.
-   */
-  getTripInfoBox = (trips, uuid) => {
-    const content = `
-      <h3>Trip Information</h3>
-      <p><strong>Trip Name:</strong> ${trips[uuid].title}</p>
-      <p><strong>Trip ID:</strong> ${trips[uuid].tripId}</p>
-      <p>
-        <strong>Trip URL:</strong>
-        <a href="https://alumni.stanford.edu/${trips[uuid].full_slug}" target="_blank" rel="noopener">
-          https://alumni.stanford.edu/${trips[uuid].full_slug}
-        </a>
-      </p>
-      <p><strong>Trip Subtitle:</strong> ${trips[uuid].subtitle}</p>
-      <p><strong>Trip Start Date:</strong> ${trips[uuid].startDate}</p>
-      <p><strong>Trip End Date:</strong> ${trips[uuid].endDate}</p>
-    `;
-    return content;
-  };
-
-  /**
-   *
-   * @returns {Promise<void>}
-   */
-  getTrips = async () => {
-    const response = await fetch(this.tripApi);
-    const trips = await response.json();
-    this.trips = trips;
-    const tripList = Object.keys(trips).map(
-      (key) => `${trips[key].title} ${trips[key].tripId}`
-    );
-    return tripList;
+    window.su_suid = this.user.display_name;
   };
 
   /**
@@ -114,15 +72,10 @@ class ggForm {
     content.style.display = 'flex';
     content.style.gap = '3rem';
 
-    const sidebar = document.createElement('aside');
-    sidebar.className = 'gg-form-sidebar';
-    sidebar.innerHTML += this.getTripInfoBox(this.trips, this.uuid);
-
     const main = document.createElement('section');
     main.className = 'gg-form-main';
     main.appendChild(this.getGGScript());
 
-    content.appendChild(sidebar);
     content.appendChild(main);
     this.render(content);
   };
@@ -130,71 +83,22 @@ class ggForm {
   /**
    * Embeds an option to select a trip.
    */
-  embedTripSelect = () => {
+  embedInterstitialPage = () => {
     const content = document.createElement('div');
-    const message = document.createElement('h3');
-    message.innerText = 'Select a trip to begin:';
-    const div = document.createElement('div');
-    div.className = 'autoComplete_wrapper';
-    const input = document.createElement('input');
-    input.id = `autoComplete`;
-    input.type = `search`;
-    input.dir = 'ltr';
-    input.spellcheck = false;
-    div.appendChild(input);
-    const go = document.createElement('button');
-    go.innerText = 'Go ➡️';
-    go.className = 'go-button';
-    go.onclick = () => {
+    const staffName = document.createElement('p');
+    staffName.innerText = `Staff name: ${this.user.su_display_name}`;
+    const next = document.createElement('button');
+    next.innerText = 'Next ➡️';
+    next.className = 'next-button';
+    next.onclick = () => {
       this.setADCVariables();
       this.renderForm();
     };
 
-    content.appendChild(message);
-    content.appendChild(div);
-    content.appendChild(go);
+    content.appendChild(staffName);
+    content.appendChild(next);
 
     this.render(content);
-  };
-
-  autocompleteConfig = async () => {
-    const trips = await this.getTrips();
-    // eslint-disable-next-line new-cap
-    // eslint-disable-next-line no-undef
-    const autoCompleteJS = new autoComplete({
-      selector: '#autoComplete',
-      placeHolder: 'Search for Trip...',
-      data: {
-        src: [...trips],
-        cache: true,
-      },
-      resultsList: {
-        element: (list, data) => {
-          if (!data.results.length) {
-            // Create "No Results" message element
-            const message = document.createElement('div');
-            // Add class to the created element
-            message.setAttribute('class', 'no_result');
-            // Add message text content
-            message.innerHTML = `<span>Found No Results for "${data.query}"</span>`;
-            // Append message element to the results list
-            list.prepend(message);
-          }
-        },
-        noResults: true,
-      },
-      resultItem: {
-        highlight: true,
-      },
-      events: {
-        input: {
-          selection: (event) => {
-            const selection = event.detail.selection.value;
-            autoCompleteJS.input.value = selection;
-          },
-        },
-      },
-    });
   };
 }
 
---
+++ static/scripts/travel-study/gg-trip-form.js
@@ -0,0 +1,207 @@
+/* eslint-disable new-cap */
+class ggTripForm {
+  /**
+   *
+   * @param {*} options
+   */
+  constructor(options) {
+    this.id = options.id;
+    this.form = options.form;
+    this.tripApi =
+      options?.tripApi || 'http://localhost:8000/api/travel-study/trips';
+  }
+
+  /**
+   * Run you fools!
+   */
+  init = async () => {
+    this.elem = document.getElementById(this.id);
+    this.render('Loading user information...');
+    await this.getUserInfo();
+    this.render('Loading trip information...');
+    this.embedTripSelect();
+    this.autocompleteConfig();
+  };
+
+  /**
+   *
+   * @param {*} content
+   */
+  render = (content) => {
+    this.elem.replaceChildren(content);
+  };
+
+  /**
+   * Get user info from the Local API.
+   */
+  getUserInfo = async () => {
+    const uid = window?.drupalSettings?.user?.uid;
+    try {
+      let udata = await fetch(`/jsonapi/user/user?filter[uid]=${uid}`);
+      udata = await udata.json();
+      this.user = udata.data[0].attributes;
+    } catch (err) {
+      console.log(err);
+      this.user = {
+        uid: 0,
+        su_display_name: 'Guest',
+        mail: 'person@example.com',
+      };
+    }
+  };
+
+  /**
+   * Load Give Gab Form Into Place
+   */
+  getGGScript = () => {
+    const script = document.createElement('script');
+    script.src = this.form;
+    return script;
+  };
+
+  /**
+   * Put the ADC Window variables into place.
+   */
+  setADCVariables = () => {
+    const { uuid } = this;
+    window.su_trip_id = this.trips[uuid].tripId;
+    window.su_trip_name = this.trips[uuid].title;
+    window.su_suid = this.user.display_name;
+  };
+
+  /**
+   * Get the information box.
+   */
+  getTripInfoBox = (trips, uuid) => {
+    const content = `
+      <h3>Trip Information</h3>
+      <p><strong>Trip Name:</strong> ${trips[uuid].title}</p>
+      <p><strong>Trip ID:</strong> ${trips[uuid].tripId}</p>
+      <p>
+        <strong>Trip URL:</strong>
+        <a href="https://alumni.stanford.edu/${trips[uuid].full_slug}" target="_blank" rel="noopener">
+          https://alumni.stanford.edu/${trips[uuid].full_slug}
+        </a>
+      </p>
+      <p><strong>Trip Subtitle:</strong> ${trips[uuid].subtitle}</p>
+      <p><strong>Trip Start Date:</strong> ${trips[uuid].startDate}</p>
+      <p><strong>Trip End Date:</strong> ${trips[uuid].endDate}</p>
+    `;
+    return content;
+  };
+
+  /**
+   *
+   * @returns {Promise<void>}
+   */
+  getTrips = async () => {
+    const response = await fetch(this.tripApi);
+    const trips = await response.json();
+    this.trips = trips;
+    const tripList = Object.keys(trips).map(
+      (uuid) => `${trips[uuid].title} ${trips[uuid].tripId}`
+    );
+    console.log(tripList);
+    return tripList;
+  };
+
+  /**
+   * Embed the trip information and form.
+   */
+  renderForm = () => {
+    const content = document.createElement('article');
+    content.className = 'gg-form-wrapper';
+    content.style.display = 'flex';
+    content.style.gap = '3rem';
+
+    const sidebar = document.createElement('aside');
+    sidebar.className = 'gg-form-sidebar';
+    sidebar.innerHTML += this.getTripInfoBox(this.trips, this.uuid);
+
+    const main = document.createElement('section');
+    main.className = 'gg-form-main';
+    main.appendChild(this.getGGScript());
+
+    content.appendChild(sidebar);
+    content.appendChild(main);
+    this.render(content);
+  };
+
+  /**
+   * Embeds an option to select a trip.
+   */
+  embedTripSelect = () => {
+    const content = document.createElement('div');
+    const message = document.createElement('h3');
+    message.innerText = 'Trip details';
+
+    const labelDiv = document.createElement('div');
+    const label = document.createElement('label');
+    label.innerText = 'Trip name, year, and trip ID number';
+    labelDiv.appendChild(label);
+
+    const inputDiv = document.createElement('div');
+    inputDiv.className = 'autoComplete_wrapper';
+    const input = document.createElement('input');
+    input.id = `autoComplete`;
+    input.type = `search`;
+    input.dir = 'ltr';
+    input.spellcheck = false;
+    inputDiv.appendChild(input);
+
+    const next = document.createElement('button');
+    next.innerText = 'Next ➡️';
+    next.className = 'next-button';
+    next.onclick = () => {
+      this.setADCVariables();
+      this.renderForm();
+    };
+
+    content.appendChild(message);
+    content.appendChild(labelDiv);
+    content.appendChild(inputDiv);
+    content.appendChild(next);
+
+    this.render(content);
+  };
+
+  autocompleteConfig = async () => {
+    const trips = await this.getTrips();
+    // eslint-disable-next-line no-undef
+    const autoCompleteJS = new autoComplete({
+      selector: '#autoComplete',
+      placeHolder: 'Search for Trip...',
+      data: {
+        src: [...trips],
+        cache: true,
+      },
+      resultsList: {
+        element: (list, data) => {
+          if (!data.results.length) {
+            const message = document.createElement('div');
+            message.setAttribute('class', 'no_result');
+            message.innerHTML = `<span>Found No Results for "${data.query}"</span>`;
+            list.prepend(message);
+          }
+        },
+        noResults: true,
+      },
+      resultItem: {
+        highlight: true,
+      },
+      events: {
+        input: {
+          selection: (event) => {
+            const selection = event.detail.selection.value;
+            autoCompleteJS.input.value = selection;
+          },
+        },
+      },
+    });
+  };
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+  const ggFormReady = new Event('ggFormReady', { bubbles: true });
+  document.dispatchEvent(ggFormReady);
+});
