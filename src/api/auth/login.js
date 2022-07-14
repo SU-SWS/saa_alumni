ad152706fe333e5327c852c8baa8a6b6582fcca8
rebeccahongsf <rebeccahongsf@gmail.com>
2022-07-14 09:32:59 -0700

    add conditional check for utm and olc params

--- src/api/auth/login.js
+++ src/api/auth/login.js
@@ -5,11 +5,14 @@ import { authInstance } from '../../utilities/authInstance';
 const handler = connect()
   // Return user to page they were previously on after logging in.
   .use((req, res, next) => {
+    const { referer } = req.headers;
+    const { query, pathname } = URLParse(referer);
     if (!req.query.final_destination) {
-      const { referer } = req.headers;
-      const { pathname } = URLParse(referer);
-      console.log('Pathname: ', pathname);
       req.query.final_destination = pathname || '/';
+    }
+    if (query.includes('utm') || query.includes('olc')) {
+      const urlParams = pathname + query;
+      req.query.final_destination = urlParams;
     }
     next();
   })
