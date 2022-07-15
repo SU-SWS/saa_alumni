f03a5f586ca6a2364d9b4cc15c308ed349139fc5
rebeccahongsf <rebeccahongsf@gmail.com>
2022-07-15 10:43:24 -0700

    revert api login

--- src/api/auth/login.js
+++ src/api/auth/login.js
@@ -5,14 +5,10 @@ import { authInstance } from '../../utilities/authInstance';
 const handler = connect()
   // Return user to page they were previously on after logging in.
   .use((req, res, next) => {
-    const { referer } = req.headers;
-    const { query, pathname } = URLParse(referer);
     if (!req.query.final_destination) {
-      req.query.final_destination = pathname || '/';
-    }
-    if (query.includes('utm') || query.includes('olc')) {
-      const urlParams = pathname + query;
-      req.query.final_destination = urlParams;
+      const { referer } = req.headers;
+      const { pathname } = URLParse(referer);
+      req.query.final_destination = pathname;
     }
     next();
   })
