b31e0ce5f2d26c0f5f4d329fcde2699cb6805566
rebeccahongsf <rebeccahongsf@gmail.com>
2022-07-15 16:36:40 -0700

    ADAPT-5108 | @rebeccahongsf | add givegab failure message

--- src/components/embed/dynaScript.js
+++ src/components/embed/dynaScript.js
@@ -1,6 +1,9 @@
 /* eslint-disable no-alert */
 import React, { useRef, useEffect, useState } from 'react';
 import ClipLoader from 'react-spinners/ClipLoader';
+import { SAACtaLink } from '../cta/SAACtaLink';
+import { SAALinkButton } from '../cta/SAALinkButton';
+import { Heading } from '../simple/Heading';
 
 /**
  * Dynamically load a script after the component has been mounted.
@@ -15,6 +18,7 @@ const DynaScript = ({
 }) => {
   const scriptRef = useRef();
   const [scriptLoaded, setScriptLoaded] = useState(false);
+  const [scriptError, setScriptError] = useState(false);
 
   // When the component mounts load the script.
   useEffect(() => {
@@ -28,7 +32,7 @@ const DynaScript = ({
       }
     };
     script.onerror = () => {
-      alert(errorText);
+      setScriptError(true);
     };
 
     scriptRef.current.appendChild(script);
@@ -46,11 +50,37 @@ const DynaScript = ({
         aria-busy={!scriptLoaded}
         id={id}
       />
-      {!scriptLoaded && (
+      {!scriptLoaded && !scriptError && (
         <>
           <ClipLoader color="#00BFFF" height={50} width={50} aria-busy="true" />
           Loading...
         </>
+      )}
+      {scriptError && (
+        <div className="su-flex su-flex-col su-items-center">
+          <Heading level={2} size={2} weight="regular">
+            Something went wrong.
+          </Heading>
+          <p className="su-big-paragraph">
+            Vestibulum ullamcorper mauris at ligula.
+          </p>
+          <SAALinkButton
+            link="/"
+            className="su-mb-27 su-bg-digital-red su-border-digital-red hocus:su-bg-digital-red-light hocus:su-border-digital-red-light"
+          >
+            Try again
+          </SAALinkButton>
+          <p className="su-mb-0 su-text-19 md:su-text-21 xl:su-text-23 su-flex">
+            If the problem persists,{' '}
+            <SAACtaLink
+              link="/"
+              trailingIcon="external"
+              linkText="please submit a help ticket"
+              textColor="bright-red-hover-white"
+              className="su-font-normal"
+            />
+          </p>
+        </div>
       )}
     </>
   );
--- src/components/embed/giveGabForm.js
+++ src/components/embed/giveGabForm.js
@@ -1,34 +1,17 @@
 /* eslint-disable camelcase */
 import React, { useContext } from 'react';
-import { Helmet } from 'react-helmet';
 import SbEditable from 'storyblok-react';
 import ClipLoader from 'react-spinners/ClipLoader';
 import { dcnb } from 'cnbuilder';
 import { Container } from '../layout/Container';
-import Embed from './embed';
 import DynaScript from './dynaScript';
 import AuthContext from '../../contexts/AuthContext';
 
 // Give Gab Form Component
 // -----------------------------------------------------------------------------
-const GiveGabForm = ({
-  blok: {
-    css_styles,
-    error_text: errorText,
-    head_js,
-    pre_markup,
-    url,
-    post_markup,
-    uuid,
-  },
-  blok,
-  tripId,
-  bgCardStyle,
-}) => {
+const GiveGabForm = ({ blok: { url, uuid }, blok, tripId, bgCardStyle }) => {
   const htmlId = uuid;
   const { isAuthenticating } = useContext(AuthContext);
-  const preBlok = { markup: pre_markup };
-  const postBlok = { markup: post_markup };
 
   const embedUrl = new URL(url);
   if (tripId) {
@@ -56,16 +39,6 @@ const GiveGabForm = ({
 
   return (
     <SbEditable content={blok}>
-      {css_styles && (
-        <Helmet>
-          <style type="text/css">{css_styles}</style>
-        </Helmet>
-      )}
-      {head_js && (
-        <Helmet>
-          <script>{head_js}</script>
-        </Helmet>
-      )}
       <Container
         width="full"
         className={dcnb(
@@ -74,14 +47,8 @@ const GiveGabForm = ({
         )}
       >
         <div className="form-gradient su-rs-p-5 md:su-rs-p-6 2xl:su-pb-[10.8rem]">
-          {pre_markup && <Embed blok={preBlok} />}
-          <DynaScript src={embedUrl} id={htmlId} errorText={errorText} />
+          <DynaScript src={embedUrl} id={htmlId} />
         </div>
-        {post_markup && (
-          <div className="form-gradient su-rs-mt-3 children:children:su-p-38 md:children:children:su-p-72 xl:children:children:su-p-76 children:children:empty:su-p-0">
-            <Embed blok={postBlok} />
-          </div>
-        )}
       </Container>
     </SbEditable>
   );
