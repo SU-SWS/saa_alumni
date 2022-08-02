a69c7aac29451aed8470be7c83e92864870f8f6f
rebeccahongsf <rebeccahongsf@gmail.com>
2022-08-02 15:45:43 -0700

    update traveler list remove font size and name spacing

--- src/components/page-types/registrationFormPage/interstitialPage.js
+++ src/components/page-types/registrationFormPage/interstitialPage.js
@@ -242,7 +242,7 @@ const InterstitialPage = (props) => {
                           <FlexBox justifyContent="center">
                             <Link
                               to={`${slug}/form`}
-                              className="su-group su-flex su-items-end su-text-18 md:su-text-24 su-font-regular su-no-underline su-border-3 su-border-digital-red-xlight su-text-white hocus:su-bg-digital-red hocus:su-border-digital-red hocus:su-text-white hocus:su-shadow-md su-px-20 su-pt-10 su-pb-11 md:su-px-30 md:su-pt-16 md:su-pb-18"
+                              className="su-rs-mt-2 su-group su-flex su-items-end su-text-18 md:su-text-24 su-font-regular su-no-underline su-border-3 su-border-digital-red-xlight su-text-white hocus:su-bg-digital-red hocus:su-border-digital-red hocus:su-text-white hocus:su-shadow-md su-px-20 su-pt-10 su-pb-11 md:su-px-30 md:su-pt-16 md:su-pb-18"
                               state={{ travelers: value[0].travelersData }}
                             >
                               Next
