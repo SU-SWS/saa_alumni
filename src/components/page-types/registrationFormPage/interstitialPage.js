a811327cc73579bd21e9ca4ba3ed8f0e1571f8ce
rebeccahongsf <rebeccahongsf@gmail.com>
2022-08-10 11:20:40 -0700

    add hasHero prop to layout

--- src/components/page-types/registrationFormPage/interstitialPage.js
+++ src/components/page-types/registrationFormPage/interstitialPage.js
@@ -156,7 +156,7 @@ const InterstitialPage = (props) => {
     <AuthenticatedPage>
       <FormContextProvider>
         <SbEditable content={blok}>
-          <Layout {...props}>
+          <Layout hasHero="true" {...props}>
             <Container
               as="main"
               id="main-content"
