653feff20268ca7fbdbf2b01f34a0c35a161a722
rebeccahongsf <rebeccahongsf@gmail.com>
2022-08-31 13:28:32 -0700

    ADAPT-5341 | @rebeccahongsf | update dob func

--- src/utilities/transformDate.js
+++ src/utilities/transformDate.js
@@ -15,8 +15,8 @@ export const formatEmailDate = (tripDate) => {
 
 export const formatUsDate = (tripDate) => {
   const date = new Date(tripDate);
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
   const year = date.getFullYear();
-  const month = (1 + date.getMonth()).toString().padStart(2, '0');
-  const day = date.getDate().toString().padStart(2, '0');
   return `${month}/${day}/${year}`;
 };
