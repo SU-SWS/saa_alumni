8a1267611ef89f887a64f2c28c3541376ab1efac
rebeccahongsf <rebeccahongsf@gmail.com>
2022-10-10 14:29:06 -0700

    test scroll

--- src/components/embed/dynaScript.js
+++ src/components/embed/dynaScript.js
@@ -18,7 +18,8 @@ const DynaScript = ({ errorBlok, src, id, ...props }) => {
     setDisplay(true);
   };
 
-  const scrollTop = () => {
+  const scrollTop = (e) => {
+    e.preventDefault();
     window.scroll({ top: 500 });
   };
 
@@ -35,7 +36,7 @@ const DynaScript = ({ errorBlok, src, id, ...props }) => {
         // Once GiveGab form has completed rendering, display form
         script.addEventListener('widgetRenderEnd', showForm);
         // Once GiveGab form has been prompted to the next form page and/or submitted, bring user back to the top of the form
-        script.addEventListener('widgetPageChange', scrollTop);
+        script.addEventListener('widgetPageChange', (e) => scrollTop(e));
       }
     };
     script.onerror = () => {
