392c9b8e22e3613abf7e3c182107cb5c53b1905a
rebeccahongsf <rebeccahongsf@gmail.com>
2022-10-11 15:26:29 -0700

    remove dupe

--- src/components/page-types/registrationFormPage/registrationFormPage.js
+++ src/components/page-types/registrationFormPage/registrationFormPage.js
@@ -188,7 +188,6 @@ const RegistrationFormPage = (props) => {
                   xl={8}
                   xxl={6}
                   className={styles.formWrapper}
-                  id="su-gg-embed"
                 >
                   <CreateBloks
                     blokSection={giveGabForm}
