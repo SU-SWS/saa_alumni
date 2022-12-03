2087934e8f302cab4a3d0b779777f8cdf83bf0d6
jdwjdwjdw <jacobdeanwilson@gmail.com>
2022-12-02 17:21:24 -0800

    Update typeOfRegistrant page and styles

--- src/components/page-types/membershipFormPage/typeOfRegistrant.js
+++ src/components/page-types/membershipFormPage/typeOfRegistrant.js
@@ -5,14 +5,13 @@ import { Link } from 'gatsby';
 import { Container } from '../../layout/Container';
 import { Heading } from '../../simple/Heading';
 import Layout from '../../partials/layout';
-import Ankle from '../../partials/ankle/ankle';
 import Hero from '../../composite/hero';
 import { Grid } from '../../layout/Grid';
-import { FormContext } from '../../../contexts/FormContext';
 import AuthenticatedPage from '../../auth/AuthenticatedPage';
 import { GridCell } from '../../layout/GridCell';
 import { FlexBox } from '../../layout/FlexBox';
 import HeroIcon from '../../simple/heroIcon';
+import Logo from '../../identity/logo';
 import * as styles from './typeOfRegistrant.styles';
 
 const TypeOfRegistrant = (props) => {
@@ -21,6 +20,14 @@ const TypeOfRegistrant = (props) => {
     blok,
   } = props;
   const helmetTitle = 'Stanford Alumni Association Membership';
+  const heroProps = {
+    image: { filename, alt, focus },
+    sansSuper: 'Stanford Alumni Association Membership',
+    headline: 'Join now!',
+    headlineSize: 'medium',
+    isDarkGradient: 'true',
+    isHideScroll: 'true',
+  };
 
   return (
     <AuthenticatedPage>
@@ -33,39 +40,120 @@ const TypeOfRegistrant = (props) => {
             className={styles.container}
             width="full"
           >
-            {/* <Hero blok={heroProps} /> */}
+            <Hero blok={heroProps} />
             <Container className={styles.contentWrapper}>
-              <Grid gap xs={12} className={styles.depositWrapper}>
-                <GridCell
-                  xs={12}
-                  lg={10}
-                  xl={8}
-                  className={styles.depositContent}
-                >
+              <Grid gap xs={12}>
+                <GridCell xs={12}>
+                  <span className={styles.superHead}>
+                    Stanford Alumni Association Membership
+                  </span>
                   <Heading
                     level={3}
                     size={5}
-                    align="left"
+                    align="center"
                     font="serif"
-                    className={styles.noMarginBottom}
+                    id="page-title"
                   >
                     Join now!
                   </Heading>
                 </GridCell>
               </Grid>
-              <Grid gap xs={12} className={styles.gridContent}>
-                <GridCell xs={12} lg={8}>
-                  <Heading
-                    level={3}
-                    size={5}
-                    align="left"
-                    font="serif"
-                    className={styles.noMarginBottom}
-                  >
-                    Select who you wish to purchase a membership for:
-                  </Heading>
+              <Grid gap xs={12} className={styles.contentWrapper}>
+                <GridCell xs={12}>
+                  <div className=" su-basefont-23 su-p-36 su-w-full su-cc su-bg-saa-black-dark su-border-3 su-border-saa-black-dark">
+                    <FlexBox justifyContent="center" className="su-rs-py-2">
+                      <Logo className="su-w-200 md:su-w-300 2xl:su-w-[350px]" />
+                    </FlexBox>
+                    <p className="su-intro-text">
+                      Become a member of the Stanford Alumni Association and you'll enjoy a host of benefits.
+                    </p>
+                    <p>
+                      To be eligible for alumni membership, you need to have
+                      completed a minimum of three quarters of matriculated
+                      coursework at either the undergraduate or graduate level.
+                      Stanford faculty, staff, interns, residents, fellows,
+                      certificate holders, postdocs, Travel/Study participants
+                      and Stanford parents are eligible for an affiliate
+                      membership.
+                    </p>
+                    <FlexBox justifyContent="center">
+                      <Link to="/" className={styles.benefitsLink}>
+                        Benefits of Membership
+                        <HeroIcon
+                          iconType="arrow-right"
+                          className={styles.benefitsLinkIcon}
+                          isAnimate
+                        />
+                      </Link>
+                    </FlexBox>
+                  </div>
                 </GridCell>
               </Grid>
+              <div className="su-p-36 su-w-full su-cc su-bg-saa-black-dark su-border-3 su-border-saa-black-dark">
+                <Grid gap xs={12} className={styles.gridContent}>
+                  <GridCell xs={12} lg={8}>
+                    <Heading level={3} size={5} align="left" font="serif">
+                      Who do you wish to purchase a membership for?
+                    </Heading>
+                  </GridCell>
+                </Grid>
+                <div className="su-card-centered">
+                  <Grid gap xs={12}>
+                    <GridCell xs={12} md={6}>
+                      <div className="su-border-3 su-px-90 su-py-58">
+                        {/* icon */}
+                        <Heading level={4} size="3" align="center" font="serif">
+                          Myself
+                        </Heading>
+                        {/* Name of myself */}
+                        <p className="su-text-center">Kristin Southard</p>
+                        <FlexBox justifyContent="center">
+                          <Link to="/" className={styles.benefitsLink}>
+                            Select
+                          </Link>
+                        </FlexBox>
+                      </div>
+                    </GridCell>
+                    <GridCell xs={12} md={6}>
+                      <div className="su-border-3 su-px-90 su-py-58">
+                        {/* icon */}
+                        <Heading level={4} size="3" align="center" font="serif">
+                          Someone else
+                        </Heading>
+                        {/* Name of myself */}
+                        <p>Existing contact or new contact</p>
+                        <FlexBox justifyContent="center">
+                          <Link to="/" className={styles.benefitsLink}>
+                            Select
+                          </Link>
+                        </FlexBox>
+                      </div>
+                    </GridCell>
+                  </Grid>
+                </div>
+                {/* tbd Promo code section  */}
+
+                <FlexBox justifyContent="center">
+                  <Link to="/" className={styles.benefitsLink}>
+                    Select membership type
+                    <HeroIcon
+                      iconType="arrow-right"
+                      className={styles.benefitsLinkIcon}
+                      isAnimate
+                    />
+                  </Link>
+                </FlexBox>
+
+                <div className="su-centered-text">
+                  <p>
+                    Please note: All memberships, both domestic and
+                    international, will have access to a digital membership card
+                    in lieu of a physical membership packet. Additionally, we
+                    are unable to send SAA Member key tags to addresses outside
+                    of the US. (note linked digital membership card)
+                  </p>
+                </div>
+              </div>
             </Container>
           </Container>
         </Layout>
--- src/components/page-types/membershipFormPage/typeOfRegistrant.styles.js
+++ src/components/page-types/membershipFormPage/typeOfRegistrant.styles.js
@@ -1,2 +1,8 @@
-export const subheading =
-  'su-mt-4 su-mb-0 su-font-semibold su-text-white su-leading-display';
+export const superHead =
+  'su-block su-max-w-prose su-font-semibold su-leading-display su-text-shadow-md su-type-2 su-text-center su-mx-auto su-mb-01em';
+export const container = 'basic-page su-relative su-flex-grow su-w-full';
+export const gridContent = 'su-rs-mb-5';
+export const contentWrapper = 'su-cc su-rs-pb-8 su-bg-saa-black su-text-white';
+export const benefitsLink =
+  'su-rs-mt-2 su-group su-flex su-items-end su-text-18 md:su-text-24 su-font-regular su-no-underline su-border-3 su-border-digital-red-xlight su-text-white hocus:su-bg-digital-red hocus:su-border-digital-red hocus:su-text-white hocus:su-shadow-md su-px-20 su-pt-10 su-pb-11 md:su-px-30 md:su-pt-16 md:su-pb-18';
+export const benefitsLinkIcon = 'su-w-1em su-text-white';
