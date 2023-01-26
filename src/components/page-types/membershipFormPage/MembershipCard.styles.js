32227cab953ca52153512127b899cb84006761c3
rebeccahongsf <rebeccahongsf@gmail.com>
2023-01-26 13:30:29 -0800

    add rounded border

--- src/components/page-types/membershipFormPage/MembershipCard.styles.js
+++ src/components/page-types/membershipFormPage/MembershipCard.styles.js
@@ -4,7 +4,7 @@ export const root =
   'su-group su-relative su-overflow-hidden su-bg-saa-black-dark su-break-words su-border-black su-w-full sm:su-max-w-[42rem] md:su-max-w-full';
 export const membershipCardWrapper = (newContact, isSelected) => {
   dcnb(
-    'su-basefont-23 su-p-30 lg:su-p-36 su-stretch-link su-w-full su-transition-all su-border-3 su-border-white hocus:su-gradient-border hocus:su-border-to-rt-palo-verde-dark-to-saa-electric-blue',
+    'su-basefont-23 su-p-30 lg:su-p-36 su-stretch-link su-w-full su-transition-all su-rounded su-border-3 su-border-white hocus:su-gradient-border hocus:su-border-to-rt-palo-verde-dark-to-saa-electric-blue',
     newContact ? 'su-border-dashed' : 'su-border-solid',
     isSelected && 'su-bg-saa-black su-border-solid'
   );
