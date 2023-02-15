a5edc79206ab8c20494f6967f9f871db24f543e3
jdwjdwjdw <jacobdeanwilson@gmail.com>
2023-02-15 12:29:18 -0800

    Fixup for select text

--- src/components/page-types/membershipFormPage/membershipPaymentCard.styles.js
+++ src/components/page-types/membershipFormPage/membershipPaymentCard.styles.js
@@ -10,9 +10,9 @@ export const membershipPaymentCardLink =
   'su-rs-mt-1 su-group su-flex su-items-end su-text-18 md:su-text-24 su-font-regular su-no-underline su-text-white su-px-20 su-pt-10 su-pb-11 md:su-px-30 md:su-pt-16 md:su-pb-18 su-border-solid su-border-3 su-transition-colors su-gradient-border su-border-to-rt-palo-verde-dark-to-saa-electric-blue su-bg-transparent group-hocus:su-text-white group-hocus:su-bg-gradient-to-tr group-hocus:su-from-palo-verde-dark group-hocus:su-to-saa-electric-blue group-hocus:su-shadow-md';
 export const membershipPaymentCardSelectedLink =
   'su-rs-mt-1 su-group su-flex su-items-end su-text-18 md:su-text-24 su-font-regular su-no-underline su-text-white su-px-20 su-pt-10 su-pb-11 md:su-px-30 md:su-pt-16 md:su-pb-18 su-border-solid su-border-3 su-transition-colors su-gradient-border su-border-to-rt-palo-verde-dark-to-saa-electric-blue su-text-white su-bg-gradient-to-tr su-from-palo-verde-dark su-to-saa-electric-blue su-shadow-md hocus:su-text-white';
+export const selectionWrapper =
+  'sm:su-absolute su-top-0 su-right-0 md:su-right-[-20px] lg:su-right-[-1.5em] su-text-16 su-font-semibold';
 // export const initialWrapper =
 //   'su-leading su-text-center su-w-50 su-h-50 su-text-24 su-border-2 su-rounded-full';
-// export const selectionWrapper =
-//   'sm:su-absolute su-top-0 su-right-0 md:su-right-[-20px] lg:su-right-[-1.5em] su-text-16 su-font-semibold';
 // export const checkLinkIcon =
 //   'su-inline-block su-text-saa-electric-blue su-w-[1.4em] su-mt-[-2px] su-mr-[2px]';
