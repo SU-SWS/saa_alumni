3da4446c98269fed534a331617b3a0470d4a6d1b
jdwjdwjdw <jacobdeanwilson@gmail.com>
2023-02-16 15:19:06 -0800

    Responsive fixups

--- src/components/page-types/membershipFormPage/membershipPaymentCard.styles.js
+++ src/components/page-types/membershipFormPage/membershipPaymentCard.styles.js
@@ -11,7 +11,7 @@ export const membershipPaymentCardLink =
 export const membershipPaymentCardSelectedLink =
   'su-group su-flex su-items-end su-text-18 md:su-text-24 su-font-regular su-no-underline su-text-white su-px-20 su-pt-10 su-pb-11 md:su-px-30 md:su-pt-16 md:su-pb-18 su-border-solid su-border-3 su-transition-colors su-gradient-border su-border-to-rt-palo-verde-dark-to-saa-electric-blue su-text-white su-bg-gradient-to-tr su-from-palo-verde-dark su-to-saa-electric-blue su-shadow-md hocus:su-text-white';
 export const selectionWrapper =
-  'sm:su-absolute su-top-0 su-right-0 md:su-right-[-20px] lg:su-right-[-1.5em] su-text-16 su-font-semibold';
+  'su-absolute su-top-0 su-right-0 md:su-right-[-20px] lg:su-right-[-1.5em] su-text-16 su-font-semibold';
 // export const initialWrapper =
 //   'su-leading su-text-center su-w-50 su-h-50 su-text-24 su-border-2 su-rounded-full';
 // export const checkLinkIcon =
