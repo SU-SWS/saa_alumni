092716e0d6c735b4202ed151f3e84a9427a8acdc
Elena <elena.cracea@gmail.com>
2023-02-23 10:45:32 -0700

    ADAPT-89: SAA Card add icons, fix missing bg

--- src/pages/membership/saacard.js
+++ src/pages/membership/saacard.js
@@ -1,10 +1,11 @@
 import React, { useState, useContext, useEffect } from 'react';
 import { dcnb } from 'cnbuilder';
+import { PrinterIcon, StarIcon, CashIcon } from '@heroicons/react/outline';
 import AuthContext from '../../contexts/AuthContext';
 import { SAACtaLink } from '../../components/cta/SAACtaLink';
 
 const SaaCard = () => {
-  const [bgColor, setBgColor] = useState({});
+  const [bgColor, setBgColor] = useState('su-bg-[#C3363A]');
   const [noCard, setNoCard] = useState(false);
   const [bgImage, setBgImage] = useState(null);
   const [logo, setLogo] = useState(null);
@@ -35,19 +36,17 @@ const SaaCard = () => {
         auth.userProfile.affiliations.filter((s) => s.includes('GSB'))
       ) {
         fetchImages('gsb-card-logo.png', 'gsb-card-bg.jpg');
-        setBgColor('#C3363A');
+        setBgColor('su-bg-[#C3363A]');
       } else if (
         auth.userProfile.affiliations.filter((s) => s.includes('SAA'))
       ) {
         // TODO: Replace names when the correct images will be added
         fetchImages('gsb-card-logo.png', 'gsb-card-bg.jpg');
-        setBgColor('#B30838');
+        setBgColor('su-bg-[#B30838]');
       } else {
         // TODO: Replace names when the correct images will be added
         fetchImages('gsb-card-logo.png', 'gsb-card-bg.jpg');
-        setBgColor({
-          bg: 'su-bg-[#E4D4B0]',
-        });
+        setBgColor('su-bg-[#C3363A]');
         setBgColor('#E4D4B0');
       }
     }
@@ -58,11 +57,11 @@ const SaaCard = () => {
       {auth.isAuthenticated && (
         <div className="su-cc">
           {!noCard && <h4>Your card</h4>}
-          <div className="xl:su-flex">
+          <div className="2xl:su-flex">
             <div
               className={dcnb(
                 'su-text-white su-relative su-overflow-hidden su-rounded-[100px] su-min-w-[900px] su-w-[900px] su-h-[567px] su-mb-20 xl:mb-0',
-                `su-bg-[${bgColor}]`
+                bgColor
               )}
             >
               <div className="su-relative su-flex su-flex-col su-justify-between su-h-full su-flex su-pt-[30px] su-text-[38px] su-z-10">
@@ -79,7 +78,7 @@ const SaaCard = () => {
                   </div>
                 )}
               </div>
-              <div className="su-absolute su-top-1/2 -su-translate-y-1/2 su-left-[40%] su-bg-digital-red su-w-full su-h-0 su-pt-[90%] su-overflow-hidden su-rounded-full">
+              <div className="su-absolute su-top-1/2 -su-translate-y-1/2 su-left-[40%] f su-w-full su-h-0 su-pt-[90%] su-overflow-hidden su-rounded-full">
                 <div className="su-absolute su-inset-0">
                   <img
                     className="su-absolute su-inset-0 su-min-h-full"
@@ -89,7 +88,7 @@ const SaaCard = () => {
                 </div>
               </div>
             </div>
-            <div className="su-flex su-items-center xl:su-rs-ml-7 print:su-hidden">
+            <div className="su-flex su-items-center 2xl:su-rs-ml-7 print:su-hidden">
               {noCard ? (
                 <div>
                   <div className="su-mb-10">
@@ -107,28 +106,40 @@ const SaaCard = () => {
                 </div>
               ) : (
                 <ul className="su-list-none su-p-0 su-m-0">
-                  <li>
+                  <li className="su-flex">
+                    <PrinterIcon className="su-w-26 su-mr-20" />
                     <SAACtaLink
                       linkText="Print your membership card"
                       trailingIcon="arrow-right"
                       size="small"
+                      isOutlineFaIcon
                       onClick={(e) => print(e)}
+                      className="!su-text-digital-red-light"
+                      textColor="all-black"
                     />
                   </li>
-                  <li>
+                  <li className="su-flex">
+                    <StarIcon className="su-w-26 su-mr-20" />
                     <SAACtaLink
                       link="#"
                       linkText="View membership benefits"
                       trailingIcon="arrow-right"
                       size="small"
+                      isOutlineFaIcon
+                      className="!su-text-digital-red-light"
+                      textColor="all-black"
                     />
                   </li>
-                  <li>
+                  <li className="su-flex">
+                    <CashIcon className="su-w-26 su-mr-20" />
                     <SAACtaLink
                       link="#"
                       linkText="Make an installment payment online"
                       trailingIcon="arrow-right"
+                      isOutlineFaIcon
                       size="small"
+                      className="!su-text-digital-red-light"
+                      textColor="all-black"
                     />
                   </li>
                 </ul>
