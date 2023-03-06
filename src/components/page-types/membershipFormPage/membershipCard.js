6a0a801e97c638fe44d8249ca1bf7c8f5f919301
rebeccahongsf <rebeccahongsf@gmail.com>
2023-03-06 14:36:44 -0800

    add aria

--- src/components/page-types/membershipFormPage/membershipCard.js
+++ src/components/page-types/membershipFormPage/membershipCard.js
@@ -11,7 +11,7 @@ const MembershipCard = ({
   initial,
   newContact = false,
   memberData,
-  props,
+  ...props
 }) => {
   const [state, dispatch] = useContext(FormContext);
   const { registrantsData } = state;
