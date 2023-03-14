1d7bc4d4eb70d47cb6f2ccc3d59f2a60e369d47c
Yvonne Tang <ytang2@stanford.edu>
2023-03-13 22:30:33 -0700

    userHeaderIcon remove unused items

--- src/components/navigation/userHeaderIcon.js
+++ src/components/navigation/userHeaderIcon.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { UserIcon } from '@heroicons/react/outline';
 import AuthContext from '../../contexts/AuthContext';
-import useDisplay from '../../hooks/useDisplay';
+import { FlexBox } from '../layout/FlexBox';
 import { Spinner } from './Spinner';
 import * as styles from './MainNav/mainNav.styles';
 
@@ -12,50 +12,32 @@ const Initial = ({ string, isAuthenticated, menuCircle }) => {
   return (
     <div className={classes} aria-hidden>
       {isAuthenticated && initial}
-      {!isAuthenticated && (
-        <UserIcon
-          className="su-w-20 su-h-20 su-flex su-self-center"
-          aria-hidden="true"
-        />
-      )}
+      {!isAuthenticated && <UserIcon className="su-w-20 su-h-20" aria-hidden />}
     </div>
   );
 };
 
-const UserHeaderIcon = ({ menuCircle }) => {
-  const { showDesktop } = useDisplay('xl');
+const UserHeaderIcon = ({ menuCircle }) => (
+  <AuthContext.Consumer>
+    {({ isAuthenticated, isAuthenticating, userProfile }) => (
+      <>
+        {/* Mobile Spinner in place of avatar */}
+        {isAuthenticating && <Spinner className="lg:su-hidden" />}
 
-  return (
-    <AuthContext.Consumer>
-      {({ isAuthenticated, isAuthenticating, userProfile }) => (
-        <>
-          {/* Mobile Spinner in place of avatar */}
-          {isAuthenticating && <Spinner className="lg:su-hidden" />}
-
-          {!isAuthenticating && (
-            <div className="su-flex su-items-center su-group su-relative">
-              {isAuthenticated && (
-                <span
-                  className={`su-inline-block su-mr-10 ${
-                    showDesktop ? '' : 'su-hidden'
-                  }`}
-                >{`Hi, ${userProfile?.name?.fullNameParsed?.firstName} ${userProfile?.name?.fullNameParsed?.lastName}`}</span>
-              )}
-
-              <Initial
-                isAuthenticated={isAuthenticated}
-                menuCircle={menuCircle}
-                string={
-                  isAuthenticated &&
-                  userProfile?.name?.fullNameParsed?.firstName
-                }
-              />
-            </div>
-          )}
-        </>
-      )}
-    </AuthContext.Consumer>
-  );
-};
+        {!isAuthenticating && (
+          <FlexBox alignItems="center" className="su-group su-relative">
+            <Initial
+              isAuthenticated={isAuthenticated}
+              menuCircle={menuCircle}
+              string={
+                isAuthenticated && userProfile?.name?.fullNameParsed?.firstName
+              }
+            />
+          </FlexBox>
+        )}
+      </>
+    )}
+  </AuthContext.Consumer>
+);
 
 export default UserHeaderIcon;
