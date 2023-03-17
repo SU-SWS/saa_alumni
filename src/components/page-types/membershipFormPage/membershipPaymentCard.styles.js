619771f2a5cae86c0374583420af2b94bf034eb0
rebeccahongsf <rebeccahongsf@gmail.com>
2023-03-17 13:01:15 -0700

    ADAPTSM-165 | fixup headings

--- src/components/page-types/membershipFormPage/membershipPaymentCard.styles.js
+++ src/components/page-types/membershipFormPage/membershipPaymentCard.styles.js
@@ -4,7 +4,6 @@ export const membershipPaymentCardWrapper =
   'su-basefont-23 su-rs-px-3 su-rs-pt-1 su-rs-pb-4 su-stretch-link su-w-full su-transition-all su-rounded su-border su-border-3 su-border-white hocus:su-gradient-border hocus:su-border-to-rt-palo-verde-dark-to-saa-electric-blue focus:su-outline-4 focus:su-outline-offset-4';
 export const initialAndSelectionWrapper =
   'su-flex-col lg:su-flex-row su-items-center su-gap-xs su-relative';
-export const heading = 'su-type-2 su-font-bold su-rs-mt-1 su-mb-0';
 export const subheading = 'su-mb-0';
 export const membershipPaymentCardLink =
   'su-group su-flex su-items-end su-text-18 md:su-text-24 su-font-regular su-no-underline su-text-white su-px-20 su-pt-10 su-pb-11 md:su-px-30 md:su-pt-16 md:su-pb-18 su-border-solid su-border-3 su-transition-colors su-gradient-border su-border-to-rt-palo-verde-dark-to-saa-electric-blue su-bg-transparent group-hocus:su-text-white group-hocus:su-bg-gradient-to-tr group-hocus:su-from-palo-verde-dark group-hocus:su-to-saa-electric-blue group-hocus:su-shadow-md';
