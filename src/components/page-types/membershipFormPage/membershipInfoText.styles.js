fcbc00deeb78c8e797798fbe8257f136c6126f88
rebeccahongsf <rebeccahongsf@gmail.com>
2023-03-20 13:45:52 -0700

    fixup benefits button

--- src/components/page-types/membershipFormPage/membershipInfoText.styles.js
+++ src/components/page-types/membershipFormPage/membershipInfoText.styles.js
@@ -1,4 +1,2 @@
 export const bodyText = 'su-card-paragraph su-rs-mb-3';
-export const benefitsLink =
-  'su-group su-flex su-items-end su-text-18 md:su-text-24 su-font-regular su-no-underline su-border-3 su-border-digital-red-xlight su-text-white hocus:su-bg-digital-red hocus:su-border-digital-red hocus:su-text-white hocus:su-shadow-md su-px-20 su-pt-10 su-pb-11 md:su-px-30 md:su-pt-16 md:su-pb-18';
-export const benefitsLinkIcon = 'su-w-1em su-text-white';
+export const benefitsLink = 'hocus:su-border-cardinal-red hocus:su-text-white';
