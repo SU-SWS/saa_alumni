837b480cd8748c31a452438590076a91918668f1
rebeccahongsf <rebeccahongsf@gmail.com>
2023-04-19 12:13:14 -0700

    remove height

--- src/tailwind/plugins/base/base.js
+++ src/tailwind/plugins/base/base.js
@@ -6,7 +6,6 @@ module.exports = function () {
   return function ({ addBase, config }) {
     addBase({
       html: {
-        height: '100%',
         overflowY: 'visible !important', // Need this for sticky nav to work
       },
       body: {
