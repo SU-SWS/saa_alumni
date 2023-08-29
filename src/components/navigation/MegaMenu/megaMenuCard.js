14c3596c477ce3bebe522b7e977f025c34ce3c8f
rebeccahongsf <rebeccahongsf@gmail.com>
2023-08-28 17:40:30 -0700

    style mega menu card image

--- src/components/navigation/MegaMenu/megaMenuCard.js
+++ src/components/navigation/MegaMenu/megaMenuCard.js
@@ -20,28 +20,28 @@ const MegaMenuCard = (
   props
 ) => (
   <SbEditable content={blok}>
-    <article className="su-w-full">
-      <SbLink link={link} classes="su-no-underline">
+    <article className="su-w-fit">
+      <SbLink link={link} classes="su-no-underline su-group">
         {filename != null && (
-          <CardImage
-            {...props}
-            filename={filename}
-            size="vertical"
-            loading="lazy"
-            imageFocus="center"
-            className="su-overflow-hidden su-aspect-[3/2] su-w-full"
-          />
+          <div className="su-overflow-hidden su-aspect-[3/2] ">
+            <CardImage
+              {...props}
+              filename={filename}
+              size="vertical"
+              loading="lazy"
+              imageFocus="center"
+              className="su-w-full su-object-cover su-w-full su-h-full su-transition-transform su-transform-gpu group-hover:su-scale-[1.03] group-focus-within:su-scale-[1.03] su-object-center"
+            />
+          </div>
         )}
-        <div
-          className={`su-text-white su-rs-px-2 su-rs-pt-2 su-rs-pb-3 su-bg-${backgroundColor}`}
-        >
+        <div className="su-text-white su-rs-px-2 su-rs-pt-2 su-rs-pb-3 su-bg-digital-red group-hover:su-bg-cardinal-red group-focus:su-bg-cardinal-red">
           <Heading size="2" level={3} font="serif" weight="bold">
             {headline}
           </Heading>
-          <p className="su-mb-0 su-flex su-items-center">
+          <p className="su-mb-0">
             {ctaText}
             <HeroIcon
-              className="su-h-full"
+              className="su-inline-block"
               isAnimate
               iconType={link.linktype === 'url' ? 'external' : 'chevron-right'}
             />
