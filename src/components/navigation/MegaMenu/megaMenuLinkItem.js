579c30a389acd2a91a3abc8bac88e87506d21e93
rebeccahongsf <rebeccahongsf@gmail.com>
2023-08-29 16:43:52 -0700

    fixup

--- src/components/navigation/MegaMenu/megaMenuLinkItem.js
+++ src/components/navigation/MegaMenu/megaMenuLinkItem.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import SbEditable from 'storyblok-react';
 import SbLink from '../../../utilities/sbLink';
-import * as styles from './megaMenuLinkItem.styles';
 
 const MegaMenuLinkItem = ({ blok: { link, linkText }, blok }) => (
   <SbEditable content={blok}>
     <li className="su-rs-mb-0">
       <SbLink
         link={link}
-        classes={styles.menuLink}
+        // classes={styles.menuLink}
         hasExternalIcon
-        externalIconClasses={styles.externalIcon}
+        // externalIconClasses={styles.externalIcon}
       >
         {linkText}
       </SbLink>
