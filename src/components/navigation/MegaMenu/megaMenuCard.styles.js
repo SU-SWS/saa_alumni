6c854f7742fa57054208a9c6540bcca48bbc10f0
rebeccahongsf <rebeccahongsf@gmail.com>
2023-09-05 16:02:21 -0700

    create seperate mega menu card stylesheet

---
+++ src/components/navigation/MegaMenu/megaMenuCard.styles.js
@@ -0,0 +1,6 @@
+export const cardImageWrapper =
+  'su-hidden lg:su-block su-overflow-hidden su-aspect-[3/2] ';
+export const cardImageStyles =
+  'su-w-full su-object-cover su-w-full su-h-full su-transition-transform su-transform-gpu group-hover:su-scale-[1.03] group-focus-within:su-scale-[1.03] su-object-center';
+export const cardContent =
+  'su-text-white su-rs-px-2 su-rs-pt-2 su-rs-pb-3 su-bg-digital-red group-hover:su-bg-cardinal-red group-focus:su-bg-cardinal-red';
