a6d06ad740cfc61dca261ec09ecc79f67bb8b43e
rebeccahongsf <rebeccahongsf@gmail.com>
2023-09-06 15:58:36 -0700

    reduce padding on lg and add word break

--- src/components/navigation/MegaMenu/megaMenuCard.styles.js
+++ src/components/navigation/MegaMenu/megaMenuCard.styles.js
@@ -3,4 +3,4 @@ export const cardImageWrapper =
 export const cardImageStyles =
   'su-w-full su-object-cover su-w-full su-h-full su-transition-transform su-transform-gpu group-hover:su-scale-[1.03] group-focus-within:su-scale-[1.03] su-object-center';
 export const cardContent =
-  'su-text-white su-rs-px-2 su-rs-pt-2 su-rs-pb-3 su-bg-digital-red group-hover:su-bg-cardinal-red group-focus:su-bg-cardinal-red';
+  'su-text-white su-rs-px-2 su-rs-pt-2 su-rs-pb-3 lg:su-rs-px-1 lg:su-rs-pt-1 lg:su-rs-pb-2 su-break-words su-bg-digital-red group-hover:su-bg-cardinal-red group-focus:su-bg-cardinal-red';
