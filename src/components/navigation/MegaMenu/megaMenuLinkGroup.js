e39afcfc4c3635b49813786bf5d97a1124aa4063
rebeccahongsf <rebeccahongsf@gmail.com>
2023-09-11 12:26:03 -0700

    remove secondary header link

--- src/components/navigation/MegaMenu/megaMenuLinkGroup.js
+++ src/components/navigation/MegaMenu/megaMenuLinkGroup.js
@@ -2,23 +2,16 @@ import React from 'react';
 import SbEditable from 'storyblok-react';
 import CreateBloks from '../../../utilities/createBloks';
 import { Heading } from '../../simple/Heading';
-import SbLink from '../../../utilities/sbLink';
 import * as styles from './megaMenuLinkGroup.styles';
 import { GridCell } from '../../layout/GridCell';
 
-const MegaMenuLinkGroup = ({
-  blok: { heading, secondaryLink, links },
-  blok,
-  onlyLinks,
-}) => (
+const MegaMenuLinkGroup = ({ blok: { heading, links }, blok, onlyLinks }) => (
   <SbEditable content={blok}>
     <GridCell lg={4} className={styles.menuGroup}>
       {heading && (
-        <SbLink link={secondaryLink} classes={styles.menuLink}>
-          <Heading weight="bold" uppercase className="su-text-17 su-rs-mb-0">
-            {heading}
-          </Heading>
-        </SbLink>
+        <Heading weight="bold" uppercase className={styles.menuHeader}>
+          {heading}
+        </Heading>
       )}
       {links !== '' && (
         <ul className="su-list-none su-p-0">
--- src/components/navigation/MegaMenu/megaMenuLinkGroup.styles.js
+++ src/components/navigation/MegaMenu/megaMenuLinkGroup.styles.js
@@ -1,4 +1,3 @@
 export const menuGroup =
   'su-pb-32 last:su-pb-0 lg:su-pb-0 su-border-b su-border-black-40 last:su-border-0 lg:su-border-0';
-export const menuLink =
-  'su-text-black hocus:su-text-digital-red su-no-underline hocus:su-underline';
+export const menuHeader = 'su-text-17 su-rs-mb-0 su-text-black';
