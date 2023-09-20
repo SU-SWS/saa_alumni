89ec4b3251b5b5cf7f1108f112e50a7d187f8cf8
rebeccahongsf <rebeccahongsf@gmail.com>
2023-09-20 12:01:59 -0700

    fixup bouncing dropdown menu links (thanks @yvonnetangsu)

--- src/components/navigation/MegaMenu/megaMenuLink.styles.js
+++ src/components/navigation/MegaMenu/megaMenuLink.styles.js
@@ -1,4 +1,3 @@
-export const menuLink = '';
-// 'su-flex su-leading-display su-text-black hocus:su-text-digital-red su-no-underline hocus:su-underline su-font-normal su-text-19 su-group';
-
+export const menuLink =
+  'su-inline-block su-leading-display su-text-black hocus:su-text-digital-red su-no-underline hocus:su-underline su-font-normal su-text-19 su-group su-backface-hidden';
 export const externalIcon = 'su-text-digital-red su-text-21';
