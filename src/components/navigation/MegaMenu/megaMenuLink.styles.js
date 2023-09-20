68bd709ffce1ecde3853d618b675572e0047f73b
rebeccahongsf <rebeccahongsf@gmail.com>
2023-09-20 11:03:45 -0700

    switch inline-block to flex for links

--- src/components/navigation/MegaMenu/megaMenuLink.styles.js
+++ src/components/navigation/MegaMenu/megaMenuLink.styles.js
@@ -1,4 +1,4 @@
 export const menuLink =
-  'su-inline-block su-leading-display su-text-black hocus:su-text-digital-red su-no-underline hocus:su-underline su-font-normal su-text-19 su-group';
+  'su-flex su-leading-display su-text-black hocus:su-text-digital-red su-no-underline hocus:su-underline su-font-normal su-text-19 su-group';
 
 export const externalIcon = 'su-text-digital-red su-text-21';
