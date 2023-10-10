fb331f9fbd98bf1506ff3c4e986ef88b6ae81daa
rebeccahongsf <rebeccahongsf@gmail.com>
2023-10-10 15:17:20 -0700

    add destination filter test

--- cypress/e2e/3-travel-study/destinations-filter.cy.ts
+++ cypress/e2e/3-travel-study/destinations-filter.cy.ts
@@ -12,9 +12,26 @@ describe('Travel-Study Destinations Page', () => {
     // Confirm form exists
     cy.get('h1').should('contain.text', 'Destinations');
 
-    // Confirm that Iran card exists
+    // Confirm that Iran trip card exists
     cy.get('h3').should('contain.text', 'Iran');
 
+    // Select Filter Month
+    cy.get('[data-test="filter-option--october"]').check();
+
+    // Confirm that Canada trip card exists
+    cy.get('h3').should('contain.text', 'Canada');
+
+    // Clear Filters
+    cy.get('[data-test="filter-btn--clear-all"]').click();
+
+    //  Select Filter Experience
+    cy.get('[data-test="filter-option--family-focused"]').check();
+
+    // Confirm that Southeast Asia trip card exists
+    cy.get('h3').should('contain.text', 'Southeast');
+
+    // Clear Filters
+    cy.get('[data-test="filter-btn--clear-all"]').click();
   });
     
 });
