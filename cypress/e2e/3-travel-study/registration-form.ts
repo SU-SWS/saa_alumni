1531dc7b5e0071ba153fc0d813d56c5fbe02362a
rebeccahongsf <rebeccahongsf@gmail.com>
2023-10-10 14:15:54 -0700

    add t/s reg form test

--- cypress/e2e/3-travel-study/registration-form.ts
+++ cypress/e2e/3-travel-study/registration-form.ts
@@ -10,6 +10,19 @@ describe('Membership Registration Form Page', () => {
     cy.url().should('include', '/travel-study/destinations/finland-2022/finland-reg-form/');
 
     cy.get('h3').first().should('contain.text', 'Before you register');
+  })
+
+  it('should be able to navigate to givegab form', () => {
+    // Login
+    cy.login();
+
+    // Visit the membership form page URL
+    cy.visit('/travel-study/destinations/finland-2022/finland-reg-form/');
+
+    // Confirm that the URL matches the expected URL
+    cy.url().should('include', '/travel-study/destinations/finland-2022/finland-reg-form/');
+
+    cy.get('h3').first().should('contain.text', 'Before you register');
 
     // Primary card should exist
     cy.get('[data-test="card-teri-dactyl"]').should('exist');
@@ -27,8 +40,52 @@ describe('Membership Registration Form Page', () => {
 
     cy.get('[data-test="list-allie-grater"]').should('exist')
 
-    // Check if the promo code field exists on the page
+    // Check if next button exists and click to continue to GG form
     cy.get('[next-btn]').should('exist').click();
+
+    // Confirm that the URL matches the expected URL
+    cy.url().should('include', '/travel-study/destinations/finland-2022/finland-reg-form/form');
+
+    // Confirm form exists
+    cy.get('[id="su-embed"]').should('exist');
+    cy.get('form').should('exist');
+    
+    // Confirm that the user's name is prefilled
+    cy.get('[data-fieldid="DigitalName"]').should('contain.value', 'Teri Dactyl');
+    
+    // Confirm email is prefilled
+    cy.get('[data-fieldid="ContactEmail"]').should('contain.value', 'tdactyl@test.com');
+
+    // Confirm phone number is prefilled
+    cy.get('[data-fieldid="PhoneNumber"]').should('contain.value', '4081111111');
+  
+    // Fill required questions
+    cy.get('[data-fieldid="Pre-TripExtension__0"]').click();
+    cy.get('[data-fieldid="Post-TripExtension__0"]').click();
+
+    // Confirm that button exists
+    cy.get('button').should('have.class', 'ggeButton--forward').click();
+
+    // Confirm that the user contact's name is prefilled
+    cy.get('[data-fieldid="DigitalName"]').should('contain.value', 'Allie Grater');
+    
+    // Fill email input
+    cy.get('[data-fieldid="ContactEmail"]').type('tdactyl@test.com');
+    cy.get('data-fieldid="EmailType__0"').click();
+
+    // Fill phone number
+    cy.get('[data-fieldid="PhoneNumber"]').type('1234567890');
+  
+    // Fill required questions
+    cy.get('[data-fieldid="Pre-TripExtension__0"]').click();
+    cy.get('[data-fieldid="Post-TripExtension__0"]').click();
+
+    // Confirm that button exists
+    cy.get('button').should('have.class', 'ggeButton--forward').click();
+
+    // Confirm that tally table exists
+    cy.get('table').should('have.class', 'ggeTallyTable');
+
   });
 
   it('should redirect to the interstitial form page at /travel-study/destinations/finland-2022/finland-reg-form', () => {
