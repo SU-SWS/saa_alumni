9c9fe20dfbf5ef649e7765962225e064e3748016
rebeccahongsf <rebeccahongsf@gmail.com>
2023-10-10 12:03:32 -0700

    add membership card test

---
+++ cypress/e2e/4-membership/saa-membership-card.cy.ts
@@ -0,0 +1,33 @@
+describe('Membership SAA Card Page', () => {
+  it('should display message to login to view card at /membership/saacard', () => {
+    // Visit the membership form page URL
+    cy.visit('/membership/saacard');
+
+    // Confirm that the URL matches the expected URL
+    cy.url().should('include', '/membership/saacard');
+
+    cy.get('h1').should('contain.text', 'Your Stanford Alumni Association Membership');
+    cy.get('h3').should('contain.text', 'Are you a Stanford Alumni Association member looking for your digital membership card?');
+
+    // Check if the promo code field exists on the page
+    cy.get('a').should('contain.text', 'Log in'); 
+  });
+
+  it('should load the saa membership card page at /membership/saacard', () => {
+    // Login
+    cy.login();
+
+    // Visit the membership form page URL
+    cy.visit('/membership/saacard');
+
+    // Confirm that the URL matches the expected URL
+    cy.url().should('include', '/membership/saacard');
+
+    cy.get('h1').should('contain.text', 'Your Stanford Alumni Association Membership');
+
+    // Check if the saa card image exists
+    cy.get('img').should('exist'); 
+
+    cy.get('p').should('contain.text', 'You are not currently a Stanford Alumni Association'); 
+  });
+});
