3fd4e4b9e8b056b8b27039e417bd7b386f4283dd
rebeccahongsf <rebeccahongsf@gmail.com>
2023-11-03 11:12:25 -0700

    adjust destinations page check

--- cypress/e2e/3-travel-study/destinations-filter.cy.ts
+++ cypress/e2e/3-travel-study/destinations-filter.cy.ts
@@ -1,5 +1,5 @@
 describe('Travel-Study Destinations Page', () => {
-  it('should load the destinations page at /travel-study/destinations/', () => {
+  it('should load at /travel-study/destinations/', () => {
     // Login
     cy.login();
 
@@ -20,7 +20,7 @@ describe('Travel-Study Destinations Page', () => {
     cy.reload(); // Needed for local Gatsby build
 
     // Confirm that a trip card exists
-    cy.get('article h3').should('exist');
+    cy.get('.trip-filter-page article h3').should('exist');
 
     // Clear Filters
     cy.get('[data-test="filter-btn--clear-all"]').first().click({force: true});
@@ -31,7 +31,33 @@ describe('Travel-Study Destinations Page', () => {
     cy.reload(); // Needed for local Gatsby build
 
     // Confirm that Southeast Asia trip card exists
-    cy.get('h3').should('contain.text', 'Southeast');
+    cy.get('.trip-filter-page article h3').should('contain.text', 'Southeast');
+
+    // Clear Filters
+    cy.get('[data-test="filter-btn--clear-all"]').first().click({force: true});
+    cy.reload(); // Needed for local Gatsby build
+  });
+  it('should have working filters', () => {
+    // Visit the destinations page URL
+    cy.visit('/travel-study/destinations/');
+
+    // Select Filter Month
+    cy.get('[data-test="filter-option--october"]').first().check({ force: true });
+    cy.reload(); // Needed for local Gatsby build
+
+    // Confirm that a trip card exists
+    cy.get('.trip-filter-page article h3').should('exist');
+
+    // Clear Filters
+    cy.get('[data-test="filter-btn--clear-all"]').first().click({force: true});
+    cy.reload(); // Needed for local Gatsby build
+
+    //  Select Filter Experience
+    cy.get('[data-test="filter-option--family-focused"]').first().check({force: true});
+    cy.reload(); // Needed for local Gatsby build
+
+    // Confirm that Southeast Asia trip card exists
+    cy.get('.trip-filter-page article h3').should('contain.text', 'Southeast');
 
     // Clear Filters
     cy.get('[data-test="filter-btn--clear-all"]').first().click({force: true});
