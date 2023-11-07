bc57c189c8ea5f4df7229a1ee9f9679a295ca83f
rebeccahongsf <rebeccahongsf@gmail.com>
2023-11-07 13:20:52 -0800

    cleanup destinations test

--- cypress/e2e/3-travel-study/destinations-filter.cy.ts
+++ cypress/e2e/3-travel-study/destinations-filter.cy.ts
@@ -1,8 +1,5 @@
 describe('Travel-Study Destinations Page', () => {
   it('should load at /travel-study/destinations/', () => {
-    // Login
-    cy.login();
-
     // Visit the destinations page URL
     cy.visit('/travel-study/destinations/');
 
@@ -13,29 +10,7 @@ describe('Travel-Study Destinations Page', () => {
     cy.get('h1').should('contain.text', 'Destinations');
 
     // Confirm that a trip card exists
-    cy.get('article h3').should('exist');
-
-    // Select Filter Month
-    cy.get('[data-test="filter-option--october"]').first().check({ force: true });
-    cy.reload(); // Needed for local Gatsby build
-
-    // Confirm that a trip card exists
     cy.get('.trip-filter-page article h3').should('exist');
-
-    // Clear Filters
-    cy.get('[data-test="filter-btn--clear-all"]').first().click({force: true});
-    cy.reload(); // Needed for local Gatsby build
-
-    //  Select Filter Experience
-    cy.get('[data-test="filter-option--family-focused"]').first().check({force: true});
-    cy.reload(); // Needed for local Gatsby build
-
-    // Confirm that Southeast Asia trip card exists
-    cy.get('.trip-filter-page article h3').should('contain.text', 'Southeast');
-
-    // Clear Filters
-    cy.get('[data-test="filter-btn--clear-all"]').first().click({force: true});
-    cy.reload(); // Needed for local Gatsby build
   });
   it('should have working filters', () => {
     // Visit the destinations page URL
