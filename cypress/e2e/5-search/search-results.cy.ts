53076ddbce8032a42bc9efacc980146eea00b17f
rebeccahongsf <rebeccahongsf@gmail.com>
2023-11-07 13:12:57 -0800

    combine no search results test into search results

--- cypress/e2e/5-search/search-results.cy.ts
+++ cypress/e2e/5-search/search-results.cy.ts
@@ -30,4 +30,15 @@ describe('Search Results', () => {
     cy.reload() // Needed for local Gatsby build
     cy.url().should('include', '/search/?q=travel%20study&site=Alumni');
   });
+  it('should return no search results when searching lorem ipsum', () => {
+    cy.visit('/search/?q=lorem%20ipsum');
+    cy.reload() // Needed for local Gatsby build
+
+    cy.get('h1').should('contain.text', 'Search for...');
+    cy.get('section > div form input[data-test="search--modal-input"]').should('contain.value','lorem ipsum');
+
+    cy.get('h2').should('contain.text', 'We’re sorry, we couldn’t find results for “lorem ipsum”.')
+    cy.get('h3').should('contain.text', 'Consider Browsing by Category:')
+    cy.get('h4').should('contain.text', 'Popular Topics')
+  });
 });
