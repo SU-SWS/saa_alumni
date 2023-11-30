d636823064b852f4d7c1c0906dd2a8c9e4edeb3e
William Francis Misiaszek <wmisiasz@gmail.com>
2023-11-29 16:33:48 -0800

    v1.1

--- src/api/stanfordcard/testPassGeneration.js
+++ src/api/stanfordcard/testPassGeneration.js
@@ -1,49 +1,35 @@
-const http = require('http');
+// testPassGeneration.js (Should be outside of your function directory, e.g., in a scripts directory)
+const fetch = require('node-fetch');
 const fs = require('fs');
 const path = require('path');
 
-// The options should be adjusted to target the local Netlify dev server
-const options = {
-  hostname: 'localhost',
-  port: 8888, // Default port for Netlify Dev
-  path: '/.netlify/functions/generate-pass', // Path to access Netlify Functions locally
+// Local server endpoint
+const url = 'http://localhost:64946/.netlify/functions/generate-pass';
+
+// Data to send to the endpoint
+const data = {
+  membershipNumber: '123456789',
+  firstName: 'John',
+  lastName: 'Doe',
+};
+
+fetch(url, {
   method: 'POST',
   headers: {
     'Content-Type': 'application/json',
   },
-};
-
-// The test data should reflect the expected input of the Netlify Function
-const data = JSON.stringify({
-  membershipNumber: '12345',
-  firstName: 'Jane',
-  lastName: 'Doe',
-});
-
-// Send a request to the local Netlify dev server to invoke the function
-const req = http.request(options, (res) => {
-  console.log(`statusCode: ${res.statusCode}`);
-
-  // Handle the response, save it to a file if the status code is 200
-  if (res.statusCode === 200) {
-    const file = fs.createWriteStream(path.join(__dirname, 'testPass.pkpass'));
-    res.pipe(file);
-
-    file.on('finish', () => {
-      file.close();
-      console.log('Pass downloaded successfully');
-    });
-  } else {
-    res.on('data', (d) => {
-      process.stdout.write(d);
-    });
+  body: JSON.stringify(data),
+})
+.then((res) => {
+  if (!res.ok) {
+    throw new Error(`Server responded with ${res.status}`);
   }
+  return res.buffer();
+})
+.then((buffer) => {
+  fs.writeFileSync(path.resolve(__dirname, 'testPass.pkpass'), buffer);
+  console.log('Pass downloaded successfully');
+})
+.catch((err) => {
+  console.error('Error generating pass:', err);
 });
-
-req.on('error', (error) => {
-  console.error(error);
-});
-
-// Write the test data and end the request
-req.write(data);
-req.end();
--- src/netlify/functions/generate-pass.js
+++ src/netlify/functions/generate-pass.js
@@ -1,17 +1,64 @@
-// Node.js backend script
-const express = require('express');
+// src/netlify/functions/generate-pass.js
+const { Pass } = require('passkit-generator');
 const fs = require('fs');
 const path = require('path');
 
-const app = express();
+exports.handler = async (event, context) => {
+  if (event.httpMethod !== 'POST') {
+    return {
+      statusCode: 405,
+      body: 'Method Not Allowed',
+    };
+  }
 
-// Endpoint to serve the test.txt file
-app.get('/generate-pass', (req, res) => {
-    const filePath = path.join(__dirname, 'test.txt');
-    res.sendFile(filePath);
-});
+  try {
+    const { membershipNumber, firstName, lastName } = JSON.parse(event.body);
 
-const PORT = process.env.PORT || 8000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+    // Corrected file path
+    const passJsonPath = path.join(__dirname, '..', '..', 'stanford.pass', 'pass.json');
+    const template = fs.readFileSync(passJsonPath, 'utf8');
+    const passData = JSON.parse(template);
+
+    // Update pass data with dynamic values
+    passData.serialNumber = membershipNumber;
+    passData.barcode.message = membershipNumber;
+    passData.coupon.primaryFields[0].value = `${firstName} ${lastName}`;
+
+    // Generate pass
+    const pass = new Pass({
+      model: passData,
+      certificates: {
+        wwdr: process.env.APPLE_WWDR_CERTIFICATE_PATH,
+        signerCert: process.env.PASS_SIGNING_CERTIFICATE_PATH,
+        signerKey: {
+          keyFile: process.env.PASS_SIGNING_CERTIFICATE_PATH,
+          passphrase: process.env.PASS_CERTIFICATE_PASSWORD,
+        },
+      },
+    });
+
+    // Add files to pass
+    pass.images.icon = fs.readFileSync(path.join(__dirname, '..', '..', 'stanford.pass', 'icon.png'));
+
+    // Generate and send pass
+    const stream = await pass.generate();
+    const buffers = [];
+    for await (const chunk of stream) {
+      buffers.push(chunk);
+    }
+    const passBuffer = Buffer.concat(buffers);
+
+    return {
+      statusCode: 200,
+      headers: { 'Content-type': 'application/vnd.apple.pkpass' },
+      body: passBuffer.toString('base64'),
+      isBase64Encoded: true,
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Server Error' }),
+    };
+  }
+};
