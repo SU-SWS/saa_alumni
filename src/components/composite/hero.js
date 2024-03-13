d937314df3c687a3c06accd63066eaa9065915db
Shirley Chang <schang11@stanford.edu>
2024-03-13 09:39:55 -0700

    Makes 'Scroll to explore' text black for white gradient on screens larger than xs

--- src/components/composite/hero.js
+++ src/components/composite/hero.js
@@ -112,7 +112,7 @@ const Hero = ({
               </div>
             )}
             {!isHideScroll && (
-              <div className="su-text-center su-text-white su-grow-0 su-rs-mt-5 su-font-serif su-font-regular su-text-19 md:su-text-22">
+              <div className={dcnb("su-text-center su-text-white su-grow-0 su-rs-mt-5 su-font-serif su-font-regular su-text-19 md:su-text-22", blackText)}>
                 <p className="su-mb-02em">Scroll to explore</p>
                 <a
                   href="#page-title"
