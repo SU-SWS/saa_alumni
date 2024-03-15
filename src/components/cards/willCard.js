9e84cbca4d79b0872ab8c00f52187b26cd891833
William Francis Misiaszek <wmisiasz@gmail.com>
2024-03-15 15:59:35 -0700

    no QR Code atm.

--- src/components/cards/willCard.js
+++ src/components/cards/willCard.js
@@ -1,13 +1,9 @@
-import React, { useState, useContext, useEffect } from 'react';
+import React from 'react';
 import SbEditable from 'storyblok-react';
 import { dcnb } from 'cnbuilder';
-import QRCode from 'qrcode.react';
-import { Grid } from '../layout/Grid';
 import CardImage from '../media/cardImage';
 import CircularImage from '../media/circularImage';
 import BasicCardContent from './basicCardContent';
-import appleWalletBadge from '../../assets/apple-wallet-badge.svg';
-import AuthContext from '../../contexts/AuthContext';
 
 const WillCard = ({
   blok: {
@@ -16,6 +12,7 @@ const WillCard = ({
     isRound,
     isMinimal,
     isBigHeadline,
+    align,
     image: { filename, focus } = {},
     imageFocus,
     headline,
@@ -25,77 +22,12 @@ const WillCard = ({
   blok,
   isDark,
 }) => {
-  const [downloadUrl, setDownloadUrl] = useState('');
-  const [userData, setUserData] = useState(null);
-  const auth = useContext(AuthContext);
-
-  useEffect(() => {
-    if (auth.userProfile) {
-      const { firstName, lastName, membershipNumber } = auth.userProfile;
-      setUserData({ firstName, lastName, membershipNumber });
-    }
-  }, [auth.userProfile]);
-
-  useEffect(() => {
-    if (downloadUrl) {
-      // Create an anchor tag and trigger download
-      const link = document.createElement('a');
-      link.href = downloadUrl;
-      link.download = 'membershipCard.pkpass'; // This can be omitted to use the filename from the Content-Disposition header
-      document.body.appendChild(link); // Append to body
-      link.click(); // Simulate click to trigger download
-      document.body.removeChild(link); // Clean up
-    }
-  }, [downloadUrl]); // This useEffect hook runs whenever downloadUrl changes
-
-  const handleAddToWallet = async () => {
-    if (!userData) {
-      console.error('User data not available');
-      alert('User data is not available. Please log in.');
-      return;
-    }
-
-    try {
-      const { membershipNumber, firstName, lastName } = userData;
-
-      const response = await fetch('/api/applewalletcard/generate-pass', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ membershipNumber, firstName, lastName }),
-      });
-
-      if (!response.ok) {
-        console.error(
-          `Error from server: ${response.statusText} (${response.status})`
-        );
-        alert(
-          `Failed to generate pass: ${response.statusText}. Please try again later.`
-        );
-        return;
-      }
-
-      const passData = await response.blob();
-      if (passData.size === 0) {
-        console.error('Received an empty pass file from the server.');
-        alert('Failed to generate the pass correctly. Please contact support.');
-        return;
-      }
-
-      const url = URL.createObjectURL(passData);
-      setDownloadUrl(url);
-    } catch (error) {
-      console.error('Error generating pass:', error);
-      alert(
-        `Error generating pass: ${error.message}. Please check the console for more details.`
-      );
-    }
-  };
-
   // Default wrapper classes for white, non-minimal cards
   let wrapperClasses =
-    'su-bg-white su-text-black su-border su-border-solid su-border-black-30-opacity-40 su-bg-clip-padding su-shadow-sm su-rs-pt-2 su-rs-px-2 su-rs-pb-3';
+    'su-bg-white su-text-black su-border su-border-solid su-border-black-30-opacity-40 su-bg-clip-padding su-shadow-sm';
+
+  // Card content padding for non-minimal cards
+  let bodyPadding = 'su-rs-px-2 su-rs-pt-2 su-rs-pb-4';
 
   // Basic card image has aspect ratio 3x2 for non-round option
   let cardImage = (
@@ -113,11 +45,15 @@ const WillCard = ({
   );
 
   if (isRound && filename) {
+    // If image is round, we need to add padding above the image
+    wrapperClasses = dcnb('su-rs-pt-3', wrapperClasses);
+
     cardImage = (
       <CircularImage
         borderColor={borderColor}
         filename={filename}
         smartFocus={focus}
+        className={isMinimal ? '' : 'su-rs-ml-2'}
         loading="lazy"
         width="126"
         height="126"
@@ -126,29 +62,30 @@ const WillCard = ({
   }
 
   if (isMinimal) {
-    wrapperClasses = '';
+    wrapperClasses = 'su-max-w-600';
+    bodyPadding = '';
+
+    // Add top padding to content if the minimal card has an image
+    if (filename) {
+      bodyPadding = 'su-rs-pt-2';
+    }
   }
 
-  let cardGrid;
+  // Content alignment including image and CTA, default is left-align
+  // This setting overrides the alignment option in the nested CTA
+  let bodyAlign = 'su-items-start';
 
-  if (filename) {
-    cardGrid = 'md:su-grid-cols-2 print:su-grid-cols-2';
-
-    if (isRound) {
-      cardGrid =
-        'md:su-grid-flow-col print:su-grid-flow-col md:su-grid-cols-auto-1fr print:su-grid-cols-auto-1fr';
-    }
+  if (align === 'center') {
+    wrapperClasses = dcnb(wrapperClasses, 'children:su-mx-auto su-text-center');
+    bodyAlign = 'su-items-center';
   }
 
   return (
     <SbEditable content={blok}>
-      <Grid
-        gap
-        alignItems="start"
+      <div
         className={dcnb(
-          'basic-card-horizontal su-gap-x-xl su-w-full su-basefont-23 su-break-words',
-          wrapperClasses,
-          cardGrid
+          'will-card su-w-full su-basefont-23 su-break-words', 
+          wrapperClasses
         )}
       >
         {filename?.startsWith('http') && cardImage}
@@ -159,44 +96,9 @@ const WillCard = ({
           isDark={isDark && isMinimal}
           text={text}
           cta={cta}
-          className={dcnb(
-            'card-body',
-            `${isRound && filename ? '' : 'su-mt-[-0.3em]'}`
-          )}
+          className={dcnb('card-body', bodyPadding, bodyAlign)}
         />
-        <div>
-          {downloadUrl ? (
-            <>
-              <div>
-                <p>
-                  Scan this QR Code with your mobile device to add to Apple
-                  Wallet:
-                </p>
-                <QRCode
-                  value={downloadUrl}
-                  size={128}
-                  level="H"
-                  includeMargin
-                />
-              </div>
-              <a
-                href={downloadUrl}
-                download="pass.pkpass"
-                style={{ display: 'none' }}
-              >
-                Download Pass
-              </a>
-            </>
-          ) : (
-            <button
-              onClick={handleAddToWallet}
-              style={{ background: 'none', border: 'none' }}
-            >
-              <img src={appleWalletBadge} alt="Add to Apple Wallet" />
-            </button>
-          )}
-        </div>
-      </Grid>
+      </div>
     </SbEditable>
   );
 };
