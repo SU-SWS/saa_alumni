d606d53f4b943e296e93c3d50df48690045916d5
William Francis Misiaszek <wmisiasz@gmail.com>
2024-04-12 09:53:16 -0700

    Qr Code Device Detection (Mobile/Desktop too)

--- src/components/wallet/qrCodeApple.js
+++ src/components/wallet/qrCodeApple.js
@@ -2,10 +2,11 @@ import React, { useState, useContext, useEffect } from 'react';
 import SbEditable from 'storyblok-react';
 import QRCode from 'qrcode.react'; // Import QRCode component
 import AuthContext from '../../contexts/AuthContext';
+import { isIOS, isAndroid } from 'react-device-detect'; // For device detection
 
 const QrCodeApple = (props) => {
   const { blok } = props;
-  const apiUrl = `${window.location.origin}/api/walletpass/apple`; // Ensure the apiUrl is absolute
+  const apiUrl = `${window.location.origin}/api/walletpass/`;
   const [membershipNumber, setMembershipNumber] = useState('');
   const auth = useContext(AuthContext);
 
@@ -13,35 +14,40 @@ const QrCodeApple = (props) => {
     const memberships = auth.userProfile ? auth.userProfile.memberships : [];
     memberships.forEach((membership) => {
       if (
-        (membership.membershipStatus?.includes('Active') &&
-          membership.membershipGroup?.includes('SAA') &&
-          membership.membershipAffiliation?.includes('Alum')) ||
-        membership.membershipAffiliation?.includes('Affiliate')
+        membership.membershipStatus?.includes('Active') &&
+        membership.membershipGroup?.includes('SAA') &&
+        (membership.membershipAffiliation?.includes('Alum') || membership.membershipAffiliation?.includes('Affiliate'))
       ) {
         setMembershipNumber(membership.membershipNumber);
       }
     });
   }, [auth]);
 
+  const deviceUrl = isIOS ? `${apiUrl}apple` : isAndroid ? `${apiUrl}android` : apiUrl;
+
   return (
     <SbEditable content={blok}>
       <div className="su-mt-30">
         {membershipNumber ? (
           <>
-            <QRCode
-              value={apiUrl}
-              size={128}
-              level="H"
-              includeMargin
-              renderAs="svg" // Renders the QR code as an SVG
-            />
-            <p className="su-text-center su-text-m">
-              Scan to add to Apple Wallet
-            </p>
+            {isIOS || isAndroid ? (
+              <a href={deviceUrl}>
+                <img src={`/images/${isIOS ? 'apple-wallet-badge.svg' : 'google-wallet-badge.svg'}`} alt="Add to Wallet" />
+              </a>
+            ) : (
+              <QRCode
+                value={deviceUrl}
+                size={128}
+                level="H"
+                includeMargin
+                renderAs="svg" // Renders the QR code as an SVG
+              />
+            )}
+            <p className="su-text-center su-text-m">Scan or click to add to your wallet</p>
           </>
         ) : (
           <p className="su-text-center su-text-m">
-            Please log in to add your membership card to Apple Wallet.
+            Please log in to add your membership card to your wallet.
           </p>
         )}
       </div>
--- src/components/wallet/walletPassApple.js
+++ src/components/wallet/walletPassApple.js
@@ -1,11 +1,12 @@
 import React, { useState, useContext, useEffect } from 'react';
 import SbEditable from 'storyblok-react';
+import QRCode from 'qrcode.react'; // Import QRCode component
 import AuthContext from '../../contexts/AuthContext';
-import AppleWalletBadge from '../../images/apple-wallet-badge.svg';
+import { isIOS, isAndroid } from 'react-device-detect'; // For device detection
 
-const WalletPassApple = (props) => {
+const WalletPass = (props) => {
   const { blok } = props;
-  const apiUrl = '/api/walletpass/apple';
+  const apiUrl = `${window.location.origin}/api/walletpass/`;
   const [membershipNumber, setMembershipNumber] = useState('');
   const auth = useContext(AuthContext);
 
@@ -15,30 +16,38 @@ const WalletPassApple = (props) => {
       if (
         membership.membershipStatus?.includes('Active') &&
         membership.membershipGroup?.includes('SAA') &&
-        membership.membershipAffiliation?.includes('Alum')
-      ) {
-        setMembershipNumber(membership.membershipNumber);
-      } else if (
-        membership.membershipStatus?.includes('Active') &&
-        membership.membershipGroup?.includes('SAA') &&
-        membership.membershipAffiliation?.includes('Affiliate')
+        (membership.membershipAffiliation?.includes('Alum') || membership.membershipAffiliation?.includes('Affiliate'))
       ) {
         setMembershipNumber(membership.membershipNumber);
       }
     });
   }, [auth]);
 
+  const deviceUrl = isIOS ? `${apiUrl}apple` : isAndroid ? `${apiUrl}android` : apiUrl;
+
   return (
     <SbEditable content={blok}>
       <div className="su-mt-30">
-        {membershipNumber && (
-          <a href={apiUrl}>
-            <img src={AppleWalletBadge} alt="Add to Wallet" />
-          </a>
-        )}
-        {!membershipNumber && (
+        {membershipNumber ? (
+          <>
+            {isIOS || isAndroid ? (
+              <a href={deviceUrl}>
+                <img src={`/images/${isIOS ? 'apple-wallet-badge.svg' : 'google-wallet-badge.svg'}`} alt="Add to Wallet" />
+              </a>
+            ) : (
+              <QRCode
+                value={deviceUrl}
+                size={128}
+                level="H"
+                includeMargin
+                renderAs="svg" // Renders the QR code as an SVG
+              />
+            )}
+            <p className="su-text-center su-text-m">Scan or click to add to your wallet</p>
+          </>
+        ) : (
           <p className="su-text-center su-text-m">
-            Please log in to add your membership card to Apple Wallet.
+            Please log in to add your membership card to your wallet.
           </p>
         )}
       </div>
@@ -46,4 +55,4 @@ const WalletPassApple = (props) => {
   );
 };
 
-export default WalletPassApple;
+export default WalletPass;
