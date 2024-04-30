0304eb39691e1989977f2095541d4172ef49a4fd
Eric Bakenhus <1059679+ericbakenhus@users.noreply.github.com>
2024-04-30 16:57:28 -0500

    Adjust tests

--- cypress/e2e/1-perks/online-database.cy.ts
+++ cypress/e2e/1-perks/online-database.cy.ts
@@ -1,13 +1,14 @@
 describe('Perks - Online Databases Page', () => {
-  it('should load the additional payment page at /perks/online-databases/', () => {
+  it('should load the additional payment page at /benefits/online-publication-databases/', () => {
     // Login
     cy.login();
 
     // Visit the additional payment form page URL
-    cy.visit('/perks/online-databases/');
+    cy.visit('/benefits/online-publication-databases/');
 
+    cy.get('h1').should('contain.text', 'Online Publication Databases');
     cy.get('main').within(() => {
-      cy.contains('I am sorry but your account is not allowed to see this content.').should('exist');
+      cy.get('h3').first().should('contain.text', 'I am sorry but your account is not allowed to see this content.');
     });
   });
 });
