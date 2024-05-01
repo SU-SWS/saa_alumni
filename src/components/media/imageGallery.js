77b8d01256eeb41eeff95f4de2630ec1a66cfe0e
Shirley Chang <schang11@stanford.edu>
2024-05-01 11:46:54 -0700

    Adds imageGallery component that uses react-image-gallery

---
+++ src/components/media/imageGallery.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import ImageGallery from "react-image-gallery";
+import "react-image-gallery/styles/css/image-gallery.css";
+import SbEditable from 'storyblok-react';
+
+const SBImageGallery = ({ 
+  blok: {
+    showBullets,
+    showIndex,
+    showPlayButton,
+    showThumbnails,
+    thumbnailPosition,
+  },
+  blok,
+}) => {
+  const {Images} = blok;
+  const formattedImages = [];
+  Images.forEach((image) => {
+    formattedImages.push({
+      original: image.filename,
+      thumbnail: image.filename,
+      originalAlt: image.alt,
+      description: image.title,
+    });
+  });
+
+  return (
+    <SbEditable content={blok}>
+      <ImageGallery items={formattedImages}
+      showBullets={showBullets}
+      showIndex={showIndex}
+      showPlayButton={showPlayButton}
+      showThumbnails={showThumbnails}
+      thumbnailPosition={thumbnailPosition}
+      />
+    </SbEditable>
+  );
+};
+
+export default SBImageGallery;
