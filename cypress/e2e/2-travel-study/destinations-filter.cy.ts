b996043584c5d441bf67ac5a98e4adbd9e3ebd74
Eric Bakenhus <1059679+ericbakenhus@users.noreply.github.com>
2024-05-02 10:14:59 -0500

    Refactor test

--- cypress/e2e/2-travel-study/destinations-filter.cy.ts
+++ cypress/e2e/2-travel-study/destinations-filter.cy.ts
@@ -12,33 +12,34 @@ describe('Travel-Study Destinations Page', () => {
     // Confirm that a trip card exists
     cy.get('.trip-filter-page article h3').should('exist');
   });
+
   it('should have working filters', () => {
     // Visit the destinations page URL
     cy.visit('/travel-study/destinations/');
 
-    // Select Filter Month
+    // Select Filter Month and check for URL update
     cy.get('[data-test="filter-option--october"]').first().check({ force: true });
-    cy.reload(); // Needed for local Gatsby build
+    cy.url().should('contain', 'trip-month=oct');
+
+    // Load the filter from the URL
+    cy.visit('/travel-study/destinations/?page=1&trip-month=oct');
+
+    // Confirm chip exists
+    cy.get('[data-test="chip:October"]').should('exist');
 
     // Confirm that a trip card exists
     cy.get('.trip-filter-page article h3').should('exist');
 
-    // Clear Filters
+    // Clear Filters and check for URL update
     cy.get('[data-test="filter-btn--clear-all"]').first().click({force: true});
-    cy.reload(); // Needed for local Gatsby build
+    cy.url().should('eq', Cypress.config().baseUrl + '/travel-study/destinations/');
 
-    //  Select Filter Experience
-    cy.get('[data-test="filter-option--family-focused"]').first().check({force: true});
-    cy.reload(); // Needed for local Gatsby build
+    // Enable family focused filter
+    cy.visit('/travel-study/destinations/?page=1&trip-experience=family-focused');
 
-    // Confirm that the filter chip exists (and wait filters to be applied)
+    // Confirm that the filter chip exists (and wait for filters to be applied)
     cy.get('[data-test="chip:Family-Focused"]').should('exist');
     // Confirm that Southeast Asia trip card exists
     cy.get('.trip-filter-page article').contains('Southeast Asia').should('exist');
-
-    // Clear Filters
-    cy.get('[data-test="filter-btn--clear-all"]').first().click({force: true});
-    cy.reload(); // Needed for local Gatsby build
   });
-    
 });
