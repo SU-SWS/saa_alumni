7fdef14a05bc44003855c6e80281e31b3b3d6be8
Eric Bakenhus <1059679+ericbakenhus@users.noreply.github.com>
2024-05-02 02:02:04 -0500

    User can now see protected content

--- cypress/e2e/1-perks/online-database.cy.ts
+++ cypress/e2e/1-perks/online-database.cy.ts
@@ -1,15 +1,17 @@
 describe('Perks - Online Databases Page', () => {
-  it('should load the additional payment page at /benefits/online-publication-databases/', () => {
+  it('should load the online databases page at /benefits/online-publication-databases/', () => {
     // Login
     cy.login();
 
-    // Visit the additional payment form page URL
+    // Visit the online databases form page URL
     cy.visit('/benefits/online-publication-databases/');
 
     cy.get('h1').should('contain.text', 'Online Publication Databases');
+    
+    // Allowed user
     cy.get('#topperk').within(() => {
-      cy.get('h3').first().should('exist').should('contain.text', 'Websites for This Benefit');
-      cy.get('p').eq(1).should('exist').should('contain.text', 'This is a member-only benefit.');
+      cy.get('h3').should('not.exist');
+      cy.get('p').should('have.length', 4);
     });
   });
 });
