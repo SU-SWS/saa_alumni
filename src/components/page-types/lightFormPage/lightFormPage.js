81e9e0f37d3b65f7556be7bcfe2d2ef692b55e05
Sherakama <sheamck@stanford.edu>
2024-05-13 14:28:29 -0700

    DS-170 | @rebeccahongsf | Membership Phase 2 (#779)
    * DS-263 | @rebeccahongsf | toggle netlify build
    
    * DS-263 | @rebeccahongsf | update netlify toml to use membership vault var
    
    * DS-264 | @rebeccahongsf | add light form page component (#738)
    
    * DS-264 | @rebeccahongsf | add light form page component
    
    * add lightFormPage component
    
    * fixup import
    
    * DS-265 | @rebeccahongsf | build out gg nonce fetch
    
    * test another payment schedule
    
    * add check for self service widget
    
    * cleanup
    
    * cleanup
    
    * DS-270 | @rebeccahongsf | theme self service portal (#744)
    
    * DS-270 | @rebeccahongsf | theme self service portal
    
    * fixup payment history pay balance btn
    
    * adjust update card form spacing
    
    * fixup spacing
    
    * adjust form spacing
    
    * fixup credit card inactive button
    
    * swap out hero for hero image and adjust styles
    
    * DS-267 | @rebeccahongsf | fetch payment ref id and correctly setup memberships mock (#746)
    
    * DS-267 | @rebeccahongsf | fetch payment ref id and correctly setup memberships mock
    
    * adjust conditional check
    
    * fixup dupe err
    
    * add loader, config no payment id err
    
    * fixup json
    
    * DS-265 | @rebeccahongsf | fixup nonce error (#750)
    
    * DS-265 | @rebeccahongsf | fixup paymentRefId err
    
    * fixup
    
    * DS-170 | @rebeccahongsf | fixup layout issue
    
    * DS-170 | @rebeccahongsf | fixup
    
    * DS-433 | @rebeccahongsf | Add error messages for paymentRefId use cases (#754)
    
    * fixup
    
    * fixup
    
    * fixup styles
    
    * revert leading styling
    
    * DS-170 | @rebeccahongsf | QA fixes (#832)
    
    * DS-170 | @rebeccahongsf | QA fixes
    
    * remove console log
    
    * DS-170 | @rebeccahongsf | fixup schedule id display based on qa feedback
    
    * DS-170 | @rebeccahongsf | fixup payment update message
    
    * fixup heading margin
    
    * Add tel link
    
    Co-authored-by: Sherakama <sheamck@stanford.edu>
    
    * add auth to api endpoint and edge function protection
    
    * fixup lint
    
    * revert files
    
    * add back in edge function protection
    
    ---------
    
    Co-authored-by: rebeccahongsf <rebeccahongsf@gmail.com>
    Co-authored-by: Rebecca Hong <34019925+rebeccahongsf@users.noreply.github.com>

---
+++ src/components/page-types/lightFormPage/lightFormPage.js
@@ -0,0 +1,246 @@
+import React, { useEffect, useContext, useState } from 'react';
+import SbEditable from 'storyblok-react';
+import { dcnb } from 'cnbuilder';
+import { ClipLoader } from 'react-spinners';
+import { Container } from '../../layout/Container';
+import { Heading } from '../../simple/Heading';
+import Layout from '../../partials/layout';
+import CreateBloks from '../../../utilities/createBloks';
+import getNumBloks from '../../../utilities/getNumBloks';
+import Ankle from '../../partials/ankle/ankle';
+import { Grid } from '../../layout/Grid';
+import { GridCell } from '../../layout/GridCell';
+import AuthenticatedPage from '../../auth/AuthenticatedPage';
+import AuthContext from '../../../contexts/AuthContext';
+import { HeroImage } from '../../composite/HeroImage/HeroImage';
+import * as styles from './lightFormPage.styles';
+import { FlexBox } from '../../layout/FlexBox';
+import SbLink from '../../../utilities/sbLink';
+
+const NoMembershipError = () => (
+  <div>
+    <Heading level={2} size="2" weight="semibold" className="su-rs-mb-1">
+      Interested in joining SAA?
+    </Heading>
+    <p className="su-card-paragraph">
+      <SbLink link={{ url: '/membership/join' }}>Join now</SbLink> or visit our{' '}
+      <SbLink link={{ url: '/membership/faq' }}>Membership FAQs</SbLink> page to
+      learn more details about our membership planms and benefits.
+    </p>
+    <p className="su-card-paragraph su-mb-0">
+      If you purchased your membership online more than one business day ago,
+      and your membership is not recognized after logging in, call us at{' '}
+      <a href="tel:6507250692">(650) 725-0692</a> or email us at{' '}
+      <a href="mailto:membership@alumni.stanford.edu">
+        membership@alumni.stanford.edu
+      </a>
+    </p>
+  </div>
+);
+
+const FullPaidMembership = () => (
+  <div>
+    <Heading level={2} size="2" weight="semibold" className="su-rs-mb-1">
+      Your membership plan has been paid in full.
+    </Heading>
+    <p className="su-card-paragraph su-mb-0">
+      Access your membership card{' '}
+      <SbLink link={{ url: '/membership/saacard' }}>here</SbLink> and{' '}
+      <SbLink link={{ url: '/perks/' }}>
+        learn more about your SAA membership benefits
+      </SbLink>
+      .
+    </p>
+  </div>
+);
+
+const LightFormPage = (props) => {
+  const {
+    blok: {
+      widgetType,
+      title,
+      heroImage: { filename, alt, focus } = {},
+      formHeading,
+      orgId,
+      dssId,
+      giveGabForm,
+      ankleContent,
+    },
+    blok,
+  } = props;
+  const { userProfile } = useContext(AuthContext);
+  const numAnkle = getNumBloks(ankleContent);
+  const memberships = userProfile?.memberships;
+  const [kwoCreds, setKwoCreds] = useState('');
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [paymentRefId, setPaymentRefId] = useState(false);
+  const [paymentComplete, setPaymentComplete] = useState(false);
+
+  // Use the useEffect hook to fetch nonce when the component mounts
+  useEffect(() => {
+    if (!orgId || !dssId) {
+      return;
+    }
+
+    const fetchData = async () => {
+      if (paymentRefId) {
+        try {
+          const response = await fetch(
+            `/api/membership/payment/${orgId}/${dssId}/${paymentRefId}`
+          );
+
+          if (response.ok) {
+            const { nonce } = await response.json();
+            setKwoCreds(nonce);
+          } else {
+            console.error('API request failed.');
+            setError('API request failed.');
+          }
+        } catch (err) {
+          console.error('An error occurred:', err);
+          setError('An error occurred while fetching data.');
+        } finally {
+          setLoading(false);
+        }
+      }
+    };
+
+    const findPaymentRefId = () => {
+      const saaMembership = memberships?.find(
+        (membership) => membership.membershipGroup === 'SAA'
+      );
+
+      if (saaMembership) {
+        setPaymentRefId(saaMembership?.membershipGGPaymentReferenceID);
+        setPaymentComplete(saaMembership?.amountDue === 0);
+        if (paymentRefId === null) {
+          console.error('PaymentRefId is missing.');
+          setError(true);
+          setLoading(false);
+        } else {
+          setLoading(false);
+          fetchData();
+        }
+      } else {
+        console.error('No SAA Membership found');
+        setError(true);
+        setLoading(false);
+      }
+    };
+
+    if (memberships?.length === 0) {
+      setPaymentRefId(false);
+      setError(true);
+      setLoading(false);
+    } else if (memberships) {
+      findPaymentRefId();
+    }
+  }, [orgId, dssId, memberships, paymentRefId]);
+
+  return (
+    <AuthenticatedPage>
+      <SbEditable content={blok}>
+        <Layout {...props}>
+          <Container
+            as="main"
+            id="main-content"
+            className={styles.container}
+            width="full"
+          >
+            <div className={styles.fixedHero}>
+              <HeroImage
+                filename={filename}
+                alt={alt}
+                focus={focus}
+                overlay="formDark"
+                aspectRatio="5x2"
+                className={styles.fixedHeroImg}
+              />
+            </div>
+            <FlexBox direction="col" className={styles.contentWrapper}>
+              <div className={styles.contentStyle}>
+                <Heading
+                  level={1}
+                  size="6"
+                  align="center"
+                  font="serif"
+                  id="page-title"
+                >
+                  {title}
+                </Heading>
+              </div>
+              <Grid gap xs={12}>
+                <GridCell
+                  xs={12}
+                  md={10}
+                  xl={8}
+                  xxl={6}
+                  className={dcnb(
+                    'su-light-form',
+                    widgetType,
+                    styles.formCardStyle
+                  )}
+                >
+                  {loading ? (
+                    <div className="su-flex su-flex-row">
+                      <ClipLoader color="#00BFFF" height={50} width={50} />
+                      <p className="su-ml-03em">Loading form...</p>
+                      <noscript>
+                        Sorry, but you must have Javascript enabled to use the
+                        form.
+                      </noscript>
+                    </div>
+                  ) : (
+                    <>
+                      {error ? (
+                        <>
+                          {paymentRefId === null ? (
+                            <FullPaidMembership />
+                          ) : (
+                            <NoMembershipError />
+                          )}
+                        </>
+                      ) : (
+                        <>
+                          {widgetType === 'su-payment-update' &&
+                          paymentComplete ? (
+                            <FullPaidMembership />
+                          ) : (
+                            <>
+                              <div>
+                                <Heading
+                                  level={2}
+                                  size="2"
+                                  weight="semibold"
+                                  className="su-rs-mb-3"
+                                >
+                                  {formHeading}
+                                </Heading>
+                              </div>
+                              <CreateBloks
+                                blokSection={giveGabForm}
+                                bgCardStyle="su-bg-transparent"
+                                kwoCredentials={kwoCreds}
+                                className={
+                                  paymentComplete ? 'gg-payment-complete' : ''
+                                }
+                              />
+                            </>
+                          )}
+                        </>
+                      )}
+                    </>
+                  )}
+                </GridCell>
+              </Grid>
+            </FlexBox>
+            {numAnkle > 0 && <Ankle isDark {...props} />}
+          </Container>
+        </Layout>
+      </SbEditable>
+    </AuthenticatedPage>
+  );
+};
+
+export default LightFormPage;
