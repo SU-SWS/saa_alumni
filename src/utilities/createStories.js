82a5848ca4032993a50c6790953c50287c3d625e
Sait Furkan Teke <35101659+stfurkan@users.noreply.github.com>
2024-05-28 16:40:34 -0700

    use console warn

--- src/utilities/createStories.js
+++ src/utilities/createStories.js
@@ -13,7 +13,7 @@ const CreateStories = ({ stories, ...props }) => {
 
         // TODO: remove this check after testing
         if (!story?.content?.component) {
-          console.error('Story does not have a component', story);
+          console.warn('Story does not have a component', story);
           return null;
         }
 
