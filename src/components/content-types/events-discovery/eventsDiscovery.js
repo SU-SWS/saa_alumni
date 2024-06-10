a0260ad3d8d572ed31a03d31123351d43c267ac3
Sait Furkan Teke <35101659+stfurkan@users.noreply.github.com>
2024-06-10 10:12:21 -0700

    update loading copy

--- src/components/content-types/events-discovery/eventsDiscovery.js
+++ src/components/content-types/events-discovery/eventsDiscovery.js
@@ -19,7 +19,7 @@ const LoadingComponent = () => (
   <div className="su-w-full su-flex su-items-center su-justify-center su-rs-my-1">
     <div className="su-flex su-justify-center su-items-center su-h-300 su-w-400">
       <div className="su-animate-spin su-rounded-full su-h-32 su-w-32 su-border-t-4 su-border-b-4 su-border-cardinal-red-light su-mr-8" />
-      Loading search results...
+      Please wait! Loading events...
     </div>
   </div>
 );
