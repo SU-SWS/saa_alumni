e24e347fbf23c2153ddd6fcb6d6db1b2bb539d96
Sait Furkan Teke <35101659+stfurkan@users.noreply.github.com>
2024-06-10 10:40:03 -0700

    add no results component

--- src/components/content-types/events-discovery/eventsDiscovery.js
+++ src/components/content-types/events-discovery/eventsDiscovery.js
@@ -5,6 +5,7 @@ import {
   InfiniteHits,
   Configure,
   useInstantSearch,
+  SearchBox,
 } from 'react-instantsearch';
 import SynchronizedEvent from '../synchronized-event/synchronizedEvent';
 
@@ -38,6 +39,39 @@ const LoadingIndicator = () => {
   return null;
 };
 
+const NoResultsBoundary = ({ children, fallback }) => {
+  const { results } = useInstantSearch();
+
+  // The `__isArtificial` flag makes sure not to display the No Results message
+  // when no hits have been returned.
+  // eslint-disable-next-line no-underscore-dangle
+  if (!results.__isArtificial && results.nbHits === 0) {
+    return (
+      <>
+        {fallback}
+        <div hidden>{children}</div>
+      </>
+    );
+  }
+
+  return children;
+};
+
+const NoResultsComponent = () => (
+  <div className="su-w-full su-flex su-items-center su-justify-center">
+    <div className="su-flex su-justify-center su-items-center su-rs-my-2">
+      <div className="su-text-center">
+        <h2 className="su-text-center su-text-cardinal-red-light">
+          No events found!
+        </h2>
+        <p className="su-text-center">
+          Try changing your search criteria or filter options to find events.
+        </p>
+      </div>
+    </div>
+  </div>
+);
+
 const EventsDiscovery = () => {
   // TODO: We might want to remove this loading state and show the loading indicator only when search is in progress.
   // Show loading indicator for 3 seconds before initial page load
@@ -65,6 +99,7 @@ const EventsDiscovery = () => {
       <div className="su-flex su-flex-row su-justify-center lg:su-space-x-40 su-mx-12">
         <div className="su-hidden lg:su-block su-h-600 su-w-300 su-border-2">
           Filters
+          <SearchBox />
         </div>
         <div className="su-flex su-flex-col su-space-y-20">
           <div className="su-flex su-flex-row su-justify-between">
@@ -75,21 +110,23 @@ const EventsDiscovery = () => {
               <span className="">Mobile Filters</span>
             </div>
           </div>
-          <InfiniteHits
-            hitComponent={Hit}
-            showPrevious={false}
-            translations={{
-              showMoreButtonText: 'Show more',
-            }}
-            classNames={{
-              root: '',
-              list: 'su-list-none su-pl-0 su-grid su-grid-cols-1 su-w-full',
-              item: 'su-mb-0 su-w-full',
-              loadMore:
-                'su-w-full su-text-center su-mt-8 su-border-2 su-border-cardinal-red su-text-cardinal-red-light su-rounded-md su-p-4 su-cursor-pointer su-transition su-duration-200 su-ease-in-out hover:su-bg-cardinal-red hover:su-text-white',
-              disabledLoadMore: 'su-hidden',
-            }}
-          />
+          <NoResultsBoundary fallback={<NoResultsComponent />}>
+            <InfiniteHits
+              hitComponent={Hit}
+              showPrevious={false}
+              translations={{
+                showMoreButtonText: 'Show more',
+              }}
+              classNames={{
+                root: '',
+                list: 'su-list-none su-pl-0 su-grid su-grid-cols-1 su-w-full',
+                item: 'su-mb-0 su-w-full',
+                loadMore:
+                  'su-w-full su-text-center su-mt-8 su-border-2 su-border-cardinal-red su-text-cardinal-red-light su-rounded-md su-p-4 su-cursor-pointer su-transition su-duration-200 su-ease-in-out hover:su-bg-cardinal-red hover:su-text-white',
+                disabledLoadMore: 'su-hidden',
+              }}
+            />
+          </NoResultsBoundary>
         </div>
       </div>
     </InstantSearch>
