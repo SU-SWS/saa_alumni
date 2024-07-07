7659d9160fbaff5ab2285fad94bbda93375cd586
Eric Bakenhus <1059679+ericbakenhus@users.noreply.github.com>
2024-07-07 18:47:22 -0500

    DS-690: Slug notes

--- src/utilities/slugify.js
+++ src/utilities/slugify.js
@@ -1,6 +1,7 @@
 export const slugify = (str = '') =>
   str
     .toLowerCase()
-    .replaceAll(/[^a-z0-9]+/i, '-')
-    .replace(/^-/, '')
-    .replace(/-$/, '');
+    .replaceAll(/[^a-z0-9]+/i, '-') // Replace all non-alphanum characters with hyphens
+    .replaceAll(/-{2,}/, '-') // Replace groups of multiple hyphens with a single hyphen
+    .replace(/^-/, '') // No leading hyphen
+    .replace(/-$/, ''); // No trailing hyphen
