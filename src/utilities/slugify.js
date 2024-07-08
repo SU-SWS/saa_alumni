3424ec732feffac87fb872f42ec7bc88b6394242
Eric Bakenhus <1059679+ericbakenhus@users.noreply.github.com>
2024-07-07 19:03:14 -0500

    DS-690: Add global flag

--- src/utilities/slugify.js
+++ src/utilities/slugify.js
@@ -1,7 +1,7 @@
 export const slugify = (str = '') =>
   str
     .toLowerCase()
-    .replaceAll(/[^a-z0-9]+/i, '-') // Replace all non-alphanum characters with hyphens
-    .replaceAll(/-{2,}/, '-') // Replace groups of multiple hyphens with a single hyphen
+    .replaceAll(/[^a-z0-9]+/gi, '-') // Replace all non-alphanum characters with hyphens
+    .replaceAll(/-{2,}/g, '-') // Replace groups of multiple hyphens with a single hyphen
     .replace(/^-/, '') // No leading hyphen
     .replace(/-$/, ''); // No trailing hyphen
