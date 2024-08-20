352d87ddd91102069d6850755da232d6dbda1ca3
Eric Bakenhus <1059679+ericbakenhus@users.noreply.github.com>
2024-08-20 13:46:11 -0500

    Nojira | @ericbakenhus | Use siteUrl for base URL in SSR (#927)
    * Use siteUrl

--- src/components/navigation/MegaMenu/megaMenuTopLevelLinkItem.js
+++ src/components/navigation/MegaMenu/megaMenuTopLevelLinkItem.js
@@ -2,20 +2,23 @@ import React from 'react';
 import SbEditable from 'storyblok-react';
 import { useLocation } from '@reach/router';
 import SbLink from '../../../utilities/sbLink';
+import getSiteUrl from '../../../utilities/getSiteUrl';
 import * as styles from './megaMenuLink.styles';
 import * as panelStyles from './megaMenuPanel.styles';
 
 const MegaMenuTopLevelLinkItem = ({ blok: { link, linkText }, blok }) => {
   const location = useLocation();
+  const baseUrl = location?.origin || getSiteUrl();
 
   // Compare the current browser URL to the link URL to determine if the button is active.
   const browserUrl = new URL(
     location.pathname.replace(/\/+$/, '') + location.hash,
-    location.origin
+    baseUrl
   );
+
   const linkUrl = link.url || link.cached_url;
   const strippedLink = linkUrl.replace(/\/+$/, '');
-  const linkUrlFull = new URL(strippedLink, location.origin);
+  const linkUrlFull = new URL(strippedLink, baseUrl);
   if (link.anchor) {
     linkUrlFull.hash = link.anchor;
   }
