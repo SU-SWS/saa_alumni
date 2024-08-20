d21e1e819dd246de4f040c8b1f24a00409dd7d9b
Eric Bakenhus <1059679+ericbakenhus@users.noreply.github.com>
2024-08-20 12:36:19 -0500

    Point empty mega menu child links to home page

--- src/components/navigation/MegaMenu/megaMenuLinkItem.js
+++ src/components/navigation/MegaMenu/megaMenuLinkItem.js
@@ -3,19 +3,24 @@ import SbEditable from 'storyblok-react';
 import SbLink from '../../../utilities/sbLink';
 import * as styles from './megaMenuLink.styles';
 
-const MegaMenuLinkItem = ({ blok: { link, linkText }, blok }) => (
-  <SbEditable content={blok}>
-    <li className="su-rs-mb-0">
-      <SbLink
-        link={link}
-        classes={styles.menuLink}
-        hasExternalIcon
-        externalIconClasses={styles.externalIcon}
-      >
-        {linkText}
-      </SbLink>
-    </li>
-  </SbEditable>
-);
+const MegaMenuLinkItem = ({ blok: { link, linkText }, blok }) => {
+  const processedLink =
+    link?.url || link?.cached_url ? link : { ...link, cached_url: 'home' };
+
+  return (
+    <SbEditable content={blok}>
+      <li className="su-rs-mb-0">
+        <SbLink
+          link={processedLink}
+          classes={styles.menuLink}
+          hasExternalIcon
+          externalIconClasses={styles.externalIcon}
+        >
+          {linkText}
+        </SbLink>
+      </li>
+    </SbEditable>
+  );
+};
 
 export default MegaMenuLinkItem;
