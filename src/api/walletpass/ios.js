c83ecc520dcbb6cd136b14baab7d88bf1aed7718
William Misiaszek <48428545+willyumfrancis@users.noreply.github.com>
2024-08-26 15:34:39 -0700

    STAN-344: Digital Membership Cards (#833)
    Android & iOS Digital Membership cards for Alumni members.

---
+++ src/api/walletpass/ios.js
@@ -0,0 +1,70 @@
+/* eslint-disable no-console */
+import { createRouter, expressWrapper } from 'next-connect';
+
+import { generateAppleWalletPass } from '../../utilities/walletPass';
+import { authInstance } from '../../utilities/authInstance';
+import { MegaProfile } from '../../utilities/MegaProfile';
+
+const generatePkPass = async (req, res) => {
+  try {
+    const mp = new MegaProfile();
+    let fullprofile = {};
+    let memberships = [];
+
+    const profileId = req.user.encodedSUID;
+
+    const requests = [
+      mp.get(`/${profileId}/profiles/fullprofile`),
+      mp.get(`/${profileId}/profiles/memberships`),
+    ];
+
+    const resolved = await Promise.allSettled(requests);
+
+    // Full GG Data.
+    if (resolved[0].status === 'fulfilled' && !!resolved[0].value?.data) {
+      fullprofile = resolved[0].value.data;
+    } else {
+      fullprofile.contact.name = {};
+      fullprofile.contact.name.digitalName = `${req.user.firstName} ${req.user.lastName}`;
+    }
+
+    // Membership Data;
+    if (resolved[1].status === 'fulfilled' && !!resolved[1].value?.data) {
+      memberships = resolved[1].value.data.memberships;
+    }
+
+    const mpUser = {
+      ...fullprofile,
+      memberships,
+    };
+
+    const pkpass = await generateAppleWalletPass(mpUser);
+    const buffer = pkpass.getAsBuffer();
+
+    if (pkpass) {
+      res.setHeader('Content-Type', 'application/vnd.apple.pkpass');
+      res.setHeader(
+        'Content-Disposition',
+        'attachment; filename="pass.pkpass"'
+      );
+      res.status(200).send(buffer);
+    } else {
+      res
+        .status(500)
+        .send(
+          'Pass URL generation failed. Please try again or contact support.'
+        );
+    }
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .send('Passkit generation failed. Please try again or contact support.');
+  }
+};
+
+const router = createRouter()
+  .use(expressWrapper(authInstance.authorize()))
+  .get(generatePkPass);
+
+export default router.handler();
