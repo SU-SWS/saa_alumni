d552b3cfe85522c8a8890aff63d2cd59ea4fe4a3
Eric Bakenhus <1059679+ericbakenhus@users.noreply.github.com>
2024-09-16 18:14:06 -0500

    DS-800 | @ericbakenhus | Filter wrapping (#958)

--- src/components/events-discovery/components/Facets/FacetList.jsx
+++ src/components/events-discovery/components/Facets/FacetList.jsx
@@ -81,12 +81,13 @@ export const FacetList = ({ attribute, subfilter = false }) => {
               classNames={{
                 root: 'su-mt-8',
                 list: 'su-flex su-flex-col su-gap-4 su-list-none su-pl-8',
-                item: 'su-mb-0',
-                checkbox:
-                  'su-mr-6 su-items-center su-rounded-md su-bg-white su-w-20 su-h-20 su-border-2 su-border-cardinal-red-light su-text-cardinal-red-light su-cursor-pointer su-transition su-duration-200 su-ease-in-out hocus:su-bg-cardinal-red focus:su-ring-1 focus:su-ring-cardinal-red-light',
+                item: 'su-mb-0 su-pl-26',
                 label:
-                  'su-flex su-items-center hover:su-cursor-pointer su-text-18',
-                count: "su-ml-4 before:su-content-['('] after:su-content-[')']",
+                  'su-flex su-flex-wrap su-items-center hover:su-cursor-pointer su-text-18',
+                checkbox:
+                  '-su-ml-26 su-mr-6 su-items-center su-rounded-md su-bg-white su-w-20 su-h-20 su-border-2 su-border-cardinal-red-light su-text-cardinal-red-light su-cursor-pointer su-transition su-duration-200 su-ease-in-out hocus:su-bg-cardinal-red focus:su-ring-1 focus:su-ring-cardinal-red-light',
+                labelText: 'su-mr-4',
+                count: "before:su-content-['('] after:su-content-[')']",
               }}
             />
           </DynamicWidgets>
