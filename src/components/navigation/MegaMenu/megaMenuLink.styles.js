6c2775551b43cf73d3484b92c5b76b7307cce3a5
shc314 <36527303+shc314@users.noreply.github.com>
2024-09-24 08:56:13 -0700

    Update src/components/navigation/MegaMenu/megaMenuLink.styles.js
    Co-authored-by: Sherakama <sheamck@stanford.edu>

--- src/components/navigation/MegaMenu/megaMenuLink.styles.js
+++ src/components/navigation/MegaMenu/megaMenuLink.styles.js
@@ -2,4 +2,4 @@ export const menuLink =
   'su-inline-block su-leading-display su-text-black hocus:su-text-digital-red su-no-underline hocus:su-underline su-font-normal su-text-19 su-group su-backface-hidden';
 export const externalIcon = 'su-text-digital-red su-text-21';
 export const externalIconWhite =
-  'su-text-white su-text-21 su-mb-2 group-hover:su-text-digital-red-xlight group-focus:su-text-digital-red-xlight';
+  'su-text-white su-text-21 su-mb-2 group-hocus:su-text-digital-red-xlight';
